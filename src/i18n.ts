3cf90d84f59ad55160d8fbecb9449fef09353f2b
Gilad Lekner <gilad215@gmail.com>
2021-12-11 18:51:52 +0200

    remove public i18n locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,5 +4,6 @@
 // TODO: We should make the custom i18next JSON lexer available to plugins so
 // that they do not need this file.
 
+// t('plugin__kubevirt-plugin~Virtualization')
 // t('plugin__kubevirt-plugin~VirtualMachines')
 // t('plugin__kubevirt-plugin~Create Virtual Machine')
