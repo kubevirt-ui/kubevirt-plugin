01b5e8658954a0ab1252f93e0f1bb36d81d4d510
Gilad Lekner <gilad215@gmail.com>
2022-01-23 14:09:54 +0200

    add cyprees

--- test-prow-e2e.sh
+++ test-prow-e2e.sh
@@ -4,7 +4,7 @@ set -eExuo pipefail
 
 if [ $# -eq 0 ]
   then
-    echo "kubevirt-plugin image not provided"
+    echo "kubevirt image not provided"
     echo "exiting..."
     exit 1
 fi
@@ -15,25 +15,22 @@ function generateLogsAndCopyArtifacts {
   oc get secrets -A -o yaml >> ${ARTIFACT_DIR}/secrets.yaml
   oc get catalogsource -A -o wide > ${ARTIFACT_DIR}/catalogsource.yaml
   oc get catalogsource -A -o yaml >> ${ARTIFACT_DIR}/catalogsource.yaml
-  oc get subscriptions -n openshift-cnv -o wide > ${ARTIFACT_DIR}/subscription_details.yaml
-  oc get subscriptions -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/subscription_details.yaml
-  oc get csvs -n openshift-cnv -o wide > ${ARTIFACT_DIR}/csvs.yaml
-  oc get csvs -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/csvs.yaml
-  oc get deployments -n openshift-cnv -o wide > ${ARTIFACT_DIR}/deployment_details.yaml
-  oc get deployments -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/deployment_details.yaml
-  oc get installplan -n openshift-cnv -o wide > ${ARTIFACT_DIR}/installplan.yaml
-  oc get installplan -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/installplan.yaml
+  oc get subscriptions -n ${NS} -o wide > ${ARTIFACT_DIR}/subscription_details.yaml
+  oc get subscriptions -n ${NS} -o yaml >> ${ARTIFACT_DIR}/subscription_details.yaml
+  oc get deployments -n ${NS} -o wide > ${ARTIFACT_DIR}/deployment_details.yaml
+  oc get deployments -n ${NS} -o yaml >> ${ARTIFACT_DIR}/deployment_details.yaml
+  oc get installplan -n ${NS} -o wide > ${ARTIFACT_DIR}/installplan.yaml
+  oc get installplan -n ${NS} -o yaml >> ${ARTIFACT_DIR}/installplan.yaml
   oc get nodes -o wide > ${ARTIFACT_DIR}/node.yaml
   oc get nodes -o yaml >> ${ARTIFACT_DIR}/node.yaml
-  oc get pods -n openshift-cnv -o wide >> ${ARTIFACT_DIR}/pod_details_openshift-cnv.yaml
-  oc get pods -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/pod_details_openshift-cnv.yaml
-  oc logs deploy/odf-operator-controller-manager manager -n openshift-cnv > ${ARTIFACT_DIR}/odf.logs
-  for pod in `oc get pods -n ${NS} --no-headers -o custom-columns=":metadata.name" | grep "kubevirt-plugin"`; do
+  oc get pods -n ${NS} -o wide >> ${ARTIFACT_DIR}/pod_details_openshift-storage.yaml
+  oc get pods -n ${NS} -o yaml >> ${ARTIFACT_DIR}/pod_details_openshift-storage.yaml
+  for pod in `oc get pods -n ${NS} --no-headers -o custom-columns=":metadata.name" | grep "kubevirt"`; do
         echo $pod 
         oc logs $pod -n ${NS} > ${ARTIFACT_DIR}/${pod}.logs
   done
-  oc get serviceaccounts -n openshift-cnv -o wide > ${ARTIFACT_DIR}/serviceaccount.yaml
-  oc get serviceaccounts -n openshift-cnv -o yaml >> ${ARTIFACT_DIR}/serviceaccount.yaml
+  oc get serviceaccounts -n ${NS} -o wide > ${ARTIFACT_DIR}/serviceaccount.yaml
+  oc get serviceaccounts -n ${NS} -o yaml >> ${ARTIFACT_DIR}/serviceaccount.yaml
   oc get console.v1.operator.openshift.io cluster -o yaml >> ${ARTIFACT_DIR}/cluster.yaml
   
   if [ -d "$ARTIFACT_DIR" ] && [ -d "$SCREENSHOTS_DIR" ]; then
@@ -54,7 +51,7 @@ trap generateLogsAndCopyArtifacts ERR
 PULL_SECRET_PATH="/var/run/operator-secret/dockerconfig" 
 NAMESPACE="openshift-marketplace"
 SECRET_NAME="ocs-secret"
-NS="openshift-cnv"
+NS="kubevirt-plugin"
 ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
 SCREENSHOTS_DIR=gui-test-screenshots
 
@@ -74,17 +71,10 @@ function linkSecrets {
 function deleteAllPods {
   oc delete pods --all -n $1 
 }
-oc patch operatorhub.config.openshift.io/cluster -p='{"spec":{"sources":[{"disabled":true,"name":"redhat-operators"}]}}' --type=merge
 
 echo "Creating secret for CI builds in ${NAMESPACE}"
 
 createSecret ${NAMESPACE}
-
-oc apply -f openshift-ci/odf-catalog-source.yaml ;
-
-echo "Waiting for CatalogSource to be Ready"
-# Have to sleep here for atleast 1 min to ensure catalog source is in stable READY state
-sleep 60 
 
 echo "Creating secret for linking pods"
 createSecret ${NS}
@@ -113,27 +103,19 @@ deleteAllPods ${NS}
 
 sleep 120
 
-# Enable console plugin for kubevirt-plugin
+# Enable console plugin for Kubevirt
 export CONSOLE_CONFIG_NAME="cluster"
 export KUBEVIRT_PLUGIN_NAME="kubevirt-plugin"
+KUBEVIRT_PLUGIN_IMAGE="$1"
 
-echo "Enabling Console Plugin for ODF Operator"
+oc process -f template.yaml \
+  -p PLUGIN_NAME=kubevirt-plugin \
+  -p NAMESPACE=kubevirt-plugin \
+  -p IMAGE=${KUBEVIRT_PLUGIN_IMAGE} \
+  | oc create -f -
+
+echo "Enabling Console Plugin for Kubevirt"
 oc patch console.v1.operator.openshift.io ${CONSOLE_CONFIG_NAME} --type=json -p="[{'op': 'add', 'path': '/spec/plugins', 'value':["${KUBEVIRT_PLUGIN_NAME}"]}]"
-
-ODF_CONSOLE_IMAGE="$1"
-export ODF_CSV_NAME="$(oc get csv -n openshift-cnv -o=jsonpath='{.items[?(@.spec.displayName=="OpenShift Data Foundation")].metadata.name}')"
-
-oc patch csv ${ODF_CSV_NAME} -n openshift-cnv --type='json' -p \
-  "[{'op': 'replace', 'path': '/spec/install/spec/deployments/1/spec/template/spec/containers/0/image', 'value': \"${ODF_CONSOLE_IMAGE}\"}]"
-
-# Installation occurs.
-# This is also the default case if the CSV is in "Installing" state initially.
-timeout 15m bash <<-'EOF'
-echo "waiting for ${ODF_CSV_NAME} clusterserviceversion to succeed"
-until [ "$(oc -n openshift-cnv get csv -o=jsonpath="{.items[?(@.metadata.name==\"${ODF_CSV_NAME}\")].status.phase}")" == "Succeeded" ]; do
-  sleep 1
-done
-EOF
 
 INSTALLER_DIR=${INSTALLER_DIR:=${ARTIFACT_DIR}/installer}
 
