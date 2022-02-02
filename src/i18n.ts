0ad9355a024f53937ea19889f6b8a6598b5a60ee
yzamir <yzamir@redhat.com>
2022-02-02 09:53:04 +0200

    ./

--- src/i18n.ts
+++
@@ -1,9 +0,0 @@
-// IMPORTANT: This file adds comments recognized by the react-i18next-parser so that
-// labels declared in console-extensions.json are added to the message catalog.
-
-// TODO: We should make the custom i18next JSON lexer available to plugins so
-// that they do not need this file.
-
-// t('plugin__kubevirt-plugin~Virtualization')
-// t('plugin__kubevirt-plugin~VirtualMachines')
-// t('plugin__kubevirt-plugin~Create Virtual Machine')
