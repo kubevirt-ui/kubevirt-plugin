ee3d5c9c8c34dd65a5341b5e40d4ac6b46d6f746
Gilad Lekner <gilad215@gmail.com>
2022-02-08 14:49:34 +0000

    test i18n action

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -10,3 +10,4 @@
 // t('plugin__kubevirt-plugin~Catalog')
 // t('plugin__kubevirt-plugin~VirtualMachineInstances')
 // t('plugin__kubevirt-plugin~Templates')
+// t('plugin__kubevirt-plugin~test i18n pr')
