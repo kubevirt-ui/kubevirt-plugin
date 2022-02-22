e6c5d919c1991d51e9abeb5dc994cfe690108b1a
Gilad Lekner <gilad215@gmail.com>
2022-02-22 16:59:01 +0000

    add initial context for wizard

--- src/views/catalog/wizard/tabs/scheduling/WizardSchedulingTab.tsx
+++ src/views/catalog/wizard/tabs/scheduling/WizardSchedulingTab.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
+// import { useWizardVMContext } from '../../../utils/WizardVMContext';
+
 const WizardSchedulingTab = () => {
+  // const { vm, updateVM } = useWizardVMContext();
+
   return <div>WizardSchedulingTab</div>;
 };
 
