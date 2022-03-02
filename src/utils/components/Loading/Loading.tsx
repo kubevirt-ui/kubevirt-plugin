51dfa4a38b5bb5141b3fc3b0a7450704298aed62
Aviv Turgeman <aturgema@redhat.com>
2022-03-02 16:51:58 +0000

    Add loading component
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

---
+++ src/utils/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Loading: React.FC = () => (
+  <div className="co-m-loader co-an-fade-in-out" data-test="loading-indicator">
+    <div className="co-m-loader-dot__one" />
+    <div className="co-m-loader-dot__two" />
+    <div className="co-m-loader-dot__three" />
+  </div>
+);
+
+export default React.memo(Loading);
