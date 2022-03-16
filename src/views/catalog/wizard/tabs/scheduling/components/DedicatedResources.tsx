4d2da933e82cd042b6b2dcceceb2a641f6f82f97
Gilad Lekner <gilad215@gmail.com>
2022-03-16 14:24:19 +0200

    add wizard scheduling tab

---
+++ src/views/catalog/wizard/tabs/scheduling/components/DedicatedResources.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+
+type DedicatedResourcesProps = {
+  vm: V1VirtualMachine;
+};
+
+const DedicatedResources: React.FC<DedicatedResourcesProps> = ({ vm }) => {
+  const { t } = useKubevirtTranslation();
+  const isDedicatedResources = vm?.spec?.template?.spec?.domain?.cpu?.dedicatedCpuPlacement;
+
+  return isDedicatedResources
+    ? t('Workload scheduled with dedicated resources (guaranteed policy)')
+    : t('No Dedicated resources applied');
+};
+
+export default DedicatedResources;
