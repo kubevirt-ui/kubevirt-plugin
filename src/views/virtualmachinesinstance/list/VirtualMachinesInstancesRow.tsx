87d8c2bcc33ffd008de951b46f2010e7222f1387
Matan Schatzman <mschatzm@redhat.com>
2022-03-21 12:46:29 +0000

    Adding more columns to VMI list
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachinesinstance/list/VirtualMachinesInstancesRow.tsx
+++ src/views/virtualmachinesinstance/list/VirtualMachinesInstancesRow.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import format from 'date-fns/format';
 import { VMStatusConditionLabelList } from 'src/views/virtualmachines/list/components/VMStatusConditionLabel';
 
+import { NodeModel } from '@kubevirt-ui/kubevirt-api/console';
 import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { ResourceLink, RowProps, TableData } from '@openshift-console/dynamic-plugin-sdk';
 import { GlobeAmericasIcon } from '@patternfly/react-icons';
 
 import VirtualMachinesInsanceActions from '../actions/VirtualMachinesInstanceActions';
+import VirtualMachinesInstancesIP from '../components/VirtualMachinesInstanceIP';
 import VirtualMachinesInstancesStatus from '../components/VirtualMachinesInstancesStatus';
 
 type VirtualMachinesInstancesRowProps = RowProps<V1VirtualMachineInstance, { kind: string }>;
@@ -33,6 +35,12 @@ const VirtualMachinesInstancesRow: React.FC<VirtualMachinesInstancesRowProps> = 
       <TableData id="created" activeColumnIDs={activeColumnIDs}>
         <GlobeAmericasIcon />{' '}
         {format(new Date(obj?.metadata?.creationTimestamp), 'MMM dd, yyyy, h:mm a')}
+      </TableData>
+      <TableData id="node" activeColumnIDs={activeColumnIDs}>
+        <ResourceLink kind={NodeModel.kind} name={obj.status.nodeName} />
+      </TableData>
+      <TableData id="ipAddress" activeColumnIDs={activeColumnIDs}>
+        <VirtualMachinesInstancesIP vmi={obj} />
       </TableData>
       <TableData id="actions" activeColumnIDs={activeColumnIDs} className="pf-c-table__action">
         <VirtualMachinesInsanceActions vmi={obj} />
