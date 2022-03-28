654ce060467c429cbabb02c308d2e955582779d3
Matan Schatzman <mschatzm@redhat.com>
2022-03-28 10:13:41 +0000

    Adding disk card to VM overivew page
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDisks/VirtualMachinesOverviewTabDisks.tsx
+++ src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDisks/VirtualMachinesOverviewTabDisks.tsx
@@ -1,7 +1,41 @@
 import * as React from 'react';
 
-const VirtualMachinesOverviewTabDisks = () => {
-  return <div>VirtualMachinesOverviewTabDisks</div>;
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import useDisksTableData from '@kubevirt-utils/resources/vm/hooks/disk/useDisksTableData';
+import { DiskRowDataLayout } from '@kubevirt-utils/resources/vm/utils/disk/constants';
+import { VirtualizedTable } from '@openshift-console/dynamic-plugin-sdk';
+import { Card, CardBody, CardTitle, Divider } from '@patternfly/react-core';
+
+import useVirtualMachinesOverviewTabDisksColumns from './hooks/useVirtualMachinesOverviewTabDisksColumns';
+import VirtualMachinesOverviewTabDisksRow from './VirtualMachinesOverviewTabDisksRow';
+
+import './virtual-machines-overview-tab-disks.scss';
+
+const VirtualMachinesOverviewTabDisks = ({ vm }) => {
+  const [disks, loaded, loadedError] = useDisksTableData(vm);
+  const { t } = useKubevirtTranslation();
+  const columns = useVirtualMachinesOverviewTabDisksColumns();
+
+  return (
+    <div className="VirtualMachinesOverviewTabDisks--main">
+      <Card>
+        <CardTitle className="text-muted">
+          {t('Disk ({{count}})', { count: disks.length || 0 })}
+        </CardTitle>
+        <Divider />
+        <CardBody isFilled>
+          <VirtualizedTable<DiskRowDataLayout>
+            data={disks}
+            unfilteredData={disks}
+            loaded={loaded}
+            loadError={loadedError}
+            columns={columns}
+            Row={VirtualMachinesOverviewTabDisksRow}
+          />
+        </CardBody>
+      </Card>
+    </div>
+  );
 };
 
 export default VirtualMachinesOverviewTabDisks;
