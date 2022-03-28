f37256016a3bd981c7f6b26da6af05c207c8a193
Gilad Lekner <gilad215@gmail.com>
2022-03-28 06:06:00 +0000

    add vm create dropdown

--- src/views/virtualmachines/list/VirtualMachinesList.tsx
+++ src/views/virtualmachines/list/VirtualMachinesList.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import { useHistory } from 'react-router-dom';
 
+import { VirtualMachineModelGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console';
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import {
   K8sResourceCommon,
   ListPageBody,
-  ListPageCreate,
+  ListPageCreateDropdown,
   ListPageFilter,
   ListPageHeader,
   useK8sWatchResource,
@@ -25,6 +27,8 @@ type VirtualMachinesListProps = {
 
 const VirtualMachinesList: React.FC<VirtualMachinesListProps> = ({ kind, namespace }) => {
   const { t } = useKubevirtTranslation();
+  const history = useHistory();
+  const ref = VirtualMachineModelGroupVersionKind;
 
   const [vms, loaded, loadError] = useK8sWatchResource<V1VirtualMachine[]>({
     kind,
@@ -35,11 +39,23 @@ const VirtualMachinesList: React.FC<VirtualMachinesListProps> = ({ kind, namespa
 
   const [unfilteredData, data, onFilterChange] = useListPageFilter(vms, filters);
 
+  const createItems = {
+    catalog: t('From catalog'),
+    yaml: t('With YAML'),
+  };
+
+  const onCreate = (type: string) =>
+    type === 'catalog'
+      ? history.push(`/k8s/ns/${namespace || 'default'}/templatescatalog`)
+      : history.push(`/k8s/cluster/${ref.group}~${ref.version}~${ref.kind}/~new`);
+
   const columns = useVirtualMachineColumns();
   return (
     <>
       <ListPageHeader title={t('Virtual Machines')}>
-        <ListPageCreate groupVersionKind={kind}>{t('Create Virtual Machine')}</ListPageCreate>
+        <ListPageCreateDropdown items={createItems} onClick={onCreate}>
+          {t('Create')}
+        </ListPageCreateDropdown>
       </ListPageHeader>
       <ListPageBody>
         <ListPageFilter
