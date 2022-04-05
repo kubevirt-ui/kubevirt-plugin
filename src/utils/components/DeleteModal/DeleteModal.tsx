ba19577a6c1fcd0077248e52f7b02edba415b22a
Matan Schatzman <mschatzm@redhat.com>
2022-04-05 06:22:49 +0000

    Adding delete + annotation modals to vmi list

---
+++ src/utils/components/DeleteModal/DeleteModal.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import TabModal, { DeleteResourceMessege } from '@kubevirt-utils/components/TabModal/TabModal';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+
+import { K8sResourceCommon } from '@openshift-console/dynamic-plugin-sdk';
+import { ButtonVariant } from '@patternfly/react-core';
+
+type DeleteModalProps = {
+  isOpen: boolean;
+  obj: K8sResourceCommon;
+  onDeleteSubmit: () => Promise<void | K8sResourceCommon>;
+  onClose: () => void;
+  headerText?: string;
+};
+
+const DeleteModal: React.FC<DeleteModalProps> = React.memo(
+  ({ isOpen, obj, onDeleteSubmit, onClose, headerText }) => {
+    const { t } = useKubevirtTranslation();
+
+    return (
+      <TabModal<K8sResourceCommon>
+        obj={obj}
+        headerText={headerText || t('Delete Resource?')}
+        onSubmit={onDeleteSubmit}
+        isOpen={isOpen}
+        onClose={onClose}
+        submitBtnText={t('Delete')}
+        submitBtnVariant={ButtonVariant.danger}
+        titleIconVariant={'warning'}
+      >
+        <DeleteResourceMessege obj={obj} />
+      </TabModal>
+    );
+  },
+);
+
+export default DeleteModal;
