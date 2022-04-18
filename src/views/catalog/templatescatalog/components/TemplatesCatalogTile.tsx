7c0291a81057e0f88f95308eb1d7449d67bebe77
Gilad Lekner <gilad215@gmail.com>
2022-04-18 12:04:36 +0300

    compress svgs and add img loading

--- src/views/catalog/templatescatalog/components/TemplatesCatalogTile.tsx
+++ src/views/catalog/templatescatalog/components/TemplatesCatalogTile.tsx
@@ -13,7 +13,7 @@ import {
   getTemplateWorkload,
 } from '@kubevirt-utils/resources/template/utils/selectors';
 import { CatalogTile } from '@patternfly/react-catalog-view-extension';
-import { Stack, StackItem } from '@patternfly/react-core';
+import { Skeleton, Stack, StackItem } from '@patternfly/react-core';
 
 import { getTemplateOSIcon } from '../utils/os-icons';
 
@@ -25,17 +25,28 @@ export type TemplateTileProps = {
 
 export const TemplateTile: React.FC<TemplateTileProps> = React.memo(({ template, onClick }) => {
   const { t } = useKubevirtTranslation();
+  const [iconLoaded, setIconLoaded] = React.useState(false);
 
   const workload = getTemplateWorkload(template);
   const displayName = getTemplateName(template);
   const bootSourceType = getTemplateBootSourceType(template)?.type;
   const { memory, cpuCount } = getTemplateFlavorData(template);
 
+  const icon = React.useMemo(() => {
+    return getTemplateOSIcon(template);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [template?.metadata?.annotations?.iconClass]);
+
   return (
     <CatalogTile
       className="vm-catalog-grid-tile"
       data-test-id={template.metadata.name}
-      icon={<img src={getTemplateOSIcon(template)} alt="os-icon" />}
+      icon={
+        <div>
+          {!iconLoaded && <Skeleton shape="circle" width="40px" height="40px" />}
+          <img src={icon} alt="os-icon" onLoad={() => setIconLoaded(true)} />
+        </div>
+      }
       title={
         <Stack>
           <StackItem>
