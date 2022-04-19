06278e237d38ac1959d8e7b1200bbb2e293e1155
Dana Orr <dorr@redhat.com>
2022-04-19 09:45:28 +0000

    changing the space in vmi console tab and in vm console tab
    Signed-off-by: Dana Orr <dorr@redhat.com>

--- src/views/virtualmachinesinstance/details/tabs/console/VirtualMachinesInstancePageConsoleTab.tsx
+++ src/views/virtualmachinesinstance/details/tabs/console/VirtualMachinesInstancePageConsoleTab.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from 'react-router-dom';
 
 import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import Consoles from '@kubevirt-utils/components/Consoles/Consoles';
+import { PageSection } from '@patternfly/react-core';
 
 type VirtualMachinesInstancePageConsoleTabProps = RouteComponentProps & {
   obj: V1VirtualMachineInstance;
@@ -11,7 +12,11 @@ type VirtualMachinesInstancePageConsoleTabProps = RouteComponentProps & {
 const VirtualMachinesInstancePageConsoleTab: React.FC<
   VirtualMachinesInstancePageConsoleTabProps
 > = ({ obj: vmi }) => {
-  return <Consoles vmi={vmi} />;
+  return (
+    <PageSection>
+      <Consoles vmi={vmi} />
+    </PageSection>
+  );
 };
 
 export default VirtualMachinesInstancePageConsoleTab;
