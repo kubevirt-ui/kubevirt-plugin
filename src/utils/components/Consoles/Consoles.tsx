5cb4089ce30ac11403a356a4babbf0413fedc62a
Matan Schatzman <mschatzm@redhat.com>
2022-04-20 11:45:28 +0000

    Adding cloud int credentials to console component
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/components/Consoles/Consoles.tsx
+++ src/utils/components/Consoles/Consoles.tsx
@@ -3,9 +3,10 @@ import * as React from 'react';
 import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import Loading from '@kubevirt-utils/components/Loading/Loading';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { Bullseye } from '@patternfly/react-core';
+import { Bullseye, Stack, StackItem } from '@patternfly/react-core';
 
 import { AccessConsoles } from './components/AccessConsoles/AccessConsoles';
+import CloudInitCredentials from './components/CloudInitCredentials/CloudInitCredentials';
 import SerialConsoleConnector from './components/SerialConsole/SerialConsoleConnector';
 import { SERIAL_CONSOLE_TYPE, VNC_CONSOLE_TYPE } from './components/utils/ConsoleConsts';
 import VncConsole from './components/vnc-console/VncConsole';
@@ -21,24 +22,29 @@ const Consoles: React.FC<ConsolesProps> = ({ vmi }) => {
       <Loading />
     </Bullseye>
   ) : (
-    <>
-      <AccessConsoles
-        preselectedType={VNC_CONSOLE_TYPE}
-        textSelectConsoleType={t('Select console type')}
-        textSerialConsole={t('Serial console')}
-        textVncConsole={t('VNC console')}
-        textDesktopViewerConsole={t('Desktop viewer')}
-      >
-        <VncConsole
-          type={VNC_CONSOLE_TYPE}
-          encrypt={isEncrypted}
-          host={window.location.hostname}
-          port={window.location.port || (isEncrypted ? SECURE : INSECURE)}
-          path={`api/kubernetes/apis/subresources.kubevirt.io/v1/namespaces/${vmi?.metadata?.namespace}/virtualmachineinstances/${vmi?.metadata?.name}/vnc`}
-        />
-        <SerialConsoleConnector vmi={vmi} type={SERIAL_CONSOLE_TYPE} />
-      </AccessConsoles>
-    </>
+    <Stack hasGutter>
+      <StackItem>
+        <CloudInitCredentials vmi={vmi} />
+      </StackItem>
+      <StackItem>
+        <AccessConsoles
+          preselectedType={VNC_CONSOLE_TYPE}
+          textSelectConsoleType={t('Select console type')}
+          textSerialConsole={t('Serial console')}
+          textVncConsole={t('VNC console')}
+          textDesktopViewerConsole={t('Desktop viewer')}
+        >
+          <VncConsole
+            type={VNC_CONSOLE_TYPE}
+            encrypt={isEncrypted}
+            host={window.location.hostname}
+            port={window.location.port || (isEncrypted ? SECURE : INSECURE)}
+            path={`api/kubernetes/apis/subresources.kubevirt.io/v1/namespaces/${vmi?.metadata?.namespace}/virtualmachineinstances/${vmi?.metadata?.name}/vnc`}
+          />
+          <SerialConsoleConnector vmi={vmi} type={SERIAL_CONSOLE_TYPE} />
+        </AccessConsoles>
+      </StackItem>
+    </Stack>
   );
 };
 
--- src/utils/components/Consoles/utils/constants.ts
+++ src/utils/components/Consoles/utils/constants.ts
@@ -1,2 +1,5 @@
 export const SECURE = '443';
 export const INSECURE = '80';
+
+export const CLOUD_INIT_MISSING_USERNAME =
+  'No username set, see operating system documentation for the default username.';
