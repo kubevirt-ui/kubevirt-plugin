f7d961ccd4f7b76bb010bf7fb14bfaff00b2cb37
Matan Schatzman <mschatzm@redhat.com>
2022-04-27 10:11:47 +0000

    Adding copy buttton to network ip at vm overview tab
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/list/components/FirstItemListPopover/FirstItemListPopover.tsx
+++ src/views/virtualmachines/list/components/FirstItemListPopover/FirstItemListPopover.tsx
@@ -2,20 +2,33 @@ import * as React from 'react';
 
 import { NO_DATA_DASH } from '@kubevirt-utils/resources/vm/utils/constants';
 import { Button, ButtonVariant, Popover, PopoverPosition } from '@patternfly/react-core';
+import { CopyIcon } from '@patternfly/react-icons';
 
 type FirstItemListPopoverProps = {
   items: string[];
   headerContent?: React.ReactNode | string;
   className?: string;
+  includeCopyFirstItem?: boolean;
 };
 
 const FirstItemListPopover: React.FC<FirstItemListPopoverProps> = ({
   items,
   headerContent,
   className,
+  includeCopyFirstItem,
 }) => (
   <div className={className}>
-    <div>{items?.[0] || NO_DATA_DASH}</div>
+    <div>
+      {items?.[0] || NO_DATA_DASH}
+      {includeCopyFirstItem && items?.[0] && (
+        <Button
+          variant={ButtonVariant.link}
+          onClick={() => navigator.clipboard.writeText(items?.[0])}
+        >
+          <CopyIcon />
+        </Button>
+      )}
+    </div>
     {items?.length > 1 && (
       <Popover
         headerContent={headerContent}
