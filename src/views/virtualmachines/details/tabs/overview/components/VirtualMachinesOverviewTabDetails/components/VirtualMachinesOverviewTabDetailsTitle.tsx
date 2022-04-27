6140d4b001cf4c0a185edc057cb7760f076cb3df
Matan Schatzman <mschatzm@redhat.com>
2022-04-27 09:53:17 +0000

    Connecting copy ssh command button in vm overview tab
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsTitle.tsx
+++ src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsTitle.tsx
@@ -4,11 +4,16 @@ import VirtualMachineModel from '@kubevirt-ui/kubevirt-api/console/models/Virtua
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import CPUMemoryModal from '@kubevirt-utils/components/CPUMemoryModal/CpuMemoryModal';
 import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
+import useSSHCommand from '@kubevirt-utils/components/UserCredentials/useSSHCommand';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { useVMIAndPodsForVM } from '@kubevirt-utils/resources/vm/hooks';
 import { k8sUpdate } from '@openshift-console/dynamic-plugin-sdk';
 import { CardTitle, Dropdown, DropdownItem, KebabToggle } from '@patternfly/react-core';
+import { CopyIcon } from '@patternfly/react-icons';
 
 import { pauseVM, startVM, stopVM, unpauseVM } from '../../../../../../actions/actions';
+import { printableVMStatus } from '../../../../../../utils';
+import useSSHService from '../../../../details/hooks/useSSHService';
 
 type VirtualMachinesOverviewTabDetailsTitleProps = {
   vm: V1VirtualMachine;
@@ -20,9 +25,13 @@ const VirtualMachinesOverviewTabDetailsTitle: React.FC<
   const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
   const { createModal } = useModal();
   const { t } = useKubevirtTranslation();
+  const { vmi } = useVMIAndPodsForVM(vm?.metadata?.name, vm?.metadata?.namespace);
+  const [sshService] = useSSHService(vmi);
+  const { command } = useSSHCommand(vmi, sshService);
 
-  const isMachinePaused = vm?.status?.printableStatus === 'Paused';
-  const isMachineStopped = vm?.status?.printableStatus === 'Stopped';
+  const isMachinePaused = vm?.status?.printableStatus === printableVMStatus.Paused;
+  const isMachineStopped = vm?.status?.printableStatus === printableVMStatus.Stopped;
+  const isMachineRunning = vm?.status?.printableStatus === printableVMStatus.Running;
 
   return (
     <CardTitle className="text-muted card-title">
@@ -33,8 +42,15 @@ const VirtualMachinesOverviewTabDetailsTitle: React.FC<
         isOpen={isDropdownOpen}
         isPlain
         dropdownItems={[
-          <DropdownItem onClick={() => null} key="copy">
-            {t('Copy SSH Command')}
+          <DropdownItem
+            onClick={() => command && navigator.clipboard.writeText(command)}
+            key="copy"
+            isDisabled={!isMachineRunning || !sshService}
+          >
+            {t('Copy SSH Command')}{' '}
+            <span className="text-muted">
+              <CopyIcon />
+            </span>
           </DropdownItem>,
           <DropdownItem onClick={() => (isMachineStopped ? startVM(vm) : stopVM(vm))} key="stop">
             {isMachineStopped ? t('Resume Virtual machine') : t('Stop Virtual machine')}
