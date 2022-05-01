28f16f39d4805298beae81a859a4abca1a4130f0
Gilad Lekner <gilad215@gmail.com>
2022-05-01 18:49:49 +0300

    switch back to i18next-parser

---
+++ i18next-parser.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  input: ['src/**/*.{js,jsx,ts,tsx}'],
+  sort: true,
+  createOldCatalogs: false,
+  keySeparator: false,
+  locales: ['en'],
+  namespaceSeparator: '~',
+  reactNamespace: false,
+  defaultNamespace: 'plugin__kubevirt-plugin',
+  useKeysAsDefaultValue: true,
+};
