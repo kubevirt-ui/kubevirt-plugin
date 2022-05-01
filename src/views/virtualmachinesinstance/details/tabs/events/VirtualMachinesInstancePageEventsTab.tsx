82912981c9aaaf6c5125698f6f70f6002e76ebea
Matan Schatzman <mschatzm@redhat.com>
2022-05-01 07:00:42 +0000

    Adding events tab to vm and vmi
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachinesinstance/details/tabs/events/VirtualMachinesInstancePageEventsTab.tsx
+++ src/views/virtualmachinesinstance/details/tabs/events/VirtualMachinesInstancePageEventsTab.tsx
@@ -1,7 +1,28 @@
-import * as React from 'react';
+import React from 'react';
+import { RouteComponentProps } from 'react-router';
 
-const VirtualMachinesInstancePageEventsTab = () => {
-  return <div>VirtualMachinesInstancesPageEventsTab</div>;
+import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { ResourceEventStream } from '@openshift-console/dynamic-plugin-sdk';
+import { Bullseye } from '@patternfly/react-core';
+import Loading from '@kubevirt-utils/components/Loading/Loading';
+
+type VirtualMachinesInstancePageEventsTabProps = RouteComponentProps & {
+  obj: V1VirtualMachineInstance;
+};
+const VirtualMachinesInstancePageEventsTab: React.FC<VirtualMachinesInstancePageEventsTabProps> = ({
+  obj: vmi,
+}) => {
+  return (
+    <React.Suspense
+      fallback={
+        <Bullseye>
+          <Loading />
+        </Bullseye>
+      }
+    >
+      {vmi?.metadata && <ResourceEventStream resource={vmi} />}
+    </React.Suspense>
+  );
 };
 
 export default VirtualMachinesInstancePageEventsTab;
