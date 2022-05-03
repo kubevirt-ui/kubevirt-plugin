355c6f942e0215f977d44e80b133e25933b52764
Aviv Turgeman <aturgema@redhat.com>
2022-05-03 12:31:54 +0000

    Add OS and Node filter to VM list view
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/virtualmachines/utils/virtualMachineRowFilter.ts
+++ src/views/virtualmachines/utils/virtualMachineRowFilter.ts
@@ -1,7 +1,16 @@
 import { TFunction } from 'i18next';
 
-import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
-import { LABEL_USED_TEMPLATE_NAME } from '@kubevirt-utils/resources/template';
+import { V1VirtualMachine, V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { getAnnotation } from '@kubevirt-utils/resources/shared';
+import {
+  ANNOTATIONS,
+  LABEL_USED_TEMPLATE_NAME,
+  OS_NAME_LABELS,
+} from '@kubevirt-utils/resources/template';
+import {
+  getOperatingSystem,
+  getOperatingSystemName,
+} from '@kubevirt-utils/resources/vm/utils/operation-system/operationSystem';
 import { RowFilter } from '@openshift-console/dynamic-plugin-sdk';
 
 import { printableVMStatus } from './virtualMachineStatuses';
@@ -52,6 +61,72 @@ export const getTemplatesFilter = (vms: V1VirtualMachine[], t: TFunction): RowFi
       items: Array.from(templates).map((templateName) => ({
         id: templateName,
         title: templateName,
+      })),
+    },
+  ];
+};
+
+export const getNodesFilter = (vmis: V1VirtualMachineInstance[], t: TFunction): RowFilter[] => {
+  if (vmis?.length === 0 || vmis?.every((vm) => !vm.status?.nodeName)) {
+    return [] as RowFilter[];
+  }
+
+  const nodes = vmis.reduce((acc, vmi) => {
+    const nodeName = vmi?.status?.nodeName;
+    if (nodeName) {
+      acc.add(nodeName);
+    }
+    return acc;
+  }, new Set<string>());
+
+  return [
+    {
+      filterGroupName: t('Node'),
+      type: 'node',
+      reducer: (obj) => {
+        return vmis?.find((vmi) => vmi.metadata?.name === obj?.metadata?.name)?.status?.nodeName;
+      },
+      filter: (selectedNodes, obj) => {
+        const nodeName = vmis?.find((vmi) => vmi.metadata?.name === obj?.metadata?.name)?.status
+          ?.nodeName;
+        return selectedNodes.selected?.length === 0 || selectedNodes.selected?.includes(nodeName);
+      },
+      items: Array.from(nodes).map((nodeName) => ({
+        id: nodeName,
+        title: nodeName,
+      })),
+    },
+  ];
+};
+
+export const getOSFilter = (vms: V1VirtualMachine[], t: TFunction): RowFilter[] => {
+  return [
+    {
+      filterGroupName: t('OS'),
+      type: 'os',
+      reducer: (obj) => {
+        const osAnnotation = getAnnotation(obj?.spec?.template, ANNOTATIONS.os);
+        const osLabel = getOperatingSystemName(obj) || getOperatingSystem(obj);
+        const osName = Object.values(OS_NAME_LABELS).find(
+          (osKey) =>
+            osAnnotation?.toLowerCase()?.startsWith(osKey?.toLowerCase()) ||
+            osLabel?.toLowerCase()?.startsWith(osKey?.toLowerCase()),
+        );
+        return osName;
+      },
+      filter: (selectedOS, obj) => {
+        const osAnnotation = getAnnotation(obj?.spec?.template, ANNOTATIONS.os);
+        const osLabel = getOperatingSystemName(obj) || getOperatingSystem(obj);
+        const osName = Object.values(OS_NAME_LABELS).find(
+          (osKey) =>
+            osAnnotation?.toLowerCase()?.startsWith(osKey?.toLowerCase()) ||
+            osLabel?.toLowerCase()?.startsWith(osKey?.toLowerCase()),
+        );
+        return selectedOS.selected?.length === 0 || selectedOS.selected?.includes(osName);
+      },
+      items: Object.values(OS_NAME_LABELS).map((osName) => ({
+        id: osName,
+        title: osName,
       })),
     },
   ];
