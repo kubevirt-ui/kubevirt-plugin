ffb8508a2ea7d0b9a238161fe5e6b2464b7d81b9
Aviv Turgeman <aturgema@redhat.com>
2022-05-15 14:18:59 +0000

    Bug 2086303: non-priv user can't create VM when namespace is not selected
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/catalog/templatescatalog/components/TemplatesCatalogPageHeader.tsx
+++ src/views/catalog/templatescatalog/components/TemplatesCatalogPageHeader.tsx
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom';
 import { VirtualMachineModelRef } from '@kubevirt-ui/kubevirt-api/console';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import {
+  Alert,
   Breadcrumb,
   BreadcrumbItem,
   Button,
@@ -12,8 +13,8 @@ import {
   Title,
 } from '@patternfly/react-core';
 
-export const TemplatesCatalogPageHeader: React.FC<{ namespace: string }> = React.memo(
-  ({ namespace }) => {
+export const TemplatesCatalogPageHeader: React.FC<{ namespace: string; isAdmin: boolean }> =
+  React.memo(({ namespace, isAdmin }) => {
     const { t } = useKubevirtTranslation();
     const history = useHistory();
 
@@ -33,14 +34,20 @@ export const TemplatesCatalogPageHeader: React.FC<{ namespace: string }> = React
           </BreadcrumbItem>
           <BreadcrumbItem>{t('Catalog')}</BreadcrumbItem>
         </Breadcrumb>
-        <Stack>
+        <Stack hasGutter>
           <StackItem className="co-m-pane__heading">
             <Title headingLevel="h1">{t('Create new VirtualMachine from catalog')}</Title>
           </StackItem>
+          {!namespace && !isAdmin && (
+            <StackItem>
+              <Alert variant="danger" title={t('Error')} isInline>
+                {t('Create VirtualMachine is forbidden at cluster scope. Please select a Project')}
+              </Alert>
+            </StackItem>
+          )}
           <StackItem>{t('Select an option to create a VirtualMachine')}</StackItem>
         </Stack>
       </div>
     );
-  },
-);
+  });
 TemplatesCatalogPageHeader.displayName = 'TemplatesCatalogPageHeader';
