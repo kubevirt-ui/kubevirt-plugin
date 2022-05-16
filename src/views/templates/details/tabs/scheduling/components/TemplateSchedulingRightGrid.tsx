58ad187157f66e520e0660734330d0fae749bfa9
Hilda Stastna <hstastna@redhat.com>
2022-05-16 18:19:00 +0200

    Make Node selector field in Template's Scheduling tab editable
    Make the field editable. Add the modal to edit the labels.
    Enable editing the labels if the template is not a common template.

--- src/views/templates/details/tabs/scheduling/components/TemplateSchedulingRightGrid.tsx
+++ src/views/templates/details/tabs/scheduling/components/TemplateSchedulingRightGrid.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { TemplateDetailsGridProps } from 'src/views/templates/details/tabs/details/TemplateDetailsPage';
 import DedicatedResources from 'src/views/templates/details/tabs/scheduling/components//DedicatedResources';
 import EvictionStrategy from 'src/views/templates/details/tabs/scheduling/components//EvictionStrategy';
-import { isCommonVMTemplate } from 'src/views/templates/utils';
 
 import { DescriptionList } from '@patternfly/react-core';
 
-const TemplateSchedulingRightGrid: React.FC<TemplateDetailsGridProps> = ({ template }) => {
-  const isTemplateEditable = !isCommonVMTemplate(template);
+import { TemplateSchedulingGridProps } from './TemplateSchedulingLeftGrid';
 
+const TemplateSchedulingRightGrid: React.FC<TemplateSchedulingGridProps> = ({
+  template,
+  editable,
+}) => {
   return (
     <DescriptionList>
-      <DedicatedResources template={template} editable={isTemplateEditable} />
-      <EvictionStrategy template={template} editable={isTemplateEditable} />
+      <DedicatedResources template={template} editable={editable} />
+      <EvictionStrategy template={template} editable={editable} />
     </DescriptionList>
   );
 };
