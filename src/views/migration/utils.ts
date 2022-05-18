c6acd17d856d10a158e30f491bab4adc11f56dd4
yzamir <yzamir@redhat.com>
2022-05-18 14:27:51 +0300

    Remove MTV and VMI mneu items

--- src/views/migration/utils.ts
+++
@@ -1,4 +0,0 @@
-import { PackageManifestKind } from './constant';
-
-export const createInstallUrl = (operator: PackageManifestKind, namespace?: string) =>
-  `/operatorhub/subscribe?pkg=${operator?.metadata?.name}&catalog=${operator?.status?.catalogSource}&catalogNamespace=${operator?.status?.catalogSourceNamespace}&targetNamespace=${namespace}`;
