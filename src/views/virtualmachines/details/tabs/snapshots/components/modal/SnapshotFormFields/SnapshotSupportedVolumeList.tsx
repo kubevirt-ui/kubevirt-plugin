2ad499c91529f4bef012b50f79a2735a02815d20
Aviv Turgeman <aturgema@redhat.com>
2022-05-24 13:31:53 +0000

    cant create snapshot if VM is without disks
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/virtualmachines/details/tabs/snapshots/components/modal/SnapshotFormFields/SnapshotSupportedVolumeList.tsx
+++ src/views/virtualmachines/details/tabs/snapshots/components/modal/SnapshotFormFields/SnapshotSupportedVolumeList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { ExpandableSection, FormGroup, Stack, StackItem } from '@patternfly/react-core';
 
 type SupportedSnapshotVolumesListProps = {
@@ -11,26 +12,29 @@ const SnapshotSupportedVolumeList: React.FC<SupportedSnapshotVolumesListProps> =
   supportedVolumes,
 }) => {
   const { t } = useKubevirtTranslation();
-  const [isExpanded, setIsExpanded] = React.useState<boolean>(true);
-  if (supportedVolumes?.length === 0) {
-    return null;
-  }
+  const [isExpanded, setIsExpanded] = React.useState<boolean>(false);
+  const volumesCount = !isEmpty(supportedVolumes) ? supportedVolumes?.length : 0;
+  const volumeCountMsg = t('Disks included in this snapshot ({{count}})', {
+    count: volumesCount,
+  });
 
   return (
     <FormGroup fieldId="snapshot-supported-volume-list">
-      <ExpandableSection
-        isExpanded={isExpanded}
-        onClick={() => setIsExpanded((prev) => !prev)}
-        toggleText={t('Disks included in this snapshot ({{count}})', {
-          count: supportedVolumes?.length,
-        })}
-      >
-        <Stack>
-          {supportedVolumes?.map((vol) => (
-            <StackItem key={vol.name}>{vol.name}</StackItem>
-          ))}
-        </Stack>
-      </ExpandableSection>
+      {volumesCount > 0 ? (
+        <ExpandableSection
+          isExpanded={isExpanded}
+          onClick={() => setIsExpanded((prev) => !prev)}
+          toggleText={volumeCountMsg}
+        >
+          <Stack>
+            {supportedVolumes?.map((vol) => (
+              <StackItem key={vol.name}>{vol.name}</StackItem>
+            ))}
+          </Stack>
+        </ExpandableSection>
+      ) : (
+        <b>{volumeCountMsg}</b>
+      )}
     </FormGroup>
   );
 };
