7643d6d90bd2e3e9e3421dcf9e6812ae7fa3b19d
Aviv Turgeman <aturgema@redhat.com>
2022-05-25 09:51:46 +0000

    Bug 2081182: change to use node IP in ssh command
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/utils/components/UserCredentials/useSSHCommand.ts
+++ src/utils/components/UserCredentials/useSSHCommand.ts
@@ -1,9 +1,7 @@
-import { InfrastructureModel, modelToGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console';
 import { IoK8sApiCoreV1Service } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { getCloudInitCredentials } from '@kubevirt-utils/resources/vmi';
 import { getSSHNodePort } from '@kubevirt-utils/utils/utils';
-import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
 export type useSSHCommandResult = {
   command: string;
@@ -15,26 +13,12 @@ const useSSHCommand = (
   vmi: V1VirtualMachineInstance,
   sshService: IoK8sApiCoreV1Service,
 ): useSSHCommandResult => {
-  const [infrastructure, infrastructureLoaded, infrastructureError] = useK8sWatchResource<any>({
-    groupVersionKind: modelToGroupVersionKind(InfrastructureModel),
-    namespace: vmi?.metadata?.namespace,
-    name: 'cluster',
-    isList: false,
-  });
-
-  const infrastuctureApiUrl =
-    infrastructureLoaded && !infrastructureError && infrastructure?.status?.apiServerURL;
-  const apiHostname = infrastuctureApiUrl && new URL(infrastuctureApiUrl).hostname;
   const consoleHostname = window.location.hostname; // fallback to console hostname
 
   const { user } = getCloudInitCredentials(vmi);
   const sshServicePort = getSSHNodePort(sshService);
 
-  let command = 'ssh ';
-
-  if (user) command += `${user}@`;
-
-  command += `${apiHostname || consoleHostname} -p ${sshServicePort}`;
+  const command = `ssh ${user && `${user}@`}${consoleHostname} -p ${sshServicePort}`;
 
   return {
     command,
