4102d5911c8b655a30784d08cd29e8df34452062
Gilad Lekner <gilad215@gmail.com>
2022-06-15 05:49:22 +0300

    Bug 2096208: Templates in the catalog list are not sorted

--- src/views/catalog/templatescatalog/utils/helpers.ts
+++ src/views/catalog/templatescatalog/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { V1Template } from '@kubevirt-ui/kubevirt-api/console';
+import { OS_NAME_TYPES } from '@kubevirt-utils/resources/template';
 import {
   getTemplateName,
   getTemplateOS,
@@ -10,23 +11,38 @@ import {
 import { TemplateFilters } from '../hooks/useVmTemplatesFilters';
 
 export const filterTemplates = (templates: V1Template[], filters: TemplateFilters): V1Template[] =>
-  templates.filter((tmp) => {
-    const textFilterLowerCase = filters?.query.toLowerCase();
-    const workload = getTemplateWorkload(tmp);
+  templates
+    .filter((tmp) => {
+      const textFilterLowerCase = filters?.query.toLowerCase();
+      const workload = getTemplateWorkload(tmp);
 
-    const textFilter = textFilterLowerCase
-      ? getTemplateName(tmp).toLowerCase().includes(textFilterLowerCase) ||
-        tmp?.metadata?.name?.includes(textFilterLowerCase)
-      : true;
+      const textFilter = textFilterLowerCase
+        ? getTemplateName(tmp).toLowerCase().includes(textFilterLowerCase) ||
+          tmp?.metadata?.name?.includes(textFilterLowerCase)
+        : true;
 
-    const defaultVariantFilter = filters?.onlyDefault
-      ? isDefaultVariantTemplate(tmp) || isCustomTemplate(tmp)
-      : true;
+      const defaultVariantFilter = filters?.onlyDefault
+        ? isDefaultVariantTemplate(tmp) || isCustomTemplate(tmp)
+        : true;
 
-    const workloadFilter = filters?.workload?.size > 0 ? filters.workload.has(workload) : true;
+      const workloadFilter = filters?.workload?.size > 0 ? filters.workload.has(workload) : true;
 
-    const osNameFilter =
-      filters?.osName?.size > 0 ? filters?.osName?.has(getTemplateOS(tmp)) : true;
+      const osNameFilter =
+        filters?.osName?.size > 0 ? filters?.osName?.has(getTemplateOS(tmp)) : true;
 
-    return defaultVariantFilter && textFilter && workloadFilter && osNameFilter;
-  });
+      return defaultVariantFilter && textFilter && workloadFilter && osNameFilter;
+    })
+    // show RHEL templates first, then alphabetically
+    .sort((a, b) => {
+      if (getTemplateOS(a) === OS_NAME_TYPES.rhel) {
+        return -1;
+      }
+      if (getTemplateOS(b) === OS_NAME_TYPES.rhel) {
+        return 1;
+      }
+
+      const aName = getTemplateName(a) || a?.metadata?.name;
+      const bName = getTemplateName(b) || b?.metadata?.name;
+
+      return aName?.localeCompare(bName);
+    });
