297c16d787c37a261410d0be9e76cc3aff062d6e
Phillip Bailey <phbailey@redhat.com>
2022-06-16 10:57:14 -0400

    Hide details card for non-admins

--- src/views/clusteroverview/overview/components/details-card/DetailsCard.tsx
+++ src/views/clusteroverview/overview/components/details-card/DetailsCard.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { useIsAdmin } from '@kubevirt-utils/hooks/useIsAdmin';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { DetailsBody } from '@openshift-console/dynamic-plugin-sdk-internal';
 import { OverviewDetailItem } from '@openshift-console/plugin-shared';
@@ -11,6 +12,7 @@ import SubscriptionStatus from './SourceMissingStatus/SubscriptionStatus';
 
 const DetailsCard: React.FC = () => {
   const { t } = useKubevirtTranslation();
+  const isAdmin = useIsAdmin();
   const kvCsvDetails = useKubevirtCSVDetails();
   const {
     name,
@@ -25,34 +27,36 @@ const DetailsCard: React.FC = () => {
   const isLoading = !loaded && !loadError && !kubevirtSub;
 
   return (
-    <Card data-test-id="kubevirt-overview-dashboard--details-card">
-      <CardHeader>
-        <CardTitle>{t('Details')}</CardTitle>
-      </CardHeader>
-      <CardBody>
-        <DetailsBody>
-          <OverviewDetailItem isLoading={isLoading} title={t('Service name')}>
-            {name}
-          </OverviewDetailItem>
-          <OverviewDetailItem isLoading={isLoading} title={t('Provider')}>
-            {provider}
-          </OverviewDetailItem>
-          <OverviewDetailItem isLoading={isLoading} title={t('OpenShift Virtualization version')}>
-            {version}
-            <div>
-              {catalogSourceMissing ? (
-                <SourceMissingStatus />
-              ) : (
-                <SubscriptionStatus subscription={kubevirtSub} />
-              )}
-            </div>
-          </OverviewDetailItem>
-          <OverviewDetailItem isLoading={isLoading} title={t('Update Channel')}>
-            {updateChannel}
-          </OverviewDetailItem>
-        </DetailsBody>
-      </CardBody>
-    </Card>
+    isAdmin && (
+      <Card data-test-id="kubevirt-overview-dashboard--details-card">
+        <CardHeader>
+          <CardTitle>{t('Details')}</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <DetailsBody>
+            <OverviewDetailItem isLoading={isLoading} title={t('Service name')}>
+              {name}
+            </OverviewDetailItem>
+            <OverviewDetailItem isLoading={isLoading} title={t('Provider')}>
+              {provider}
+            </OverviewDetailItem>
+            <OverviewDetailItem isLoading={isLoading} title={t('OpenShift Virtualization version')}>
+              {version}
+              <div>
+                {catalogSourceMissing ? (
+                  <SourceMissingStatus />
+                ) : (
+                  <SubscriptionStatus subscription={kubevirtSub} />
+                )}
+              </div>
+            </OverviewDetailItem>
+            <OverviewDetailItem isLoading={isLoading} title={t('Update Channel')}>
+              {updateChannel}
+            </OverviewDetailItem>
+          </DetailsBody>
+        </CardBody>
+      </Card>
+    )
   );
 };
 
