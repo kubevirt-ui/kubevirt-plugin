1d3fec0a81c60854708dfa2dc5665b5527f59347
Ugo Palatucci <upalatuc@redhat.com>
2022-07-04 08:52:20 +0000

    Fix top consumer exception

--- src/views/clusteroverview/overview/components/top-consumers-card/utils/TopConsumerCard.tsx
+++ src/views/clusteroverview/overview/components/top-consumers-card/utils/TopConsumerCard.tsx
@@ -52,7 +52,7 @@ const TopConsumerCard: React.FC<TopConsumersMetricCard> = ({ cardID }) => {
             isCheckboxSelectionBadgeHidden
           >
             {TopConsumerMetric.getAll().map((metric) => (
-              <SelectOption key={metric.getValue()} value={t(metric.getDropdownLabel())} />
+              <SelectOption key={metric?.getValue()} value={t(metric?.getDropdownLabel())} />
             ))}
           </FormPFSelect>
         </div>
@@ -60,12 +60,12 @@ const TopConsumerCard: React.FC<TopConsumersMetricCard> = ({ cardID }) => {
           <FormPFSelect
             toggleId="kv-top-consumers-card-scope-select"
             variant={SelectVariant.single}
-            selections={t(TopConsumerScope.fromString(scopeKey).getDropdownLabel())}
+            selections={t(TopConsumerScope.fromString(scopeKey)?.getDropdownLabel())}
             onSelect={(e, value) => onScopeSelect(value)}
             isCheckboxSelectionBadgeHidden
           >
             {TopConsumerScope.getAll().map((scope) => (
-              <SelectOption key={scope.getValue()} value={t(scope.getDropdownLabel())} />
+              <SelectOption key={scope?.getValue()} value={t(scope?.getDropdownLabel())} />
             ))}
           </FormPFSelect>
         </div>
