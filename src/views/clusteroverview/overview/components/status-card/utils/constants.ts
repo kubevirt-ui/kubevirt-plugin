2c1a7ec923ea23b6e56e9a2aedea001e9dcd45a3
Phillip Bailey <phbailey@redhat.com>
2022-07-06 13:30:18 -0400

    Fix ODF status reporting on overview page

--- src/views/clusteroverview/overview/components/status-card/utils/constants.ts
+++ src/views/clusteroverview/overview/components/status-card/utils/constants.ts
@@ -3,7 +3,7 @@ export const AVAILABLE = 'Available';
 export const VIRTUALIZATION = 'Virtualization';
 
 export const LSO_NAME = 'local-storage-operator';
-export const ODF_OPERATOR_NAME = 'odf-operator';
+export const ODF_OPERATOR_NAME = 'odf-csi-addons-operator';
 
 export const COMPONENT_NOT_FOUND_ERROR = 'Component not found';
 
