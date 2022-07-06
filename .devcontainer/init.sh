fb4f38ee7b3d94172d8e0046a880fd6c498f46f0
Gilad Lekner <gilad215@gmail.com>
2022-07-06 18:29:38 +0300

    add validation for i18n in dev.env

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -7,7 +7,9 @@ if [[ ! -f .devcontainer/dev.env ]]
 then
     cat << EOF
     env file 'dev.env' does not exist in .devcontainer, please create it and add the the correct values for your cluster.
-    OC_PLUGIN_NAME=my-plugin
+    OC_PLUGIN_NAME=kubevirt-plugin
+    OC_PLUGIN_I18N_NAMESPACES=plugin__kubevirt-plugin
+
     OC_URL=https://api.example.com:6443
     OC_USER=kubeadmin
     OC_PASS=<password>
@@ -23,13 +25,17 @@ success=1
 ! grep -q OC_URL= ".devcontainer/dev.env" && success=0
 ! grep -q OC_USER= ".devcontainer/dev.env" && success=0
 ! grep -q OC_PASS= ".devcontainer/dev.env" && success=0
+! grep -q OC_PLUGIN_I18N_NAMESPACES= ".devcontainer/dev.env" && success=0
+
 
 if ((success)); then
     echo 'dev.env is formatted correctly, proceeding.'
 else
     cat << EOF
     dev.env is not formatted correctly, please add the the correct values for your cluster.
-    OC_PLUGIN_NAME=my-plugin
+    OC_PLUGIN_NAME=kubevirt-plugin
+    OC_PLUGIN_I18N_NAMESPACES=plugin__kubevirt-plugin
+
     OC_URL=https://api.example.com:6443
     OC_USER=kubeadmin
     OC_PASS=<password>
