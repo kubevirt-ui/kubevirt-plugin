bc05f3793c9bf58f8beb04891d1a2a7eee467276
Aviv Turgeman <aturgema@redhat.com>
2022-07-06 09:34:13 +0000

    Create sub tab 'Top consumers'
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/clusteroverview/overview/components/MonitoringTab/MonitoringTab.tsx
+++ src/views/clusteroverview/overview/components/MonitoringTab/MonitoringTab.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 
-import { Overview } from '@openshift-console/dynamic-plugin-sdk';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { HorizontalNav, NavPage } from '@openshift-console/dynamic-plugin-sdk';
 
 import TopConsumersCard from './top-consumers-card/TopConsumersCard';
 
-const MonitoringTab: React.FC = () => (
-  <Overview>
-    <TopConsumersCard />
-  </Overview>
-);
+const MonitoringTab: React.FC = () => {
+  const { t } = useKubevirtTranslation();
+
+  const MonitoringSubTabs: NavPage[] = [
+    {
+      href: '',
+      name: t('Top consumers'),
+      component: TopConsumersCard,
+    },
+  ];
+  return <HorizontalNav pages={MonitoringSubTabs} />;
+};
 
 export default MonitoringTab;
