76322f97430c6d2a5061c60cf53536410712f429
Phillip Bailey <phbailey@redhat.com>
2022-08-10 09:15:09 -0400

    Create resources inventory card

--- src/views/clusteroverview/OverviewTab/OverviewTab.tsx
+++ src/views/clusteroverview/OverviewTab/OverviewTab.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
+import React from 'react';
 
 import { Overview } from '@openshift-console/dynamic-plugin-sdk';
 import { Grid, GridItem } from '@patternfly/react-core';
 
 import OverviewAlertsCard from './alerts-card/OverviewAlertsCard';
 import GettingStartedCard from './getting-started-card/GettingStartedCard';
+import ResourcesInventoryCard from './resources-inventory-card/ResourcesInventoryCard';
 import RunningVMsPerTemplateCard from './running-vms-per-template-card/RunningVMsPerTemplateCard';
 import VMStatusesCard from './vm-statuses-card/VMStatusesCard';
 
@@ -12,6 +13,7 @@ const OverviewTab: React.FC = () => {
   return (
     <Overview>
       <GettingStartedCard />
+      <ResourcesInventoryCard />
       <OverviewAlertsCard />
       <Grid hasGutter>
         <GridItem span={6}>
---
+++ src/views/clusteroverview/OverviewTab/resources-inventory-card/hooks/useResourcesQuantities.ts
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react';
+
+import {
+  modelToGroupVersionKind,
+  NodeModel,
+  TemplateModel,
+  VirtualMachineModelGroupVersionKind,
+} from '@kubevirt-ui/kubevirt-api/console';
+import { NetworkAttachmentDefinitionModelGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console/models/NetworkAttachmentDefinitionModel';
+import { ALL_NAMESPACES_SESSION_KEY } from '@kubevirt-utils/hooks/constants';
+import { TEMPLATE_TYPE_LABEL } from '@kubevirt-utils/resources/template';
+import { K8sResourceCommon, useK8sWatchResources } from '@openshift-console/dynamic-plugin-sdk';
+import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk-internal';
+
+type UseResourcesQuantities = () => {
+  loaded: boolean;
+  networks: number;
+  nodes: number;
+  vms: number;
+  vmTemplates: number;
+};
+
+const useResourcesQuantities: UseResourcesQuantities = () => {
+  const [activeNamespace] = useActiveNamespace();
+  const namespace = React.useMemo(
+    () => (activeNamespace === ALL_NAMESPACES_SESSION_KEY ? null : activeNamespace),
+    [activeNamespace],
+  );
+
+  const watchedResources = {
+    vms: {
+      groupVersionKind: VirtualMachineModelGroupVersionKind,
+      namespaced: !!namespace,
+      namespace,
+      isList: true,
+    },
+    vmTemplates: {
+      groupVersionKind: modelToGroupVersionKind(TemplateModel),
+      namespaced: !!namespace,
+      namespace,
+      isList: true,
+      selector: {
+        matchExpressions: [
+          {
+            key: TEMPLATE_TYPE_LABEL,
+            operator: 'Exists',
+          },
+        ],
+      },
+    },
+    nodes: {
+      groupVersionKind: modelToGroupVersionKind(NodeModel),
+      namespaced: !!namespace,
+      namespace,
+      isList: true,
+    },
+    nads: {
+      groupVersionKind: NetworkAttachmentDefinitionModelGroupVersionKind,
+      namespaced: !!namespace,
+      namespace,
+      isList: true,
+    },
+  };
+
+  const resources = useK8sWatchResources<{ [key: string]: K8sResourceCommon[] }>(watchedResources);
+
+  return useMemo(() => {
+    return Object.entries(resources).reduce(
+      (acc, [key, value]) => {
+        acc[key] = value?.data?.length;
+        acc.loaded = acc.loaded && value?.loaded;
+        return acc;
+      },
+      { loaded: false as boolean, networks: 0, nodes: 0, vms: 0, vmTemplates: 0 },
+    );
+  }, [resources]);
+};
+
+export default useResourcesQuantities;
