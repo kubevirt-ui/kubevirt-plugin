55d3780673cb32665bc48f5a6ea1207392253483
Hilda Stastna <hstastna@redhat.com>
2022-10-21 16:05:38 +0200

    Bug 2134668: Display correct command with username for SSH
    This is the additional fix for Bug 2134668:
    Get the correct cloud-init credentials even when the VM is stopped
    and generate the ssh command properly (SSH over NodePort).
    Disable the SSH over NodePort checkbox and display the tooltip to inform
    the user in case that there is no username set.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2134668

--- src/utils/components/Consoles/components/CloudInitCredentials/CloudInitCredentialsContent.tsx
+++ src/utils/components/Consoles/components/CloudInitCredentials/CloudInitCredentialsContent.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { Trans } from 'react-i18next';
 
-import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { getCloudInitCredentials } from '@kubevirt-utils/resources/vmi';
+import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { ClipboardCopy } from '@patternfly/react-core';
 
-import { CLOUD_INIT_MISSING_USERNAME } from '../../utils/constants';
-
 type CloudInitCredentialsContentProps = {
-  vmi: V1VirtualMachineInstance;
+  vm: V1VirtualMachine;
 };
 
-const CloudInitCredentialsContent: React.FC<CloudInitCredentialsContentProps> = ({ vmi }) => {
+const CloudInitCredentialsContent: React.FC<CloudInitCredentialsContentProps> = ({ vm }) => {
   const { t } = useKubevirtTranslation();
-  const { user, password } = getCloudInitCredentials(vmi);
+  const { users } = getCloudInitCredentials(vm);
+  const usernameTitle = t('User name: ', { count: users?.length });
 
-  if (!user && !password) {
+  if (isEmpty(users)) {
     return <>{t('No credentials, see operating system documentation for the default username.')}</>;
   }
 
@@ -28,23 +28,28 @@ const CloudInitCredentialsContent: React.FC<CloudInitCredentialsContentProps> = 
         for more information.
       </Trans>
       <div>
-        <strong>{t('User name: ')}</strong>
-        {user || CLOUD_INIT_MISSING_USERNAME}
-
-        {password && (
+        <strong>{usernameTitle}</strong>
+        {users.map((user, index) => (
           <>
-            <strong>{t(' Password: ')} </strong>
+            {user?.name}
+            {index + 1 < users?.length ? ', ' : ''}
 
-            <ClipboardCopy
-              variant="inline-compact"
-              isCode
-              clickTip={t('Copied')}
-              hoverTip={t('Copy to clipboard')}
-            >
-              {password}
-            </ClipboardCopy>
+            {user?.password && (
+              <>
+                <strong>{t(' Password: ')} </strong>
+
+                <ClipboardCopy
+                  variant="inline-compact"
+                  isCode
+                  clickTip={t('Copied')}
+                  hoverTip={t('Copy to clipboard')}
+                >
+                  {user.password}
+                </ClipboardCopy>
+              </>
+            )}
           </>
-        )}
+        ))}
       </div>
     </>
   );
