f9b908e1775abd9b47b62c137636e0f1a97d46b1
Dana Orr <dorr@redhat.com>
2022-11-07 12:54:33 +0200

    Fixing the breadcrumbs list in virtual machine for non admins
    Signed-off-by: Dana Orr <dorr@redhat.com>

--- src/utils/hooks/useLastNamespacePath.ts
+++ src/utils/hooks/useLastNamespacePath.ts
@@ -1,3 +1,5 @@
+import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk-internal';
+
 import {
   ALL_NAMESPACES,
   ALL_NAMESPACES_SESSION_KEY as ALL_NAMESPACES_ACTIVE_KEY,
@@ -9,6 +11,9 @@ export const useLastNamespacePath: UseActiveNamespacePathType = () => {
   const lastNamespace = JSON.parse(localStorage.getItem('console-user-settings'))?.[
     'console.lastNamespace'
   ];
-
+  const activeNamespace = useActiveNamespace();
+  if (!lastNamespace) {
+    return `ns/${activeNamespace}`;
+  }
   return lastNamespace === ALL_NAMESPACES_ACTIVE_KEY ? ALL_NAMESPACES : `ns/${lastNamespace}`;
 };
