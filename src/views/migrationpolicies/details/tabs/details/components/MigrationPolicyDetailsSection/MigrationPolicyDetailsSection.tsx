eb2860e8c5fadbfeaf6c4aa630f1ff62809210f0
Hilda Stastna <hstastna@redhat.com>
2023-01-05 20:08:58 +0100

    Bug 2148850: Display help texts in MigrationPolicy details
    Display help texts for the fields in MigrationPolicy details page.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2148850

--- src/views/migrationpolicies/details/tabs/details/components/MigrationPolicyDetailsSection/MigrationPolicyDetailsSection.tsx
+++ src/views/migrationpolicies/details/tabs/details/components/MigrationPolicyDetailsSection/MigrationPolicyDetailsSection.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Trans } from 'react-i18next';
 
 import { V1alpha1MigrationPolicy } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
@@ -12,6 +13,7 @@ import {
   FlexItem,
   Grid,
   GridItem,
+  Popover,
   Title,
 } from '@patternfly/react-core';
 import { LinkIcon, PencilAltIcon } from '@patternfly/react-icons';
@@ -54,7 +56,16 @@ const MigrationPolicyDetailsSection: React.FC<MigrationPolicyDetailsSectionProps
       <Grid hasGutter>
         <GridItem span={5}>
           <DescriptionList>
-            <MigrationPolicyDescriptionItem title={t('Name')} description={mp?.metadata?.name} />
+            <MigrationPolicyDescriptionItem
+              title={t('Name')}
+              description={mp?.metadata?.name}
+              isPopover
+              popoverContent={t(
+                'Name must be unique within a namespace. Is required when creating resources, although some resources may allow a client to request the generation of an appropriate name automatically. Name is primarily intended for creation idempotence and configuration definition. ',
+              )}
+              moreInfoURL="http://kubernetes.io/docs/user-guide/identifiers#names"
+              breadcrumb="MigrationPolicy.metadata.name"
+            />
             <MigrationPolicyDescriptionItem
               title={t('Description')}
               description={mp?.metadata?.annotations?.description}
@@ -85,6 +96,11 @@ const MigrationPolicyDetailsSection: React.FC<MigrationPolicyDetailsSectionProps
                         ? getBandwidthPerMigrationText(mp?.spec?.bandwidthPerMigration)
                         : NO_DATA_DASH
                     }
+                    isPopover
+                    popoverContent={t(
+                      'BandwidthPerMigration limits the amount of network bandwith live migrations are allowed to use. The value is in quantity per second. Defaults to 0 (no limit). ',
+                    )}
+                    moreInfoURL="http://kubevirt.io/api-reference/main/definitions.html#_v1_migrationconfiguration"
                   />
                   <MigrationPolicyDescriptionItem
                     title={t('Auto converge')}
@@ -93,6 +109,11 @@ const MigrationPolicyDetailsSection: React.FC<MigrationPolicyDetailsSectionProps
                         ? getBooleanText(mp?.spec?.allowAutoConverge)
                         : NO_DATA_DASH
                     }
+                    isPopover
+                    popoverContent={t(
+                      'AllowAutoConverge allows the platform to compromise performance/availability of VMIs to guarantee successful VMI live migrations. Defaults to false. ',
+                    )}
+                    moreInfoURL="http://kubevirt.io/api-reference/main/definitions.html#_v1_migrationconfiguration"
                   />
                   <MigrationPolicyDescriptionItem
                     title={t('Post-copy')}
@@ -101,6 +122,11 @@ const MigrationPolicyDetailsSection: React.FC<MigrationPolicyDetailsSectionProps
                         ? getBooleanText(mp?.spec?.allowPostCopy)
                         : NO_DATA_DASH
                     }
+                    isPopover
+                    popoverContent={t(
+                      'AllowPostCopy enables post-copy live migrations. Such migrations allow even the busiest VMIs to successfully live-migrate. However, events like a network failure can cause a VMI crash. If set to true, migrations will still start in pre-copy, but switch to post-copy when CompletionTimeoutPerGiB triggers. Defaults to false. ',
+                    )}
+                    moreInfoURL="http://kubevirt.io/api-reference/main/definitions.html#_v1_migrationconfiguration"
                   />
                   <MigrationPolicyDescriptionItem
                     title={t('Completion timeout')}
@@ -109,11 +135,33 @@ const MigrationPolicyDetailsSection: React.FC<MigrationPolicyDetailsSectionProps
                         ? getCompletionTimeoutText(mp?.spec?.completionTimeoutPerGiB)
                         : NO_DATA_DASH
                     }
+                    isPopover
+                    popoverContent={t(
+                      'CompletionTimeoutPerGiB is the maximum number of seconds per GiB a migration is allowed to take. If a live-migration takes longer to migrate than this value multiplied by the size of the VMI, the migration will be cancelled, unless AllowPostCopy is true. Defaults to 800. ',
+                    )}
+                    moreInfoURL="http://kubevirt.io/api-reference/main/definitions.html#_v1_migrationconfiguration"
                   />
                 </DescriptionList>
               </DescriptionListDescription>
             </>
-            <Title headingLevel="h2">{t('Labels')}</Title>
+
+            <Title headingLevel="h2">
+              <Popover
+                headerContent={t('Labels')}
+                bodyContent={
+                  <Trans ns="plugin__kubevirt-plugin">
+                    Map of string keys and values that can be used to organize and categorize (scope
+                    and select) objects. May match selectors of replication controllers and
+                    services. More info:{' '}
+                    <a href="http://kubernetes.io/docs/user-guide/labels">
+                      http://kubernetes.io/docs/user-guide/labels
+                    </a>
+                  </Trans>
+                }
+              >
+                <DescriptionListTermHelpTextButton>{t('Labels')}</DescriptionListTermHelpTextButton>
+              </Popover>
+            </Title>
             <MigrationPolicyProjectLabels mp={mp} />
             <MigrationPolicyVirtualMachineLabels mp={mp} />
           </DescriptionList>
--- src/views/migrationpolicies/details/tabs/details/components/MigrationPolicyDetailsSection/components/MigrationPolicyDescriptionItem.tsx
+++ src/views/migrationpolicies/details/tabs/details/components/MigrationPolicyDetailsSection/components/MigrationPolicyDescriptionItem.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 
+import { DescriptionItemHeader } from '@kubevirt-utils/components/DescriptionItem/DescriptionItemHeader';
 import { NO_DATA_DASH } from '@kubevirt-utils/resources/vm/utils/constants';
 import {
   DescriptionListDescription,
   DescriptionListGroup,
   DescriptionListTermHelpText,
-  DescriptionListTermHelpTextButton,
 } from '@patternfly/react-core';
 
 type MigrationPolicyDescriptionItemProps = {
   title: string;
-  description: React.ReactNode;
+  description?: React.ReactNode;
+  isPopover?: boolean;
+  popoverContent?: React.ReactNode;
+  moreInfoURL?: string;
+  breadcrumb?: string;
+  hasAutoWidth?: boolean;
 };
 
 const MigrationPolicyDescriptionItem: React.FC<MigrationPolicyDescriptionItemProps> = ({
   title,
   description,
+  isPopover,
+  popoverContent,
+  moreInfoURL,
+  breadcrumb,
 }) => (
   <DescriptionListGroup>
     <DescriptionListTermHelpText>
-      <DescriptionListTermHelpTextButton>{title}</DescriptionListTermHelpTextButton>
+      <DescriptionItemHeader
+        isPopover={isPopover}
+        bodyContent={popoverContent}
+        moreInfoURL={moreInfoURL}
+        breadcrumb={breadcrumb}
+        descriptionHeader={title}
+      />
     </DescriptionListTermHelpText>
     <DescriptionListDescription>{description || NO_DATA_DASH}</DescriptionListDescription>
   </DescriptionListGroup>
