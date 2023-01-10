3796054efc0342f31a5d0dac8b9637f2f9d87a07
Hilda Stastna <hstastna@redhat.com>
2023-01-10 16:17:16 +0100

    CNV-21086: Add 'Not migratable' badge to VM title
    Add 'Not migratable' badge next to the status of the VM title,
    to clearly mark the VM that isn't live migratable.
    
    This change follows the very recent design changes.
    
    Fixes: https://issues.redhat.com/browse/CNV-21086

--- src/views/virtualmachines/details/VirtualMachineNavPageTitle.tsx
+++ src/views/virtualmachines/details/VirtualMachineNavPageTitle.tsx
@@ -5,9 +5,10 @@ import { V1VirtualMachine, V1VirtualMachineInstance } from '@kubevirt-ui/kubevir
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import useSingleNodeCluster from '@kubevirt-utils/hooks/useSingleNodeCluster';
 import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
-import { Label } from '@patternfly/react-core';
+import { Label, Split, SplitItem } from '@patternfly/react-core';
 import VirtualMachineActions from '@virtualmachines/actions/components/VirtualMachineActions/VirtualMachineActions';
 import useVirtualMachineInstanceMigration from '@virtualmachines/actions/hooks/useVirtualMachineInstanceMigration';
+import VMNotMigratableBadge from '@virtualmachines/list/components/VMNotMigratableBadge/VMNotMigratableBadge';
 
 import VirtualMachineBreadcrumb from '../list/components/VirtualMachineBreadcrumb/VirtualMachineBreadcrumb';
 import { getVMStatusIcon } from '../utils';
@@ -34,16 +35,22 @@ const VirtualMachineNavPageTitle: React.FC<VirtualMachineNavPageTitleProps> = ({
   const [isSingleNodeCluster] = useSingleNodeCluster();
 
   const StatusIcon = getVMStatusIcon(vm?.status?.printableStatus);
+
   return (
     <div className="co-m-nav-title co-m-nav-title--detail">
       <VirtualMachineBreadcrumb />
       <span className="co-m-pane__heading">
         <h1 className="co-resource-item__resource-name">
-          <span className="co-m-resource-icon co-m-resource-icon--lg">{t('VM')}</span>
-          {name}{' '}
-          <Label isCompact icon={<StatusIcon />} className="vm-resource-label">
-            {vm?.status?.printableStatus}
-          </Label>
+          <Split hasGutter>
+            <SplitItem>
+              <span className="co-m-resource-icon co-m-resource-icon--lg">{t('VM')}</span>
+              {name}{' '}
+              <Label isCompact icon={<StatusIcon />} className="vm-resource-label">
+                {vm?.status?.printableStatus}
+              </Label>
+            </SplitItem>
+            <VMNotMigratableBadge vm={vm} />
+          </Split>
         </h1>
         <VirtualMachineActions vm={vm} vmim={vmim} isSingleNodeCluster={isSingleNodeCluster} />
       </span>
