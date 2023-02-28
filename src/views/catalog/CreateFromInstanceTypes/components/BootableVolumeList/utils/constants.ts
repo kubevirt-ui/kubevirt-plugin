3e7054868f7e4b8c20372042dcee5cdf53c52b98
Aviv Turgeman <aturgema@redhat.com>
2023-02-28 17:35:30 +0200

    Enable column sorting for bootable volumes list
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/catalog/CreateFromInstanceTypes/components/BootableVolumeList/utils/constants.ts
+++ src/views/catalog/CreateFromInstanceTypes/components/BootableVolumeList/utils/constants.ts
@@ -1,22 +1,31 @@
+import { PerPageOptions } from '@patternfly/react-core';
+
 export const paginationDefaultValuesForm = [
   { title: '5', value: 5 },
   { title: '10', value: 10 },
 ];
 
-export const paginationDefaultValuesModal = [
+export const paginationDefaultValuesModal: PerPageOptions[] = [
   { title: '15', value: 15 },
   { title: '30', value: 30 },
   { title: '50', value: 50 },
 ];
 
-export const paginationInitialStateForm = {
+export type PaginationState = {
+  page: number;
+  perPage: number;
+  startIndex: number;
+  endIndex: number;
+};
+
+export const paginationInitialStateForm: PaginationState = {
   page: 1,
   perPage: 5,
   startIndex: 0,
   endIndex: 5,
 };
 
-export const paginationInitialStateModal = {
+export const paginationInitialStateModal: PaginationState = {
   page: 1,
   perPage: 15,
   startIndex: 0,
