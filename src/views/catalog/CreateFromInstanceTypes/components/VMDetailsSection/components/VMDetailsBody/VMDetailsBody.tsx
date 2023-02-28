abfb7d1fa60d00c51d4e684430d63e28b7466eb5
Aviv Turgeman <aturgema@redhat.com>
2023-02-28 16:40:34 +0200

    Create VM from boot source with correct SC
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/components/VMDetailsBody/VMDetailsBody.tsx
+++ src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/components/VMDetailsBody/VMDetailsBody.tsx
@@ -2,6 +2,7 @@ import React, { Dispatch, SetStateAction } from 'react';
 
 import { InstanceTypeState } from '@catalog/CreateFromInstanceTypes/utils/constants';
 import { V1beta1DataSource } from '@kubevirt-ui/kubevirt-api/containerized-data-importer/models';
+import { V1alpha1PersistentVolumeClaim } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { Grid, GridItem } from '@patternfly/react-core';
 
 import DetailsLeftGrid from './components/DetailsLeftGrid';
@@ -15,6 +16,7 @@ type VMDetailsBodyProps = {
   namespace: string;
   bootSource: V1beta1DataSource;
   instancetype: InstanceTypeState;
+  pvcSource: V1alpha1PersistentVolumeClaim;
 };
 
 const VMDetailsBody: React.FC<VMDetailsBodyProps> = ({
@@ -23,6 +25,7 @@ const VMDetailsBody: React.FC<VMDetailsBodyProps> = ({
   namespace,
   bootSource,
   instancetype,
+  pvcSource,
 }) => (
   <div className="instancetypes-vm-details-body">
     <Grid hasGutter>
@@ -36,7 +39,7 @@ const VMDetailsBody: React.FC<VMDetailsBodyProps> = ({
       </GridItem>
       <GridItem span={1}>{/* Spacer */}</GridItem>
       <GridItem span={3}>
-        <DetailsRightGrid bootSource={bootSource} namespace={namespace} />
+        <DetailsRightGrid pvcSource={pvcSource} namespace={namespace} />
       </GridItem>
     </Grid>
   </div>
--- src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/components/VMDetailsBody/components/DetailsRightGrid.tsx
+++ src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/components/VMDetailsBody/components/DetailsRightGrid.tsx
@@ -1,34 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { V1beta1DataSource } from '@kubevirt-ui/kubevirt-api/containerized-data-importer/models';
 import { V1alpha1PersistentVolumeClaim } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { t } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { getPVC } from '@kubevirt-utils/resources/template/hooks/useVmTemplateSource/utils';
 import { DescriptionList } from '@patternfly/react-core';
 import VirtualMachineDescriptionItem from '@virtualmachines/details/tabs/details/components/VirtualMachineDescriptionItem/VirtualMachineDescriptionItem';
 
 import { humanizeBinaryBytes } from '../../../../../../../../utils/utils/humanize';
 
 type DetailsRightGridProps = {
-  bootSource: V1beta1DataSource;
+  pvcSource: V1alpha1PersistentVolumeClaim;
   namespace: string;
 };
 
-const DetailsRightGrid: React.FC<DetailsRightGridProps> = ({ bootSource, namespace }) => {
-  const [pvcSource, setPVCSource] = useState<V1alpha1PersistentVolumeClaim>();
-
-  const getPVCSource = async () => {
-    if (!bootSource) return;
-
-    const pvcData = bootSource?.spec?.source?.pvc;
-    const pvc = await getPVC(pvcData?.name, pvcData?.namespace);
-    setPVCSource(pvc);
-  };
-  getPVCSource().catch(console.log);
-
+const DetailsRightGrid: React.FC<DetailsRightGridProps> = ({ pvcSource, namespace }) => {
   const pvcDiskSize = pvcSource?.spec?.resources?.requests?.storage;
   const sizeData = humanizeBinaryBytes(pvcDiskSize);
-  const storageClassName = pvcSource?.spec?.storageClassName;
 
   return (
     <DescriptionList isHorizontal isFluid>
@@ -38,7 +24,7 @@ const DetailsRightGrid: React.FC<DetailsRightGridProps> = ({ bootSource, namespa
         descriptionHeader={t('Boot disk size')}
       />
       <VirtualMachineDescriptionItem
-        descriptionData={storageClassName}
+        descriptionData={pvcSource?.spec?.storageClassName}
         descriptionHeader={t('Storage class')}
       />
     </DescriptionList>
