ba52402b6d54040133c43b9a39d9d4c2658ace06
Hilda Stastna <hstastna@redhat.com>
2023-04-13 22:48:10 +0200

    Bug 2186592: Display titles in VMI Disks tab in correct size
    Display VirtualMachineInstance's "Disks" and "File systems" in Disks tab
    smaller as they were, so they are more consistent with titles in other
    tabs.
    
    Additionally, place both "?" icons to their correct position -
    - to the left and next to the titles, and make them displayed enabled,
    not greyed out and disabled, as they were before this change,
    so the user can see them easily and click on them.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2186592

--- src/utils/components/DiskListTitle/DiskListTitle.tsx
+++ src/utils/components/DiskListTitle/DiskListTitle.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import HelpTextIcon from '@kubevirt-utils/components/HelpTextIcon/HelpTextIcon';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
