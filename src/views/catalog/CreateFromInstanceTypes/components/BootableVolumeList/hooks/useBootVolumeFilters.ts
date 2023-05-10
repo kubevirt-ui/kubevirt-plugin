1e1034af51558cd16ea07d2607710749e6d8e584
Aviv Turgeman <aturgema@redhat.com>
2023-05-10 17:32:59 +0300

    CNV-27327: Add resource kind filter
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/views/catalog/CreateFromInstanceTypes/components/BootableVolumeList/hooks/useBootVolumeFilters.ts
+++ src/views/catalog/CreateFromInstanceTypes/components/BootableVolumeList/hooks/useBootVolumeFilters.ts
@@ -1,4 +1,6 @@
 import { BootableVolume } from '@catalog/CreateFromInstanceTypes/utils/types';
+import { PersistentVolumeClaimModel } from '@kubevirt-ui/kubevirt-api/console';
+import DataSourceModel from '@kubevirt-ui/kubevirt-api/console/models/DataSourceModel';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { OS_NAMES } from '@kubevirt-utils/resources/template';
 import { getItemNameWithOther } from '@kubevirt-utils/utils/utils';
@@ -6,20 +8,37 @@ import { RowFilter } from '@openshift-console/dynamic-plugin-sdk';
 
 import { getBootVolumeOS } from '../utils/utils';
 
-const useBootVolumeFilters = (type?: string): RowFilter<BootableVolume>[] => {
+const useBootVolumeFilters = (isModal: boolean): RowFilter<BootableVolume>[] => {
   const { t } = useKubevirtTranslation();
 
   return [
     {
       filterGroupName: t('Operating system'),
-      type,
+      type: `osName${isModal && '-modal'}`,
       reducer: (obj) => getItemNameWithOther(getBootVolumeOS(obj), OS_NAMES),
       filter: (availableOsNames, obj) =>
         availableOsNames?.selected?.length === 0 ||
         availableOsNames?.selected?.includes(getItemNameWithOther(getBootVolumeOS(obj), OS_NAMES)),
       items: OS_NAMES,
     },
+    {
+      filterGroupName: t('Resource'),
+      type: `resourceKind${isModal && '-modal'}`,
+      reducer: (obj) => obj?.kind,
+      filter: (availableResourceNames, obj) =>
+        availableResourceNames?.selected?.length === 0 ||
+        availableResourceNames?.selected?.includes(obj?.kind),
+      items: [
+        {
+          id: PersistentVolumeClaimModel.kind,
+          title: PersistentVolumeClaimModel.abbr,
+        },
+        {
+          id: DataSourceModel.kind,
+          title: 'DS',
+        },
+      ],
+    },
   ];
 };
-
 export default useBootVolumeFilters;
