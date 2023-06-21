fa3c3cddefd44b1cad3e463205415b8d6b19bb7c
Hilda Stastna <hstastna@redhat.com>
2023-06-21 16:59:49 +0200

    Bug 2190448: Disable Resume, Pause buttons on stopped VM in Overview
    Disable unnecessary Resume and Pause buttons on Details card present
    in a VM's Overview page for a stopped VM. Display Stop button (disabled
    for a stopped VM) as it was missing, hidden in the kebab menu
    in the Details card.
    
    Fixes https://bugzilla.redhat.com/show_bug.cgi?id=2190448

--- src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsTitle.tsx
+++ src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsTitle.tsx
@@ -1,5 +1,7 @@
-import * as React from 'react';
+import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { pauseVM, startVM, stopVM, unpauseVM } from 'src/views/virtualmachines/actions/actions';
+import { printableVMStatus } from 'src/views/virtualmachines/utils';
 
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { getConsoleVirtctlCommand } from '@kubevirt-utils/components/SSHAccess/utils';
@@ -8,18 +10,16 @@ import { getCloudInitCredentials } from '@kubevirt-utils/resources/vmi';
 import { CardTitle, Dropdown, DropdownItem, KebabToggle } from '@patternfly/react-core';
 import { CopyIcon } from '@patternfly/react-icons';
 
-import { pauseVM, startVM, stopVM, unpauseVM } from '../../../../../../actions/actions';
-import { printableVMStatus } from '../../../../../../utils';
 import { createURL } from '../../../utils/utils';
 
 type VirtualMachinesOverviewTabDetailsTitleProps = {
   vm: V1VirtualMachine;
 };
 
-const VirtualMachinesOverviewTabDetailsTitle: React.FC<
-  VirtualMachinesOverviewTabDetailsTitleProps
-> = ({ vm }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = React.useState(false);
+const VirtualMachinesOverviewTabDetailsTitle: FC<VirtualMachinesOverviewTabDetailsTitleProps> = ({
+  vm,
+}) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const { t } = useKubevirtTranslation();
   const userName = getCloudInitCredentials(vm)?.users?.[0]?.name;
   const virtctlCommand = getConsoleVirtctlCommand(
@@ -47,9 +47,13 @@ const VirtualMachinesOverviewTabDetailsTitle: React.FC<
             </span>
           </DropdownItem>,
           <DropdownItem key="stop" onClick={() => (isMachineStopped ? startVM(vm) : stopVM(vm))}>
-            {isMachineStopped ? t('Resume VirtualMachine') : t('Stop VirtualMachine')}
+            {isMachineStopped ? t('Start VirtualMachine') : t('Stop VirtualMachine')}
           </DropdownItem>,
-          <DropdownItem key="pause" onClick={() => (isMachinePaused ? unpauseVM(vm) : pauseVM(vm))}>
+          <DropdownItem
+            isDisabled={isMachineStopped}
+            key="pause"
+            onClick={() => (isMachinePaused ? unpauseVM(vm) : pauseVM(vm))}
+          >
             {isMachinePaused ? t('Unpause VirtualMachine') : t('Pause VirtualMachine')}
           </DropdownItem>,
         ]}
