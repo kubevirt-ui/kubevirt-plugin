1b98660e283eca480381e371677a6fa5ac72db11
Aviv Turgeman <aturgema@redhat.com>
2023-06-21 15:40:47 +0300

    CNV-23135: Authorized SSH Key Managment - part 2
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

---
+++ src/views/clusteroverview/SettingsTab/UserTab/components/ManageSSHKeySection/ProjectSSHKeysList/components/AddProjectAuthKeyButton/AddProjectAuthKeyButton.tsx
@@ -0,0 +1,62 @@
+import React, { FC } from 'react';
+
+import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
+import SSHSecretModal from '@kubevirt-utils/components/SSHSecretSection/SSHSecretModal';
+import { initialSSHCredentials } from '@kubevirt-utils/components/SSHSecretSection/utils/constants';
+import {
+  SecretSelectionOption,
+  SSHSecretDetails,
+} from '@kubevirt-utils/components/SSHSecretSection/utils/types';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { isEmpty } from '@kubevirt-utils/utils/utils';
+import { Button } from '@patternfly/react-core';
+import { PencilAltIcon } from '@patternfly/react-icons';
+
+type AddProjectAuthKeyButtonProps = {
+  onSubmit: (sshDetails: SSHSecretDetails) => Promise<any>;
+  secretName: string;
+  selectedProject: string;
+};
+
+const AddProjectAuthKeyButton: FC<AddProjectAuthKeyButtonProps> = ({
+  onSubmit,
+  secretName,
+  selectedProject,
+}) => {
+  const { t } = useKubevirtTranslation();
+
+  const { createModal } = useModal();
+  return (
+    <Button
+      onClick={() =>
+        createModal((modalProps) => (
+          <SSHSecretModal
+            initialSSHSecretDetails={
+              isEmpty(secretName)
+                ? initialSSHCredentials
+                : {
+                    applyKeyToProject: true,
+                    secretOption: SecretSelectionOption.useExisting,
+                    sshPubKey: '',
+                    sshSecretName: secretName,
+                  }
+            }
+            namespace={selectedProject}
+            onSubmit={onSubmit}
+            {...modalProps}
+            isUserTab
+          />
+        ))
+      }
+      className="project-ssh-row__secret-name"
+      isDisabled={isEmpty(selectedProject)}
+      isInline
+      variant="link"
+    >
+      {isEmpty(secretName) ? t('Not configured') : secretName}{' '}
+      <PencilAltIcon className="co-icon-space-l pf-c-button-icon--plain" />
+    </Button>
+  );
+};
+
+export default AddProjectAuthKeyButton;
