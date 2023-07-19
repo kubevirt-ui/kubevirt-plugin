4a51601aafbc764283437b087772b197ecc44571
Matan Schatzman <mschatzm@redhat.com>
2023-07-19 17:02:53 +0300

    CNV-31162: fixing cpu queries
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/clusteroverview/TopConsumersTab/utils/queries.ts
+++ src/views/clusteroverview/TopConsumersTab/utils/queries.ts
@@ -4,7 +4,7 @@ import { TopConsumerScope as Scope } from './topConsumerScope';
 const topConsumerQueries = {
   [Scope.NODE.getValue()]: {
     [Metric.CPU.getValue()]: (numItemsToShow, duration) =>
-      `sort_desc(topk(${numItemsToShow}, sum(rate(container_cpu_usage_seconds_total{container="compute",pod=~"virt-launcher-.*"}[${duration}])) by (node))) > 0`,
+      `sort_desc(topk(${numItemsToShow}, sum(rate(kubevirt_vmi_cpu_usage_seconds[${duration}])) by (node))) > 0`,
     [Metric.MEMORY_SWAP_TRAFFIC.getValue()]: (numItemsToShow) =>
       `sort_desc(topk(${numItemsToShow}, sum(kubevirt_vmi_memory_swap_in_traffic_bytes_total + kubevirt_vmi_memory_swap_out_traffic_bytes_total) by (node))) > 0`,
     [Metric.MEMORY.getValue()]: (numItemsToShow) =>
@@ -18,7 +18,7 @@ const topConsumerQueries = {
   },
   [Scope.PROJECT.getValue()]: {
     [Metric.CPU.getValue()]: (numItemsToShow, duration) =>
-      `sort_desc(topk(${numItemsToShow}, sum(rate(container_cpu_usage_seconds_total{container="compute",pod=~"virt-launcher-.*"}[${duration}])) by (namespace))) > 0`,
+      `sort_desc(topk(${numItemsToShow}, sum(rate(kubevirt_vmi_cpu_usage_seconds[${duration}])) by (namespace))) > 0`,
     [Metric.MEMORY_SWAP_TRAFFIC.getValue()]: (numItemsToShow) =>
       `sort_desc(topk(${numItemsToShow}, sum(kubevirt_vmi_memory_swap_in_traffic_bytes_total + kubevirt_vmi_memory_swap_out_traffic_bytes_total) by (namespace))) > 0`,
     [Metric.MEMORY.getValue()]: (numItemsToShow) =>
@@ -32,7 +32,7 @@ const topConsumerQueries = {
   },
   [Scope.VM.getValue()]: {
     [Metric.CPU.getValue()]: (numItemsToShow, duration) =>
-      `sort_desc(topk(${numItemsToShow}, sum(rate(container_cpu_usage_seconds_total{container="compute",pod=~"virt-launcher-.*"}[${duration}])) by (pod, namespace, label_vm_kubevirt_io_name))) + on (pod, namespace) group_left(label_vm_kubevirt_io_name) (0 * topk by (pod,namespace) ( 1,kube_pod_labels{pod=~".*virt-launcher.*"} )) > 0`,
+      `sort_desc(topk(${numItemsToShow}, sum(rate(kubevirt_vmi_cpu_usage_seconds[${duration}])) BY (name, namespace)))`,
     [Metric.MEMORY_SWAP_TRAFFIC.getValue()]: (numItemsToShow, duration) =>
       `sort_desc(topk (${numItemsToShow}, sum(rate(kubevirt_vmi_memory_swap_in_traffic_bytes_total[${duration}]) + rate(kubevirt_vmi_memory_swap_out_traffic_bytes_total[${duration}]))by(name, namespace))) > 0`,
     [Metric.MEMORY.getValue()]: (numItemsToShow) =>
