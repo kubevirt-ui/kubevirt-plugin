d40ca2e29db6a7a5d9fd7a105dc7c214b9bcc392
Matan Schatzman <mschatzm@redhat.com>
2023-08-08 11:27:15 +0300

    Update fetch query filters
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/hooks/useKubevirtDataPod/useKubevirtDataPodFilters.ts
+++ src/utils/hooks/useKubevirtDataPod/useKubevirtDataPodFilters.ts
@@ -1,35 +1,31 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { debounce } from '@kubevirt-utils/utils/debounce';
+import { isEmpty } from '@kubevirt-utils/utils/utils';
 import useQuery from '@virtualmachines/details/tabs/metrics/NetworkCharts/hook/useQuery';
 
 import useDeepCompareMemoize from '../useDeepCompareMemoize/useDeepCompareMemoize';
 
 type UseKubevirtDataPodFilters = (filters: { [key: string]: string }) => any;
 
+const updateQueryString = (setQueryString) =>
+  debounce((url) => setQueryString((prevQuery) => (prevQuery !== url ? url : prevQuery)), 700);
+
 const useKubevirtDataPodFilters: UseKubevirtDataPodFilters = (filters) => {
   const [queryString, setQueryString] = useState<string>('');
   const query = useQuery();
-  const filtersCompared = useDeepCompareMemoize(filters, true);
-  const updateQueryString = useMemo(
-    () =>
-      debounce(
-        (url) =>
-          setQueryString((prevQuery) =>
-            prevQuery !== url?.toString() ? url?.toString() : prevQuery,
-          ),
-        700,
-      ),
-    [],
-  );
+  const filtersCompared = useDeepCompareMemoize(filters);
+
   useEffect(() => {
     const url = new URLSearchParams();
     for (const [key, value] of query.entries()) {
       const valueReplaced = value === 'No InstanceType' || value === 'None' ? null : value;
       filtersCompared?.[key] && url.set(filtersCompared?.[key], valueReplaced);
     }
-    updateQueryString(url);
-  }, [filtersCompared, query, updateQueryString]);
+    const urlString = url.toString();
+
+    (!isEmpty(urlString) || !isEmpty(queryString)) && updateQueryString(setQueryString)(urlString);
+  }, [filtersCompared, query, queryString]);
 
   return queryString;
 };
