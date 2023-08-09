5ec7c9bdd6b61973680eafed3355c35ecd0fb048
Dana Orr <dorr@redhat.com>
2023-08-09 11:28:24 +0300

    Fixing warnings and behaviour
    Signed-off-by: Dana Orr <dorr@redhat.com>

--- src/utils/resources/vm/utils/source.ts
+++ src/utils/resources/vm/utils/source.ts
@@ -65,6 +65,15 @@ export const getVMBootSourceType = (vm: V1VirtualMachine): TemplateBootSource =>
     };
   }
 
+  if (volume?.persistentVolumeClaim) {
+    return {
+      source: {
+        pvc: { name: volume?.persistentVolumeClaim.claimName, namespace: vm?.metadata?.namespace },
+      },
+      type: BOOT_SOURCE.PVC,
+    };
+  }
+
   return { type: BOOT_SOURCE.NONE, source: {} };
 };
 
