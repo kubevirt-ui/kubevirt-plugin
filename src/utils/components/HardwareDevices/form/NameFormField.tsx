bf0bfc7e7729496fdc0b983c0b743b6a31a30861
Matan Schatzman <mschatzm@redhat.com>
2023-09-12 15:10:03 +0300

    CNV-32869: Devices modal - name auto-populated and not required
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/components/HardwareDevices/form/NameFormField.tsx
+++ src/utils/components/HardwareDevices/form/NameFormField.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Dispatch, FC, SetStateAction } from 'react';
 
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { FormGroup, GridItem, TextInput } from '@patternfly/react-core';
@@ -6,16 +6,16 @@ import { FormGroup, GridItem, TextInput } from '@patternfly/react-core';
 type NameFormFieldProps = {
   index: number;
   name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
+  setName: Dispatch<SetStateAction<string>>;
 };
 
-const NameFormField: React.FC<NameFormFieldProps> = ({ index, name, setName }) => {
+const NameFormField: FC<NameFormFieldProps> = ({ index, name, setName }) => {
   const { t } = useKubevirtTranslation();
 
   return (
     <>
       <GridItem span={5}>
-        <FormGroup fieldId="name" isRequired label={!index && t('Name')}>
+        <FormGroup fieldId="name" label={!index && t('Name')}>
           <TextInput id="name" onChange={setName} type="text" value={name} />
         </FormGroup>
       </GridItem>
--- src/utils/components/HardwareDevices/utils/helpers.ts
+++ src/utils/components/HardwareDevices/utils/helpers.ts
@@ -1,11 +1,22 @@
+import { adjectives, animals, uniqueNamesGenerator } from 'unique-names-generator';
+
 import { V1GPU, V1HostDevice } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
 
-import { HardwareDeviceModalRow } from './constants';
+import { HARDWARE_DEVICE_TYPE, HardwareDeviceModalRow } from './constants';
 
 export const getInitialDevices = (
   initialDevices: V1GPU[] | V1HostDevice[],
+  type: HARDWARE_DEVICE_TYPE,
 ): HardwareDeviceModalRow[] =>
-  (!isEmpty(initialDevices) ? initialDevices : [{ deviceName: '', name: '' }]).map(
-    (device, deviceIndex) => ({ ...device, deviceIndex }),
-  );
+  (!isEmpty(initialDevices)
+    ? initialDevices
+    : [{ deviceName: '', name: generateDeviceName(type) }]
+  ).map((device, deviceIndex) => ({ ...device, deviceIndex }));
+
+export const generateDeviceName = (type: HARDWARE_DEVICE_TYPE): string => {
+  return `${type}-${uniqueNamesGenerator({
+    dictionaries: [adjectives, animals],
+    separator: '-',
+  })}`;
+};
