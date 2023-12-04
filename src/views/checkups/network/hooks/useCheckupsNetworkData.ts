7db738c5a40c6a2298cbd2eff88e32b477541210
Matan Schatzman <mschatzm@redhat.com>
2023-12-04 15:03:39 +0200

    Change checkups data  hook source
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/checkups/network/hooks/useCheckupsNetworkData.ts
+++ src/views/checkups/network/hooks/useCheckupsNetworkData.ts
@@ -5,8 +5,7 @@ import {
 } from '@kubevirt-ui/kubevirt-api/console';
 import { IoK8sApiBatchV1Job, IoK8sApiCoreV1ConfigMap } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import { ALL_NAMESPACES_SESSION_KEY } from '@kubevirt-utils/hooks/constants';
-import useKubevirtWatchResource from '@kubevirt-utils/hooks/useKubevirtWatchResource';
-import { useActiveNamespace } from '@openshift-console/dynamic-plugin-sdk';
+import { useActiveNamespace, useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
 import { KUBEVIRT_VM_LATENCY_LABEL } from '../../utils/utils';
 import { KUBEVIRT_VM_LATENCY_LABEL_VALUE } from '../utils/utils';
@@ -14,7 +13,7 @@ import { KUBEVIRT_VM_LATENCY_LABEL_VALUE } from '../utils/utils';
 const useCheckupsNetworkData = () => {
   const [namespace] = useActiveNamespace();
 
-  const [configMaps, loadingConfigMap, loadErrorConfigMaps] = useKubevirtWatchResource<
+  const [configMaps, loadingConfigMap, loadErrorConfigMaps] = useK8sWatchResource<
     IoK8sApiCoreV1ConfigMap[]
   >({
     groupVersionKind: modelToGroupVersionKind(ConfigMapModel),
@@ -27,7 +26,7 @@ const useCheckupsNetworkData = () => {
     },
   });
 
-  const [jobs, loadingJobs, loadErrorJobs] = useKubevirtWatchResource<IoK8sApiBatchV1Job[]>({
+  const [jobs, loadingJobs, loadErrorJobs] = useK8sWatchResource<IoK8sApiBatchV1Job[]>({
     groupVersionKind: modelToGroupVersionKind(JobModel),
     isList: true,
     ...(namespace !== ALL_NAMESPACES_SESSION_KEY && { namespace, namespaced: true }),
