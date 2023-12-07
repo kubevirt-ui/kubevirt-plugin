29429958b7abad41f676e797460f966b887be08e
Matan Schatzman <mschatzm@redhat.com>
2023-12-07 15:08:42 +0200

    CNV-31864: Adding 2 new instancetypes O and N
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/components/AddBootableVolumeModal/components/VolumeMetadata/components/InstanceTypeDrilldownSelect/utils/constants.ts
+++ src/utils/components/AddBootableVolumeModal/components/VolumeMetadata/components/InstanceTypeDrilldownSelect/utils/constants.ts
@@ -69,13 +69,11 @@ export const instanceTypeSeriesNameMapper: {
     seriesLabel: t('M series'),
   },
   n1: {
-    disabled: true,
     Icon: NetworkIcon,
     possibleSizes: ['medium', 'large', 'xlarge', '2xlarge', '4xlarge', '8xlarge'],
     seriesLabel: t('N series'),
   },
   o1: {
-    disabled: true,
     Icon: ModuleIcon,
     possibleSizes: [
       'nano',
