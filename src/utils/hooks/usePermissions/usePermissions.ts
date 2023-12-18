8bc36af855038826bc0f0856935e489a6c757d66
Matan Schatzman <mschatzm@redhat.com>
2023-12-18 15:16:37 +0200

    CNV-36090: fix permissions
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/hooks/usePermissions/usePermissions.ts
+++ src/utils/hooks/usePermissions/usePermissions.ts
@@ -1,14 +1,13 @@
-import { TemplateModel } from '@kubevirt-ui/kubevirt-api/console';
 import NetworkAttachmentDefinitionModel from '@kubevirt-ui/kubevirt-api/console/models/NetworkAttachmentDefinitionModel';
-import {
-  KUBEVIRT_OS_IMAGES_NS,
-  OPENSHIFT_NAMESPACE,
-  OPENSHIFT_OS_IMAGES_NS,
-} from '@kubevirt-utils/constants/constants';
 import { FLAG_KUBEVIRT_CDI } from '@kubevirt-utils/flags/consts';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { UploadTokenRequestModel } from '@kubevirt-utils/models';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
-import { useAccessReview, useFlag } from '@openshift-console/dynamic-plugin-sdk';
+import {
+  useAccessReview,
+  useActiveNamespace,
+  useFlag,
+} from '@openshift-console/dynamic-plugin-sdk';
 
 type Key = 'attacheNetworks' | 'clone' | 'uploadImage';
 
@@ -19,57 +18,37 @@ export type UsePermissions = () => {
 
 const usePermissions: UsePermissions = () => {
   const { t } = useKubevirtTranslation();
+  const [activeNamespace] = useActiveNamespace();
   const cdiInstalled = useFlag(FLAG_KUBEVIRT_CDI);
 
-  const [canReadOpenshiftNs, canReadOpenshiftNsLoading] = useAccessReview({
-    namespace: OPENSHIFT_NAMESPACE,
-    resource: TemplateModel.plural,
-    verb: 'get',
-  });
-
-  const [canReadOpenshiftOsImgNs, canReadOpenshiftOsImgNsLoading] = useAccessReview({
-    namespace: OPENSHIFT_OS_IMAGES_NS,
-    resource: TemplateModel.plural,
-    verb: 'get',
-  });
-
-  const [canReadKvOsImgNs, canReadKvOsImgNsLoading] = useAccessReview({
-    namespace: KUBEVIRT_OS_IMAGES_NS,
-    resource: TemplateModel.plural,
-    verb: 'get',
-  });
-
-  const [canWriteToKvOsImgNs, canWriteToKvOsImgNsLoading] = useAccessReview({
-    namespace: KUBEVIRT_OS_IMAGES_NS,
-    resource: TemplateModel.plural,
-    verb: 'create',
-  });
-
   const [canReadNads, canReadNadsLoading] = useAccessReview({
+    group: NetworkAttachmentDefinitionModel.apiGroup,
+    namespace: activeNamespace,
     resource: NetworkAttachmentDefinitionModel.plural,
     verb: 'get',
   });
 
-  const canReadOsImagesNs = canReadKvOsImgNs && canReadOpenshiftOsImgNs;
-  const canReadOsImagesNsLoading = canReadKvOsImgNsLoading && canReadOpenshiftOsImgNsLoading;
-
-  const basePermissionsAllowed = canReadOpenshiftNs && canReadOsImagesNs;
-  const basePermissionsLoading = canReadOpenshiftNsLoading && canReadOsImagesNsLoading;
+  const [canCreateUploadToken, canCreateUploadTokenLoading] = useAccessReview({
+    group: UploadTokenRequestModel.apiGroup,
+    namespace: activeNamespace,
+    resource: UploadTokenRequestModel.plural,
+    verb: 'create',
+  });
 
   const capabilitiesData = {
     attacheNetworks: {
-      allowed: basePermissionsAllowed && canReadNads,
-      isLoading: basePermissionsLoading && canReadNadsLoading,
+      allowed: canReadNads,
+      isLoading: canReadNadsLoading,
       taskName: t('Attach VirtualMachine to multiple networks'),
     },
     clone: {
-      allowed: basePermissionsAllowed && cdiInstalled,
-      isLoading: basePermissionsLoading,
+      allowed: cdiInstalled,
+      isLoading: false,
       taskName: t('Clone a VirtualMachine'),
     },
     uploadImage: {
-      allowed: basePermissionsAllowed && canWriteToKvOsImgNs,
-      isLoading: basePermissionsLoading && canWriteToKvOsImgNsLoading,
+      allowed: canCreateUploadToken,
+      isLoading: canCreateUploadTokenLoading,
       taskName: t('Upload a base image from local disk'),
     },
   };
