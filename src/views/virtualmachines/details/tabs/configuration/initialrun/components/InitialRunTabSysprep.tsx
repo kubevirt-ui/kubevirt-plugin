039fc7c5d5dcc7072085063f140df6e1d37007f9
Matan Schatzman <mschatzm@redhat.com>
2023-12-25 15:35:16 +0200

    CNV-35730: Fix windows sysprep on vm init run tab
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/configuration/initialrun/components/InitialRunTabSysprep.tsx
+++ src/views/virtualmachines/details/tabs/configuration/initialrun/components/InitialRunTabSysprep.tsx
@@ -1,27 +1,33 @@
 import React, { FC } from 'react';
-import { getSyspredConfigMapName } from 'src/views/templates/details/tabs/scripts/components/SysPrepItem/sysprep-utils';
+import { getSysprepConfigMapName } from 'src/views/templates/details/tabs/scripts/components/SysPrepItem/sysprep-utils';
 
 import { ConfigMapModel, modelToGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console';
 import { IoK8sApiCoreV1ConfigMap } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import WindowsLabel from '@kubevirt-utils/components/Labels/WindowsLabel';
+import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
 import SearchItem from '@kubevirt-utils/components/SearchItem/SearchItem';
 import { AUTOUNATTEND, UNATTEND } from '@kubevirt-utils/components/SysprepModal/sysprep-utils';
 import { SysprepDescription } from '@kubevirt-utils/components/SysprepModal/SysprepDescription';
+import { SysprepModal } from '@kubevirt-utils/components/SysprepModal/SysprepModal';
 import VirtualMachineDescriptionItem from '@kubevirt-utils/components/VirtualMachineDescriptionItem/VirtualMachineDescriptionItem';
 import { t } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { getVolumes } from '@kubevirt-utils/resources/vm';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
+import { createSysprepConfigMap, patchVMWithExistingSysprepConfigMap } from '../utils/utils';
+
 type InitialRunTabSysprepProps = {
   canUpdateVM: boolean;
   vm: V1VirtualMachine;
 };
 const InitialRunTabSysprep: FC<InitialRunTabSysprepProps> = ({ canUpdateVM, vm }) => {
+  const { createModal } = useModal();
   const vmVolumes = getVolumes(vm);
-  const currentSysprepVolume = vmVolumes?.find(getSyspredConfigMapName);
-  const currentVMSysprepName = getSyspredConfigMapName(currentSysprepVolume);
+
+  const currentSysprepVolume = vmVolumes?.find(getSysprepConfigMapName);
+  const currentVMSysprepName = getSysprepConfigMapName(currentSysprepVolume);
 
   const sysprepSelected = !isEmpty(currentVMSysprepName) && currentVMSysprepName;
   const [externalSysprepConfig, sysprepLoaded, sysprepLoadError] =
@@ -35,6 +41,11 @@ const InitialRunTabSysprep: FC<InitialRunTabSysprepProps> = ({ canUpdateVM, vm }
 
   const { [AUTOUNATTEND]: autoUnattend, [UNATTEND]: unattend } = externalSysprepConfig?.data || {};
 
+  const onSysprepSelected = (name: string) => patchVMWithExistingSysprepConfigMap(name, vm);
+
+  const onSysprepCreation = async (unattended: string, autounattend: string): Promise<void> =>
+    createSysprepConfigMap(unattended, autounattend, externalSysprepConfig, vm);
+
   return (
     <VirtualMachineDescriptionItem
       descriptionData={
@@ -46,9 +57,22 @@ const InitialRunTabSysprep: FC<InitialRunTabSysprepProps> = ({ canUpdateVM, vm }
           selectedSysprepName={currentVMSysprepName}
         />
       }
+      onEditClick={() =>
+        createModal((modalProps) => (
+          <SysprepModal
+            {...modalProps}
+            autoUnattend={autoUnattend}
+            namespace={vm?.metadata?.namespace}
+            onSysprepCreation={onSysprepCreation}
+            onSysprepSelected={onSysprepSelected}
+            sysprepSelected={sysprepSelected}
+            unattend={unattend}
+          />
+        ))
+      }
       data-test-id="sysprep-button"
       descriptionHeader={<SearchItem id="sysprep">{t('Sysprep')}</SearchItem>}
-      isDisabled
+      isDisabled={!canUpdateVM}
       isEdit={canUpdateVM}
       label={<WindowsLabel />}
       showEditOnTitle
