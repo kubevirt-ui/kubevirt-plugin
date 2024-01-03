edadb4ce8d6e3bd380c16399950ee2ee3a43956b
Matan Schatzman <mschatzm@redhat.com>
2024-01-03 09:23:59 +0200

    CNV-36883: Align disk title
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/components/DiskListTitle/DiskListTitle.tsx
+++ src/utils/components/DiskListTitle/DiskListTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import HelpTextIcon from '@kubevirt-utils/components/HelpTextIcon/HelpTextIcon';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { PopoverPosition, Title } from '@patternfly/react-core';
+import { Flex, PopoverPosition, Title } from '@patternfly/react-core';
 
 import SearchItem from '../SearchItem/SearchItem';
 
@@ -11,14 +11,15 @@ const DiskListTitle = () => {
 
   return (
     <Title headingLevel="h2">
-      <SearchItem id="disks">{t('Disks')}</SearchItem>
-      <HelpTextIcon
-        bodyContent={t(
-          'The following information is provided by the OpenShift Virtualization operator.',
-        )}
-        helpIconClassName="title-help-text-icon"
-        position={PopoverPosition.right}
-      />
+      <Flex alignItems={{ default: 'alignItemsCenter' }}>
+        <SearchItem id="disks">{t('Disks')}</SearchItem>
+        <HelpTextIcon
+          bodyContent={t(
+            'The following information is provided by the OpenShift Virtualization operator.',
+          )}
+          position={PopoverPosition.right}
+        />
+      </Flex>
     </Title>
   );
 };
