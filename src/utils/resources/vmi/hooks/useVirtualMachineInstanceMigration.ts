bc45597c3636ac52b5f443a20a2064fe0a62726d
Ugo Palatucci <upalatuc@redhat.com>
2024-01-08 11:15:20 +0100

    CNV-36625: filter vmims by vmi name label

--- src/utils/resources/vmi/hooks/useVirtualMachineInstanceMigration.ts
+++ src/utils/resources/vmi/hooks/useVirtualMachineInstanceMigration.ts
@@ -1,14 +1,21 @@
 import { VirtualMachineInstanceMigrationModelGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console';
 import { V1VirtualMachineInstanceMigration } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { MIGRATION_VMI_NAME_LABEL } from '@kubevirt-utils/resources/vmim/constants';
 import { K8sResourceCommon, useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
 const useVirtualMachineInstanceMigration = (resource: K8sResourceCommon) => {
-  const [vmims] = useK8sWatchResource<V1VirtualMachineInstanceMigration[]>({
-    groupVersionKind: VirtualMachineInstanceMigrationModelGroupVersionKind,
-    isList: true,
-    name: `${resource?.metadata?.name}-migration`,
-    namespace: resource?.metadata?.namespace,
-  });
+  const [vmims] = useK8sWatchResource<V1VirtualMachineInstanceMigration[]>(
+    resource && {
+      groupVersionKind: VirtualMachineInstanceMigrationModelGroupVersionKind,
+      isList: true,
+      namespace: resource?.metadata?.namespace,
+      selector: {
+        matchLabels: {
+          [MIGRATION_VMI_NAME_LABEL]: resource?.metadata?.name,
+        },
+      },
+    },
+  );
 
   // since migration objects are kepts until VMI is deleted
   // we will need to find the one which is related to VMI and is not copmleted.
---
+++ src/utils/resources/vmim/constants.ts
@@ -0,0 +1,1 @@
+export const MIGRATION_VMI_NAME_LABEL = 'kubevirt.io/vmi-name';
