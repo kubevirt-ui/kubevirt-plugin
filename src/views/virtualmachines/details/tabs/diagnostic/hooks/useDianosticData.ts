0369a1aeb5e5f4f7251a86914073197f2efface9
Matan Schatzman <mschatzm@redhat.com>
2024-01-11 14:15:38 +0200

    CNV-37094: Adding datavolume statuses to diagnostic tab
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/diagnostic/hooks/useDianosticData.ts
+++ src/views/virtualmachines/details/tabs/diagnostic/hooks/useDianosticData.ts
@@ -1,24 +1,41 @@
 import { useMemo } from 'react';
 
+import { V1beta1DataVolume } from '@kubevirt-ui/kubevirt-api/containerized-data-importer/models';
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { useK8sWatchResources } from '@openshift-console/dynamic-plugin-sdk';
 
-import { VirtualizationStatusCondition, VirtualizationVolumeSnapshotStatus } from '../utils/types';
-import { conditionsTransformer, volumeSnapshotStatusesTransformer } from '../utils/utils';
+import {
+  VirtualizationDataVolumeStatus,
+  VirtualizationStatusCondition,
+  VirtualizationVolumeSnapshotStatus,
+} from '../utils/types';
+import {
+  buildDataVolumeResources,
+  buildDVStatus,
+  conditionsTransformer,
+  volumeSnapshotStatusesTransformer,
+} from '../utils/utils';
 
 type UseDiagnosticData = (vm: V1VirtualMachine) => {
   conditions: VirtualizationStatusCondition[];
+  dataVolumesStatuses: VirtualizationDataVolumeStatus[];
   volumeSnapshotStatuses: VirtualizationVolumeSnapshotStatus[];
 };
 
 const useDiagnosticData: UseDiagnosticData = (vm) => {
+  const dataVolumesData = useK8sWatchResources<{ [name: string]: V1beta1DataVolume }>(
+    buildDataVolumeResources(vm),
+  );
+
   const data = useMemo(() => {
     const volumeSnapshotStatuses = volumeSnapshotStatusesTransformer(
       vm?.status?.volumeSnapshotStatuses,
     );
     const conditions = conditionsTransformer(vm?.status?.conditions);
+    const dataVolumesStatuses = buildDVStatus(dataVolumesData);
 
-    return { conditions, volumeSnapshotStatuses };
-  }, [vm]);
+    return { conditions, dataVolumesStatuses, volumeSnapshotStatuses };
+  }, [dataVolumesData, vm?.status?.conditions, vm?.status?.volumeSnapshotStatuses]);
 
   return data;
 };
