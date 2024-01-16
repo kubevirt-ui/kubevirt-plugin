7d27afbaaa27dea1eb7a90a7a2e7e4bc38f3686f
Matan Schatzman <mschatzm@redhat.com>
2024-01-16 11:17:12 +0200

    CNV-32502: Set create IT default stroage class to virt default or cluster default
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/utils/hooks/useDefaultStorage/constants.ts
+++ src/utils/hooks/useDefaultStorage/constants.ts
@@ -1,1 +1,3 @@
 export const DEFAULT_STORAGE_CLASS_ANNOTATION = 'storageclass.kubernetes.io/is-default-class';
+export const DEFAULT_VIRT_STORAGE_CLASS_ANNOTATION =
+  'storageclass.kubevirt.io/is-default-virt-class';
