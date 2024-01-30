a49acecb37a1b201886b265e8034104034d434ce
Matan Schatzman <mschatzm@redhat.com>
2024-01-30 15:05:13 +0200

    CNV-37457: Add user provided tab to instance type
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/instancetypes/list/InstanceTypePage.tsx
+++ src/views/instancetypes/list/InstanceTypePage.tsx
@@ -2,12 +2,10 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import classNames from 'classnames';
 
-import {
-  VirtualMachineClusterPreferenceModelGroupVersionKind,
-  VirtualMachineClusterPreferenceModelRef,
-  VirtualMachinePreferenceModelGroupVersionKind,
-  VirtualMachinePreferenceModelRef,
-} from '@kubevirt-ui/kubevirt-api/console';
+import { VirtualMachineInstancetypeModelRef } from '@kubevirt-ui/kubevirt-api/console';
+import VirtualMachineClusterInstancetypeModel, {
+  VirtualMachineClusterInstancetypeModelRef,
+} from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachineClusterInstancetypeModel';
 import { ALL_NAMESPACES_SESSION_KEY } from '@kubevirt-utils/hooks/constants';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import {
@@ -17,41 +15,39 @@ import {
 } from '@openshift-console/dynamic-plugin-sdk';
 import { Tab, Tabs, TabTitleText } from '@patternfly/react-core';
 
-import ClusterPreferenceList from './ClusterPreferenceList';
-import UserPreferenceList from './UserPreferenceList';
+import ClusterInstancetypeList from './ClusterInstancetypeList';
+import UserInstancetypeList from './UserInstancetypeList';
 
 import '@kubevirt-utils/styles/list-managment-group.scss';
 
-const PreferencePage = () => {
+const InstanceTypePage = () => {
   const { t } = useKubevirtTranslation();
   const history = useHistory();
   const [activeNamespace] = useActiveNamespace();
   const [activeTabKey, setActiveTabKey] = useState<number | string>(
-    history?.location?.pathname.includes(VirtualMachineClusterPreferenceModelGroupVersionKind.kind)
-      ? 0
-      : 1,
+    history?.location?.pathname.includes(VirtualMachineClusterInstancetypeModel.kind) ? 0 : 1,
   );
 
   const urlUserPreference = useMemo(
     () =>
       activeNamespace === ALL_NAMESPACES_SESSION_KEY
-        ? `/k8s/all-namespaces/${VirtualMachinePreferenceModelRef}`
-        : `/k8s/ns/${activeNamespace}/${VirtualMachinePreferenceModelRef}`,
+        ? `/k8s/all-namespaces/${VirtualMachineInstancetypeModelRef}`
+        : `/k8s/ns/${activeNamespace}/${VirtualMachineInstancetypeModelRef}`,
     [activeNamespace],
   );
 
   const groupVersionKind = useMemo(
     () =>
       activeTabKey === 0
-        ? VirtualMachineClusterPreferenceModelGroupVersionKind
-        : VirtualMachinePreferenceModelGroupVersionKind,
+        ? VirtualMachineClusterInstancetypeModelRef
+        : VirtualMachineInstancetypeModelRef,
     [activeTabKey],
   );
 
   useEffect(() => {
     history.push(
       activeTabKey === 0
-        ? `/k8s/cluster/${VirtualMachineClusterPreferenceModelRef}`
+        ? `/k8s/cluster/${VirtualMachineClusterInstancetypeModelRef}`
         : urlUserPreference,
     );
   }, [activeTabKey, history, urlUserPreference]);
@@ -62,8 +58,8 @@ const PreferencePage = () => {
         <ListPageHeader
           title={
             activeTabKey === 0
-              ? t('VirtualMachineClusterPreferences')
-              : t('VirtualMachinePreferences')
+              ? t('VirtualMachineClusterInstancetypes')
+              : t('VirtualMachineInstancetypes')
           }
         >
           <ListPageCreate
@@ -83,15 +79,15 @@ const PreferencePage = () => {
         activeKey={activeTabKey}
         style={{ flexShrink: 0 }}
       >
-        <Tab eventKey={0} title={<TabTitleText>{t('Cluster preferences')}</TabTitleText>}>
-          <ClusterPreferenceList />
+        <Tab eventKey={0} title={<TabTitleText>{t('Cluster provided')}</TabTitleText>}>
+          <ClusterInstancetypeList />
         </Tab>
-        <Tab eventKey={1} title={<TabTitleText>{t('User preferences')}</TabTitleText>}>
-          <UserPreferenceList />
+        <Tab eventKey={1} title={<TabTitleText>{t('User provided')}</TabTitleText>}>
+          <UserInstancetypeList />
         </Tab>
       </Tabs>
     </>
   );
 };
 
-export default PreferencePage;
+export default InstanceTypePage;
