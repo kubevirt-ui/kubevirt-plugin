232108cb8253a89baddc5fb08993c013dda37abd
Phillip Bailey <phbailey@redhat.com>
2024-02-20 12:03:22 -0500

    Show user-created VM InstanceTypes in Add boot volume modal

--- src/utils/components/AddBootableVolumeModal/components/VolumeMetadata/components/InstanceTypeDrilldownSelect/components/UserInstanceTypeMenu/UserInstanceTypeMenu.tsx
+++ src/utils/components/AddBootableVolumeModal/components/VolumeMetadata/components/InstanceTypeDrilldownSelect/components/UserInstanceTypeMenu/UserInstanceTypeMenu.tsx
@@ -1,36 +1,47 @@
 import React, { Dispatch, FC, SetStateAction, useMemo, useState } from 'react';
 
+import { InstanceTypes } from '@catalog/CreateFromInstanceTypes/state/utils/types';
+import { VirtualMachineInstancetypeModelGroupVersionKind } from '@kubevirt-ui/kubevirt-api/console';
+import { isRedHatInstanceType } from '@kubevirt-utils/components/AddBootableVolumeModal/components/VolumeMetadata/components/InstanceTypeDrilldownSelect/utils/utils';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { VirtualMachineClusterInstancetypeModelGroupVersionKind } from '@kubevirt-utils/models';
+import { getName } from '@kubevirt-utils/resources/shared';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { ResourceIcon } from '@openshift-console/dynamic-plugin-sdk';
 import { MenuItem, MenuSearch, MenuSearchInput, SearchInput } from '@patternfly/react-core';
 
 type UserInstanceTypeMenuProps = {
-  items: string[];
+  allInstanceTypes: InstanceTypes;
   selected: string;
   setSelected: Dispatch<SetStateAction<string>>;
 };
 
-const UserInstanceTypeMenu: FC<UserInstanceTypeMenuProps> = ({ items, selected, setSelected }) => {
+const UserInstanceTypeMenu: FC<UserInstanceTypeMenuProps> = ({
+  allInstanceTypes,
+  selected,
+  setSelected,
+}) => {
   const { t } = useKubevirtTranslation();
 
   const [searchInput, setSearchInput] = useState('');
 
+  const userCreatedInstanceTypes = allInstanceTypes.filter((it) => !isRedHatInstanceType(it));
+
   const filteredItems = useMemo(
     () =>
-      items.filter(
-        (opt) =>
-          isEmpty(searchInput) || opt.toLowerCase().includes(searchInput.toString().toLowerCase()),
+      userCreatedInstanceTypes.filter(
+        (it) =>
+          isEmpty(searchInput) ||
+          getName(it).toLowerCase().includes(searchInput.toString().toLowerCase()),
       ),
-    [items, searchInput],
+    [userCreatedInstanceTypes, searchInput],
   );
 
   if (isEmpty(filteredItems)) return <MenuItem isDisabled>{t('No results found')}</MenuItem>;
 
   return (
     <>
-      {items.length > 5 && (
+      {filteredItems.length > 5 && (
         <MenuSearch>
           <MenuSearchInput>
             <SearchInput
@@ -42,21 +53,28 @@ const UserInstanceTypeMenu: FC<UserInstanceTypeMenuProps> = ({ items, selected, 
           </MenuSearchInput>
         </MenuSearch>
       )}
-      {filteredItems.map((userITName) => (
-        <MenuItem
-          icon={
-            <ResourceIcon
-              groupVersionKind={VirtualMachineClusterInstancetypeModelGroupVersionKind}
-            />
-          }
-          isSelected={selected === userITName}
-          itemId={userITName}
-          key={userITName}
-          onClick={() => setSelected(userITName)}
-        >
-          {userITName}
-        </MenuItem>
-      ))}
+      {filteredItems.map((it) => {
+        const itName = getName(it);
+        return (
+          <MenuItem
+            icon={
+              <ResourceIcon
+                groupVersionKind={
+                  it?.kind === 'VirtualMachineClusterInstancetype'
+                    ? VirtualMachineClusterInstancetypeModelGroupVersionKind
+                    : VirtualMachineInstancetypeModelGroupVersionKind
+                }
+              />
+            }
+            isSelected={selected === itName}
+            itemId={itName}
+            key={itName}
+            onClick={() => setSelected(itName)}
+          >
+            {itName}
+          </MenuItem>
+        );
+      })}
     </>
   );
 };
