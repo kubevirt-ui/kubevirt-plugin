040ff2e9601c8c4cdbc4cb7c20cac9ff8e25cf78
Hilda Stastna <hstastna@redhat.com>
2024-03-04 11:53:30 +0100

    CNV-38883: Update the link to Activation key doc
    Fix the broken "Read more" link to the documentation about activation
    key for automatic subscription of RHEL VirtualMachines. use the newer
    working link:
    https://access.redhat.com/documentation/en-us/red_hat_insights/1-latest/html/remote_host_configuration_and_management/activation-keys_intro-rhc
    
    Fixes https://issues.redhat.com/browse/CNV-38883

--- src/views/clusteroverview/SettingsTab/ClusterTab/components/GuestManagmentSection/AutomaticSubscriptionRHELGuests/utils/constants.ts
+++ src/views/clusteroverview/SettingsTab/ClusterTab/components/GuestManagmentSection/AutomaticSubscriptionRHELGuests/utils/constants.ts
@@ -1,6 +1,6 @@
 export const ACTIVATION_KEYS_URL = 'https://console.redhat.com/settings/connector/activation-keys';
 export const ACTIVATION_KEYS_DOCUMENTATION_URL =
-  'https://access.redhat.com/documentation/en-us/red_hat_insights/2023/html/remote_host_configuration_and_management/activation-keys';
+  'https://access.redhat.com/documentation/en-us/red_hat_insights/1-latest/html/remote_host_configuration_and_management/activation-keys_intro-rhc';
 export const REDHAT_CONSOLE_URL = 'https://console.redhat.com/';
 
 export const AUTOMATIC_UPDATE_FEATURE_NAME = 'auto-update-rhel-vms';
