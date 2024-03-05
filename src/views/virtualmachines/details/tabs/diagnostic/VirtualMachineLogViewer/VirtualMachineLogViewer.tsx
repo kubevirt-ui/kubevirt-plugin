3c47fc4cc66fbb7898b399d880846d5784c05096
Matan Schatzman <mschatzm@redhat.com>
2024-03-05 07:58:14 +0200

    CNV-38577: Fix initial load of serial console
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/diagnostic/VirtualMachineLogViewer/VirtualMachineLogViewer.tsx
+++ src/views/virtualmachines/details/tabs/diagnostic/VirtualMachineLogViewer/VirtualMachineLogViewer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { V1Devices } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import Loading from '@kubevirt-utils/components/Loading/Loading';
 import { getChangedGuestSystemAccessLog } from '@kubevirt-utils/components/PendingChanges/utils/helpers';
 import { DISABLED_GUEST_SYSTEM_LOGS_ACCESS } from '@kubevirt-utils/hooks/useFeatures/constants';
 import { useFeatures } from '@kubevirt-utils/hooks/useFeatures/useFeatures';
@@ -15,29 +16,42 @@ import VirtualMachineBasicLogViewer from './VirtualMachineBasicLogViewer/Virtual
 
 const VirtualMachineLogViewer = ({ connect, vm }) => {
   const { t } = useKubevirtTranslation();
-  const { pods, vmi } = useVMIAndPodsForVM(vm?.metadata?.name, vm?.metadata?.namespace);
+  const { loaded, pods, vmi } = useVMIAndPodsForVM(vm?.metadata?.name, vm?.metadata?.namespace);
   const pod = getVMIPod(vmi, pods);
   const { featureEnabled: isClusterDisabledGuestSystemLogs } = useFeatures(
     DISABLED_GUEST_SYSTEM_LOGS_ACCESS,
   );
+
   const isDisabledAtVM =
     (getDevices(vm) as V1Devices & { logSerialConsole: boolean })?.logSerialConsole === false &&
     !isClusterDisabledGuestSystemLogs;
 
   const isNeededRestart = getChangedGuestSystemAccessLog(vm, vmi);
 
-  const { data } = useVirtualMachineLogData({ connect, pod });
+  const isPodLogContainerExist = Boolean(
+    pod?.spec?.containers?.find((container) => container?.name === 'guest-console-log'),
+  );
+
+  const { data } = useVirtualMachineLogData({ connect: connect && isPodLogContainerExist, pod });
+
+  if (!loaded) {
+    return (
+      <Bullseye>
+        <Loading />
+      </Bullseye>
+    );
+  }
 
   if (!isRunning(vm)) {
     return <Bullseye>{t('Virtual machine is not running')}</Bullseye>;
   }
 
-  if (isNeededRestart) {
-    return <Bullseye>{t('Guest system logs not ready. Restart required')}</Bullseye>;
-  }
-
   if (isClusterDisabledGuestSystemLogs) {
     return <Bullseye>{t('Guest system logs are disabled at cluster')}</Bullseye>;
+  }
+
+  if (isNeededRestart || !isPodLogContainerExist) {
+    return <Bullseye>{t('Guest system logs not ready. Restart required')}</Bullseye>;
   }
 
   if (isDisabledAtVM) {
