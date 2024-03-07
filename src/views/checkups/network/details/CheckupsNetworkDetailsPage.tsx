43154aff415050ad4e860af3abfe37860e688e9d
Matan Schatzman <mschatzm@redhat.com>
2024-03-07 09:14:22 +0200

    CNV-39048: Fix 4.15 broken link of checkups details
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/checkups/network/details/CheckupsNetworkDetailsPage.tsx
+++ src/views/checkups/network/details/CheckupsNetworkDetailsPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import Loading from '@kubevirt-utils/components/Loading/Loading';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
@@ -16,12 +16,14 @@ import CheckupsNetworkDetailsPageSection from './CheckupsNetworkDetailsPageSecti
 import './checkups-network-details-page.scss';
 
 const CheckupsNetworkDetailsPage = () => {
-  const { vmName } = useParams<{ vmName: string }>();
+  const location = useLocation();
   const { t } = useKubevirtTranslation();
   const { configMaps, error, jobs, loading } = useCheckupsNetworkData();
   const [activeTabKey, setActiveTabKey] = useState<number>(0);
 
-  const configMap = configMaps.find((cm) => cm.metadata.name === vmName);
+  const configMap = configMaps.find(
+    (cm) => cm.metadata.name === location.pathname.split('/').pop(),
+  );
   const jobMatches = getJobByName(jobs, configMap?.metadata?.name);
 
   if (!configMap)
