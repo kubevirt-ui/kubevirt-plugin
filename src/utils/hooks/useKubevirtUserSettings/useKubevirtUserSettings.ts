d1560621cb9820bafab997df32fe1199419b13e0
Aviv Turgeman <aturgema@redhat.com>
2024-04-11 11:36:21 +0300

    UI settings should not be stored in default namespace
    Signed-off-by: Aviv Turgeman <aturgema@redhat.com>

--- src/utils/hooks/useKubevirtUserSettings/useKubevirtUserSettings.ts
+++ src/utils/hooks/useKubevirtUserSettings/useKubevirtUserSettings.ts
@@ -3,27 +3,22 @@ import { useEffect, useState } from 'react';
 import {
   ConfigMapModel,
   modelToGroupVersionKind,
-  RoleBindingModel,
-  RoleModel,
   UserModel,
 } from '@kubevirt-ui/kubevirt-api/console';
-import {
-  IoK8sApiCoreV1ConfigMap,
-  IoK8sApiRbacV1Role,
-  IoK8sApiRbacV1RoleBinding,
-} from '@kubevirt-ui/kubevirt-api/kubernetes';
-import { DEFAULT_NAMESPACE } from '@kubevirt-utils/constants/constants';
+import { IoK8sApiCoreV1ConfigMap } from '@kubevirt-ui/kubevirt-api/kubernetes';
+import { DEFAULT_NAMESPACE, OPENSHIFT_CNV } from '@kubevirt-utils/constants/constants';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
-import { k8sCreate, useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import { useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
 
-import {
-  KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME,
-  userSettingsRole,
-  userSettingsRoleBinding,
-} from './utils/const';
+import { KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME } from './utils/const';
 import { UseKubevirtUserSettings } from './utils/types';
-import userSettingsInitialState, { UserSettingsState } from './utils/userSettingsInitialState';
-import { parseNestedJSON, patchUserConfigMap } from './utils/utils';
+import { UserSettingsState } from './utils/userSettingsInitialState';
+import {
+  createEmptyUserSettings,
+  moveUserSettingsToOpenshiftCNV,
+  parseNestedJSON,
+  patchUserConfigMap,
+} from './utils/utils';
 
 const useKubevirtUserSettings: UseKubevirtUserSettings = (key) => {
   const [error, setError] = useState<Error>();
@@ -37,7 +32,7 @@ const useKubevirtUserSettings: UseKubevirtUserSettings = (key) => {
 
   const userName = user?.metadata?.uid || user?.metadata?.name?.replace(/[^-._a-zA-Z0-9]+/g, '-');
 
-  const [userConfigMap, loadedConfigMap, configMapError] =
+  const [userConfigMapDefault, , errorConfigMapDefault] =
     useK8sWatchResource<IoK8sApiCoreV1ConfigMap>(
       userName && {
         groupVersionKind: modelToGroupVersionKind(ConfigMapModel),
@@ -46,56 +41,60 @@ const useKubevirtUserSettings: UseKubevirtUserSettings = (key) => {
       },
     );
 
+  const [userConfigMapOpenshiftCNV, loadedConfigMapOpenshiftCNV, errorConfigMapOpenshiftCNV] =
+    useK8sWatchResource<IoK8sApiCoreV1ConfigMap>(
+      userName && {
+        groupVersionKind: modelToGroupVersionKind(ConfigMapModel),
+        name: KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME,
+        namespace: OPENSHIFT_CNV,
+      },
+    );
+
   useEffect(() => {
     if (userName) {
-      if (configMapError?.code === 404) {
-        const createResources = async () => {
-          await k8sCreate<IoK8sApiCoreV1ConfigMap>({
-            data: {
-              data: { [userName]: JSON.stringify(userSettingsInitialState) },
-              metadata: {
-                name: KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME,
-                namespace: DEFAULT_NAMESPACE,
-              },
-            },
-            model: ConfigMapModel,
-          });
+      if (errorConfigMapOpenshiftCNV?.code === 404) {
+        if (!isEmpty(userConfigMapDefault)) {
+          try {
+            moveUserSettingsToOpenshiftCNV(userConfigMapDefault);
+            setError(null);
+          } catch (e) {
+            setError(e);
+          }
+        }
 
-          await k8sCreate<IoK8sApiRbacV1Role>({
-            data: userSettingsRole,
-            model: RoleModel,
-          });
-
-          await k8sCreate<IoK8sApiRbacV1RoleBinding>({
-            data: userSettingsRoleBinding,
-            model: RoleBindingModel,
-          });
-        };
-
-        try {
-          createResources();
-          setError(null);
-        } catch (e) {
-          setError(e);
+        if (errorConfigMapDefault?.code === 404) {
+          try {
+            createEmptyUserSettings(userName);
+            setError(null);
+          } catch (e) {
+            setError(e);
+          }
         }
       }
-      loadedConfigMap && setLoading(false);
+
+      loadedConfigMapOpenshiftCNV && setLoading(false);
     }
-  }, [configMapError?.code, userName, loadedConfigMap]);
+  }, [
+    errorConfigMapDefault?.code,
+    userName,
+    loadedConfigMapOpenshiftCNV,
+    errorConfigMapOpenshiftCNV?.code,
+  ]);
 
   useEffect(() => {
-    if (!isEmpty(userConfigMap) && userName) {
+    if (!isEmpty(userConfigMapOpenshiftCNV) && userName) {
       setUserSettings(
-        (<unknown>parseNestedJSON(userConfigMap?.data?.[userName]) || {}) as UserSettingsState,
+        (<unknown>parseNestedJSON(userConfigMapOpenshiftCNV?.data?.[userName]) ||
+          {}) as UserSettingsState,
       );
     }
-  }, [userConfigMap, userName]);
+  }, [userConfigMapOpenshiftCNV, userName]);
 
   const pushUserSettingsChanges = async (data, resolve, reject) => {
     setLoading(true);
 
     try {
-      await patchUserConfigMap(userConfigMap, userName, data);
+      await patchUserConfigMap(userConfigMapOpenshiftCNV, userName, data);
       resolve(key ? data[key] : data);
     } catch (apiError) {
       setError(apiError);
@@ -120,8 +119,8 @@ const useKubevirtUserSettings: UseKubevirtUserSettings = (key) => {
   return [
     key ? userSettings?.[key] : userSettings,
     userSettings && updateUserSetting,
-    !loading && loadedUser && loadedConfigMap,
-    error || errorUser || configMapError,
+    !loading && loadedUser && loadedConfigMapOpenshiftCNV,
+    error || errorUser || errorConfigMapDefault,
   ];
 };
 
--- src/utils/hooks/useKubevirtUserSettings/utils/utils.ts
+++ src/utils/hooks/useKubevirtUserSettings/utils/utils.ts
@@ -1,6 +1,18 @@
-import { ConfigMapModel } from '@kubevirt-ui/kubevirt-api/console';
-import { IoK8sApiCoreV1ConfigMap } from '@kubevirt-ui/kubevirt-api/kubernetes';
-import { k8sPatch } from '@openshift-console/dynamic-plugin-sdk';
+import { ConfigMapModel, RoleBindingModel, RoleModel } from '@kubevirt-ui/kubevirt-api/console';
+import {
+  IoK8sApiCoreV1ConfigMap,
+  IoK8sApiRbacV1Role,
+  IoK8sApiRbacV1RoleBinding,
+} from '@kubevirt-ui/kubevirt-api/kubernetes';
+import { OPENSHIFT_CNV } from '@kubevirt-utils/constants/constants';
+import { k8sCreate, k8sPatch } from '@openshift-console/dynamic-plugin-sdk';
+
+import {
+  KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME,
+  userSettingsRole,
+  userSettingsRoleBinding,
+} from './const';
+import userSettingsInitialState from './userSettingsInitialState';
 
 export const parseNestedJSON = <T>(str: string): T => {
   try {
@@ -29,3 +41,41 @@ export const patchUserConfigMap = async (
     model: ConfigMapModel,
     resource: userConfigMap,
   });
+
+const createUserSettingsConfigMap = (data: IoK8sApiCoreV1ConfigMap['data']) => ({
+  data,
+  metadata: {
+    name: KUBEVIRT_USER_SETTINGS_CONFIG_MAP_NAME,
+    namespace: OPENSHIFT_CNV,
+  },
+});
+
+const createUserSettingsBindings = async () => {
+  await k8sCreate<IoK8sApiRbacV1Role>({
+    data: userSettingsRole,
+    model: RoleModel,
+  });
+
+  await k8sCreate<IoK8sApiRbacV1RoleBinding>({
+    data: userSettingsRoleBinding,
+    model: RoleBindingModel,
+  });
+};
+
+export const moveUserSettingsToOpenshiftCNV = async (userSCDefaultNS: IoK8sApiCoreV1ConfigMap) => {
+  await k8sCreate<IoK8sApiCoreV1ConfigMap>({
+    data: createUserSettingsConfigMap(userSCDefaultNS?.data),
+    model: ConfigMapModel,
+  });
+
+  createUserSettingsBindings();
+};
+
+export const createEmptyUserSettings = async (userName: string) => {
+  await k8sCreate<IoK8sApiCoreV1ConfigMap>({
+    data: createUserSettingsConfigMap({ [userName]: JSON.stringify(userSettingsInitialState) }),
+    model: ConfigMapModel,
+  });
+
+  createUserSettingsBindings();
+};
