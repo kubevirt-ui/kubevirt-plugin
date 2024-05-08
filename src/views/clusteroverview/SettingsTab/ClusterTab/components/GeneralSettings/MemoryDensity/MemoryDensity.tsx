1bdf8d46df77bf7144379fadd51ac40edb44cee1
Matan Schatzman <mschatzm@redhat.com>
2024-05-08 16:32:24 +0300

    CNV-41579: Removed featureGate fo memoryOverCommit
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/clusteroverview/SettingsTab/ClusterTab/components/GeneralSettings/MemoryDensity/MemoryDensity.tsx
+++ src/views/clusteroverview/SettingsTab/ClusterTab/components/GeneralSettings/MemoryDensity/MemoryDensity.tsx
@@ -10,6 +10,8 @@ import { Skeleton } from '@patternfly/react-core';
 
 import ExpandSection from '../../../../ExpandSection/ExpandSection';
 
+import { MEMORY_OVERCOMMIT_END_VALUE, MEMORY_OVERCOMMIT_STARTING_VALUE } from './utils/const';
+
 type MemoryDensityProps = {
   hyperConvergeConfiguration: [hyperConvergeConfig: HyperConverged, loaded: boolean, error: any];
   newBadge: boolean;
@@ -26,13 +28,8 @@ const MemoryDensity: FC<MemoryDensityProps> = ({ hyperConvergeConfiguration, new
         data: [
           {
             op: 'replace',
-            path: `/spec/featureGates/enableHigherDensityWithSwap`,
-            value: checked,
-          },
-          {
-            op: 'replace',
             path: `/spec/higherWorkloadDensity/memoryOvercommitPercentage`,
-            value: checked ? 150 : 100,
+            value: checked ? MEMORY_OVERCOMMIT_END_VALUE : MEMORY_OVERCOMMIT_STARTING_VALUE,
           },
         ],
         model: HyperConvergedModel,
@@ -44,16 +41,16 @@ const MemoryDensity: FC<MemoryDensityProps> = ({ hyperConvergeConfiguration, new
 
   if (!hyperLoaded) return <Skeleton width={'300px'} />;
 
-  const higherDensity = hyperConverge.spec?.featureGates?.enableHigherDensityWithSwap;
+  const higherDensity = hyperConverge.spec?.higherWorkloadDensity?.memoryOvercommitPercentage;
 
   return (
     <ExpandSection toggleText={t('Memory density')}>
       <SectionWithSwitch
         helpTextIconContent={t('Configures the VM workloads to use swap for higher density')}
         id="memory-density-feature"
-        isDisabled={!hyperLoaded || !isAdmin || higherDensity === undefined}
+        isDisabled={!hyperLoaded || !isAdmin}
         newBadge={newBadge}
-        switchIsOn={Boolean(higherDensity)}
+        switchIsOn={Boolean(higherDensity > MEMORY_OVERCOMMIT_STARTING_VALUE)}
         title={t('Enable memory density')}
         turnOnSwitch={onChange}
       />
