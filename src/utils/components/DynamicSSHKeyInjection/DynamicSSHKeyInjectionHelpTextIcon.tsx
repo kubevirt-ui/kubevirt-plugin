e2514eaf9634992c1219e50e5b1ea279435a464e
Hilda Stastna <hstastna@redhat.com>
2024-05-20 18:11:26 +0200

    CNV-41200: Make popup text for dynamic SSH key injection more clear
    Change popover text for disabled dynamic SSH key injection from
    "Only RHEL 9 supports dynamic SSH key injection. Instead, you must add
    an public SSH key before the VirtualMachine boots for the first time."
    to "Before you boot the VirtualMachine (VM) for the first time, add
    a public SSH key. Only RHEL 9 supports dynamic key injection,
    and in RHEL 9 you must enable dynamic SSH when you create the VM."
    
    Fixes https://issues.redhat.com/browse/CNV-41200

--- src/utils/components/DynamicSSHKeyInjection/DynamicSSHKeyInjectionHelpTextIcon.tsx
+++ src/utils/components/DynamicSSHKeyInjection/DynamicSSHKeyInjectionHelpTextIcon.tsx
@@ -17,7 +17,7 @@ const DynamicSSHKeyInjectionHelpTextIcon: FC<DynamicSSHKeyInjectionHelpTextIconP
       bodyContent={
         isDisabled
           ? t(
-              'Only RHEL 9 supports dynamic SSH key injection. Instead, you must add an public SSH key before the VirtualMachine boots for the first time.',
+              'Before you boot the VirtualMachine (VM) for the first time, add a public SSH key. Only RHEL 9 supports dynamic key injection, and in RHEL 9 you must enable dynamic SSH when you create the VM.',
             )
           : t(
               'Requires RHEL 9. If enabled, SSH keys can be changed while the VirtualMachine is running. Otherwise, the VirtualMachine inherits the key injection setting of its image.',
