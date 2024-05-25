fcfea6538daa0ee29b04c8bfc64eb0cfe1c3dc3d
Phillip Bailey <phbailey@redhat.com>
2024-05-24 22:04:01 -0400

    Fix SSH key injection when DataSource is a VolumeSnapshot

--- src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/utils/utils.ts
+++ src/views/catalog/CreateFromInstanceTypes/components/VMDetailsSection/utils/utils.ts
@@ -2,10 +2,12 @@ import {
   DEFAULT_PREFERENCE_LABEL,
   PREFERENCE_DISPLAY_NAME_KEY,
 } from '@catalog/CreateFromInstanceTypes/utils/constants';
+import { IoK8sApiCoreV1PersistentVolumeClaim } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import {
   V1beta1VirtualMachineClusterInstancetype,
   V1beta1VirtualMachineClusterPreference,
 } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { DYNAMIC_CREDENTIALS_SUPPORT } from '@kubevirt-utils/components/DynamicSSHKeyInjection/constants/constants';
 import { t } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { BootableVolume } from '@kubevirt-utils/resources/bootableresources/types';
 import { getAnnotation, getLabel } from '@kubevirt-utils/resources/shared';
@@ -38,3 +40,10 @@ export const getCPUAndMemoryFromDefaultInstanceType = (
 
   return t('{{cpu}} CPU | {{memory}} Memory', { cpu, memory });
 };
+
+export const dataSourceHasDynamicSSHLabel = (
+  pvcSource: IoK8sApiCoreV1PersistentVolumeClaim,
+  selectedBootableVolume: BootableVolume,
+) =>
+  getLabel(pvcSource, DYNAMIC_CREDENTIALS_SUPPORT) ||
+  getLabel(selectedBootableVolume, DYNAMIC_CREDENTIALS_SUPPORT);
