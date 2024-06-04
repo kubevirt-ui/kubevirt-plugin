7743cc5af81352f23264c81df0c8a6f4896d7c79
Matan Schatzman <mschatzm@redhat.com>
2024-06-04 09:19:05 +0300

    CNV-41586: Missing functions for sysprep customizr IT flow
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/configuration/initialrun/utils/utils.ts
+++ src/views/virtualmachines/details/tabs/configuration/initialrun/utils/utils.ts
@@ -11,38 +11,57 @@ import {
   UNATTEND,
 } from '@kubevirt-utils/components/SysprepModal/sysprep-utils';
 import { getDisks, getVolumes } from '@kubevirt-utils/resources/vm';
+import { UpdateCustomizeInstanceType } from '@kubevirt-utils/store/customizeInstanceType';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { k8sCreate, k8sPatch } from '@openshift-console/dynamic-plugin-sdk';
 
 export const patchVMWithExistingSysprepConfigMap = async (
   name: string,
   vm: V1VirtualMachine,
+  onSubmit?: UpdateCustomizeInstanceType,
 ): Promise<void> => {
   const vmVolumes = getVolumes(vm);
   const vmDisks = getDisks(vm);
 
-  await k8sPatch<V1VirtualMachine>({
-    data: [
-      {
-        op: 'replace',
-        path: `/spec/template/spec/domain/devices/disks`,
-        value: [
-          ...vmDisks.filter((disk) => disk?.name !== SYSPREP),
-          ...(!isEmpty(name) ? [sysprepDisk()] : []),
+  onSubmit
+    ? onSubmit([
+        {
+          data: [
+            ...(vmDisks || []).filter((disk) => disk?.name !== SYSPREP),
+            ...(!isEmpty(name) ? [sysprepDisk()] : []),
+          ],
+          path: `spec.template.spec.domain.devices.disks`,
+        },
+        {
+          data: [
+            ...(vmVolumes || []).filter((vol) => vol?.name !== SYSPREP),
+            ...(!isEmpty(name) ? [sysprepVolume(name)] : []),
+          ],
+          path: `spec.template.spec.volumes`,
+        },
+      ])
+    : await k8sPatch<V1VirtualMachine>({
+        data: [
+          {
+            op: 'replace',
+            path: `/spec/template/spec/domain/devices/disks`,
+            value: [
+              ...vmDisks.filter((disk) => disk?.name !== SYSPREP),
+              ...(!isEmpty(name) ? [sysprepDisk()] : []),
+            ],
+          },
+          {
+            op: 'replace',
+            path: `/spec/template/spec/volumes`,
+            value: [
+              ...vmVolumes.filter((vol) => vol?.name !== SYSPREP),
+              ...(!isEmpty(name) ? [sysprepVolume(name)] : []),
+            ],
+          },
         ],
-      },
-      {
-        op: 'replace',
-        path: `/spec/template/spec/volumes`,
-        value: [
-          ...vmVolumes.filter((vol) => vol?.name !== SYSPREP),
-          ...(!isEmpty(name) ? [sysprepVolume(name)] : []),
-        ],
-      },
-    ],
-    model: VirtualMachineModel,
-    resource: vm,
-  });
+        model: VirtualMachineModel,
+        resource: vm,
+      });
 };
 
 export const createSysprepConfigMap = async (
@@ -50,6 +69,7 @@ export const createSysprepConfigMap = async (
   autounattend: string,
   externalSysprepConfig: IoK8sApiCoreV1ConfigMap,
   vm: V1VirtualMachine,
+  onSubmit?: UpdateCustomizeInstanceType,
 ): Promise<void> => {
   const vmVolumes = getVolumes(vm);
   const vmDisks = getDisks(vm);
@@ -78,20 +98,33 @@ export const createSysprepConfigMap = async (
   }
 
   await k8sCreate({ data: configMap, model: ConfigMapModel });
-  await k8sPatch<V1VirtualMachine>({
-    data: [
-      {
-        op: 'replace',
-        path: `/spec/template/spec/domain/devices/disks`,
-        value: [...vmDisks, sysprepDisk()],
-      },
-      {
-        op: 'replace',
-        path: `/spec/template/spec/volumes`,
-        value: [...vmVolumes, sysprepVolume(configMap.metadata.name)],
-      },
-    ],
-    model: VirtualMachineModel,
-    resource: vm,
-  });
+  onSubmit
+    ? onSubmit([
+        {
+          data: [sysprepDisk()],
+          merge: true,
+          path: 'spec.template.spec.domain.devices.disks',
+        },
+        {
+          data: [sysprepVolume(configMap.metadata.name)],
+          merge: true,
+          path: `spec.template.spec.volumes`,
+        },
+      ])
+    : await k8sPatch<V1VirtualMachine>({
+        data: [
+          {
+            op: 'replace',
+            path: `/spec/template/spec/domain/devices/disks`,
+            value: [...vmDisks, sysprepDisk()],
+          },
+          {
+            op: 'replace',
+            path: `/spec/template/spec/volumes`,
+            value: [...vmVolumes, sysprepVolume(configMap.metadata.name)],
+          },
+        ],
+        model: VirtualMachineModel,
+        resource: vm,
+      });
 };
