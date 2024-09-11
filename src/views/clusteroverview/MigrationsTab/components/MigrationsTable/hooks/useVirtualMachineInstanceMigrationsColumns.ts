7a025ca74e216274910c5e065c61e15fc0efa9e9
Matan Schatzman <mschatzm@redhat.com>
2024-09-11 14:42:18 +0300

    CNV-45539: Fix migration table column managment
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/clusteroverview/MigrationsTab/components/MigrationsTable/hooks/useVirtualMachineInstanceMigrationsColumns.ts
+++ src/views/clusteroverview/MigrationsTab/components/MigrationsTable/hooks/useVirtualMachineInstanceMigrationsColumns.ts
@@ -1,12 +1,16 @@
-import * as React from 'react';
-
 import {
   NodeModel,
   VirtualMachineInstanceMigrationModelRef,
 } from '@kubevirt-ui/kubevirt-api/console';
+import { ALL_NAMESPACES_SESSION_KEY } from '@kubevirt-utils/hooks/constants';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import useKubevirtUserSettingsTableColumns from '@kubevirt-utils/hooks/useKubevirtUserSettings/useKubevirtUserSettingsTableColumns';
-import { K8sVerb, TableColumn, useAccessReview } from '@openshift-console/dynamic-plugin-sdk';
+import {
+  K8sVerb,
+  TableColumn,
+  useAccessReview,
+  useActiveNamespace,
+} from '@openshift-console/dynamic-plugin-sdk';
 import { sortable } from '@patternfly/react-table';
 
 import { MigrationTableDataLayout } from '../utils/utils';
@@ -16,65 +20,72 @@ const useVirtualMachineInstanceMigrationsColumns = (): [
   TableColumn<MigrationTableDataLayout>[],
 ] => {
   const { t } = useKubevirtTranslation();
-
+  const [activeNamespace] = useActiveNamespace();
   const [canGetNode] = useAccessReview({
     resource: NodeModel.plural,
     verb: 'get' as K8sVerb,
   });
 
-  const columns: TableColumn<MigrationTableDataLayout>[] = React.useMemo(
-    () => [
-      {
-        id: 'vm-name',
-        sort: 'vmim.spec.vmiName',
-        title: t('VirtualMachine name'),
-        transforms: [sortable],
-      },
-      {
-        id: 'status',
-        sort: 'vmim.status.phase',
-        title: t('Status'),
-        transforms: [sortable],
-      },
-      {
-        id: 'source',
-        sort: 'vmiObj.status.migrationState.sourceNode',
-        title: t('Source'),
-        transforms: [sortable],
-      },
-      {
-        id: 'target',
-        sort: 'vmiObj.status.migrationState.targetNode',
-        title: t('Target'),
-        transforms: [sortable],
-      },
-      {
-        id: 'migration-policy',
-        sort: 'vmiObj.status.migrationState.migrationPolicyName',
-        title: t('MigrationPolicy'),
-        transforms: [sortable],
-      },
-      {
-        id: 'vmim-name',
-        sort: 'vmim.metadata.name',
-        title: t('VirtualMachineInstanceMigration name'),
-        transforms: [sortable],
-      },
-      {
-        additional: true,
-        id: 'created',
-        sort: 'vmim.metadata.creationTimestamp',
-        title: t('Created'),
-        transforms: [sortable],
-      },
-      {
-        id: '',
-        props: { className: 'dropdown-kebab-pf pf-v5-c-table__action' },
-        title: '',
-      },
-    ],
-    [t],
-  );
+  const columns: TableColumn<MigrationTableDataLayout>[] = [
+    {
+      id: 'vm-name',
+      sort: 'vmim.spec.vmiName',
+      title: t('VirtualMachine name'),
+      transforms: [sortable],
+    },
+    ...(activeNamespace === ALL_NAMESPACES_SESSION_KEY
+      ? [
+          {
+            id: 'namespace',
+            props: { className: 'pf-m-width-10' },
+            title: t('Namespace'),
+            transforms: [sortable],
+          },
+        ]
+      : []),
+    {
+      id: 'status',
+      sort: 'vmim.status.phase',
+      title: t('Status'),
+      transforms: [sortable],
+    },
+    {
+      id: 'source',
+      sort: 'vmiObj.status.migrationState.sourceNode',
+      title: t('Source'),
+      transforms: [sortable],
+    },
+    {
+      id: 'target',
+      sort: 'vmiObj.status.migrationState.targetNode',
+      title: t('Target'),
+      transforms: [sortable],
+    },
+    {
+      id: 'migration-policy',
+      sort: 'vmiObj.status.migrationState.migrationPolicyName',
+      title: t('MigrationPolicy'),
+      transforms: [sortable],
+    },
+    {
+      id: 'vmim-name',
+      sort: 'vmim.metadata.name',
+      title: t('VirtualMachineInstanceMigration name'),
+      transforms: [sortable],
+    },
+    {
+      additional: true,
+      id: 'created',
+      sort: 'vmim.metadata.creationTimestamp',
+      title: t('Created'),
+      transforms: [sortable],
+    },
+    {
+      id: '',
+      props: { className: 'dropdown-kebab-pf pf-v5-c-table__action' },
+      title: '',
+    },
+  ];
 
   const [activeColumns] = useKubevirtUserSettingsTableColumns<MigrationTableDataLayout>({
     columnManagementID: VirtualMachineInstanceMigrationModelRef,
