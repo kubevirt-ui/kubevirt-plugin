2887a45cc6e2721447c0e682158ea41f18572208
Ugo Palatucci <upalatuc@redhat.com>
2024-10-17 13:59:23 +0200

    CNV-36070: Create initial migration interface

--- src/utils/components/ActionDropdownItem/ActionDropdownItem.tsx
+++ src/utils/components/ActionDropdownItem/ActionDropdownItem.tsx
@@ -2,19 +2,21 @@ import React, { Dispatch, FC, SetStateAction } from 'react';
 import classNames from 'classnames';
 
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { Action, useAccessReview } from '@openshift-console/dynamic-plugin-sdk';
-import { DropdownItem, TooltipPosition } from '@patternfly/react-core';
+import { useAccessReview } from '@openshift-console/dynamic-plugin-sdk';
+import { Menu, MenuContent, MenuItem, MenuList, TooltipPosition } from '@patternfly/react-core';
+
+import { ActionDropdownItemType } from '../ActionsDropdown/constants';
 
 import './action-dropdown-item.scss';
 
 type ActionDropdownItemProps = {
-  action: Action;
+  action: ActionDropdownItemType;
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 };
 
 const ActionDropdownItem: FC<ActionDropdownItemProps> = ({ action, setIsOpen }) => {
   const { t } = useKubevirtTranslation();
-  const [actionAllowed] = useAccessReview(action?.accessReview);
+  const [actionAllowed] = useAccessReview(action?.accessReview || {});
   const isCloneDisabled = !actionAllowed && action?.id === 'vm-action-clone';
 
   const handleClick = () => {
@@ -25,7 +27,7 @@ const ActionDropdownItem: FC<ActionDropdownItemProps> = ({ action, setIsOpen }) 
   };
 
   return (
-    <DropdownItem
+    <MenuItem
       data-test-id={`${action?.id}`}
       description={action?.description}
       isDisabled={action?.disabled || !actionAllowed}
@@ -37,7 +39,22 @@ const ActionDropdownItem: FC<ActionDropdownItemProps> = ({ action, setIsOpen }) 
           position: TooltipPosition.left,
         },
       })}
-      className={classNames({ ActionDropdownItem__disabled: isCloneDisabled })}
+      className={classNames('ActionDropdownItem', {
+        ActionDropdownItem__disabled: isCloneDisabled,
+      })}
+      flyoutMenu={
+        action?.options && (
+          <Menu containsFlyout id={`menu-${action.id}`}>
+            <MenuContent>
+              <MenuList>
+                {action?.options?.map((option) => (
+                  <ActionDropdownItem action={option} key={option.id} setIsOpen={setIsOpen} />
+                ))}
+              </MenuList>
+            </MenuContent>
+          </Menu>
+        )
+      }
     >
       {action?.label}
       {action?.icon && (
@@ -46,7 +63,7 @@ const ActionDropdownItem: FC<ActionDropdownItemProps> = ({ action, setIsOpen }) 
           <span className="text-muted">{action.icon}</span>
         </>
       )}
-    </DropdownItem>
+    </MenuItem>
   );
 };
 
