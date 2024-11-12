beda9ea244129f676365c46bb0f7db26bda2bcb1
Matan Schatzman <mschatzm@redhat.com>
2024-11-12 14:23:36 +0200

    CNV-50914: Support for ephermeral nic
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- src/views/virtualmachines/details/tabs/configuration/network/components/list/NetworkInterfaceRow.tsx
+++ src/views/virtualmachines/details/tabs/configuration/network/components/list/NetworkInterfaceRow.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from 'react';
 
-import { V1Network, V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import {
+  V1Network,
+  V1VirtualMachine,
+  V1VirtualMachineInstanceNetworkInterface,
+} from '@kubevirt-ui/kubevirt-api/kubevirt';
+import EphemeralBadge from '@kubevirt-utils/components/badges/EphemeralBadge/EphemeralBadge';
 import PendingBadge from '@kubevirt-utils/components/badges/PendingBadge/PendingBadge';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { NO_DATA_DASH } from '@kubevirt-utils/resources/vm/utils/constants';
 import { NetworkPresentation } from '@kubevirt-utils/resources/vm/utils/network/constants';
 import { getPrintableNetworkInterfaceType } from '@kubevirt-utils/resources/vm/utils/network/selectors';
 import { RowProps, TableData } from '@openshift-console/dynamic-plugin-sdk';
+
+import { isInterfaceEphemeral } from '../../utils/utils';
 
 import NetworkInterfaceActions from './NetworkInterfaceActions';
 
@@ -17,28 +24,37 @@ export type NetworkInterfaceRowProps = {
 const NetworkInterfaceRow: FC<
   RowProps<
     NetworkPresentation,
-    { isPending: (network: V1Network) => boolean; vm: V1VirtualMachine }
+    {
+      isInterfaceEphemeral: (
+        network: V1Network,
+        iface: V1VirtualMachineInstanceNetworkInterface,
+      ) => undefined | V1VirtualMachineInstanceNetworkInterface;
+      isPending: (network: V1Network) => boolean;
+      vm: V1VirtualMachine;
+    }
   >
 > = ({ activeColumnIDs, obj: { iface, network }, rowData: { isPending, vm } }) => {
   const { t } = useKubevirtTranslation();
+  const ephemeralNic = isInterfaceEphemeral(network, iface);
 
   return (
     <>
       <TableData activeColumnIDs={activeColumnIDs} id="name">
-        {network.name}
-        {isPending(network) && <PendingBadge />}
+        {network?.name || ephemeralNic?.interfaceName || NO_DATA_DASH}
+        {isPending(network) && !ephemeralNic && <PendingBadge />}
+        {ephemeralNic && <EphemeralBadge />}
       </TableData>
       <TableData activeColumnIDs={activeColumnIDs} id="model">
         {iface.model || NO_DATA_DASH}
       </TableData>
       <TableData activeColumnIDs={activeColumnIDs} id="network">
-        {network.pod ? t('Pod networking') : network.multus?.networkName || NO_DATA_DASH}
+        {network?.pod ? t('Pod networking') : network?.multus?.networkName || NO_DATA_DASH}
       </TableData>
       <TableData activeColumnIDs={activeColumnIDs} id="type">
         {getPrintableNetworkInterfaceType(iface)}
       </TableData>
       <TableData activeColumnIDs={activeColumnIDs} id="macAddress">
-        {iface.macAddress || NO_DATA_DASH}
+        {iface?.macAddress || ephemeralNic?.mac || NO_DATA_DASH}
       </TableData>
       <TableData
         activeColumnIDs={activeColumnIDs}
@@ -46,7 +62,7 @@ const NetworkInterfaceRow: FC<
         id=""
       >
         <NetworkInterfaceActions
-          nicName={network.name}
+          nicName={network?.name}
           nicPresentation={{ iface, network }}
           vm={vm}
         />
