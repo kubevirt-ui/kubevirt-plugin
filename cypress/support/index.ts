fd9bfca6b8e653dcc05ed8642b2953d3d455cb3d
Guohua Ouyang <gouyang@redhat.com>
2024-11-19 15:01:55 +0800

    Update index.ts

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -4,3 +4,16 @@ import './project.ts';
 import './resource.ts';
 import './selectors.ts';
 import './views.ts';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      install(encrypted?: boolean): Chainable<Element>;
+    }
+  }
+}
+
+Cypress.on('uncaught:exception', () => {
+  // don't fail on Cypress' internal errors.
+  return false;
+});
