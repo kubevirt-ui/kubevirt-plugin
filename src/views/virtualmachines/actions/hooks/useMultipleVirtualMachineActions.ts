53010b8c87c30929abbc8995b770a42c9784b437
Ugo Palatucci <upalatuc@redhat.com>
2024-11-25 10:49:56 +0100

    CNV-50655: hide nonsense actions

--- src/views/virtualmachines/actions/hooks/useMultipleVirtualMachineActions.ts
+++ src/views/virtualmachines/actions/hooks/useMultipleVirtualMachineActions.ts
@@ -3,40 +3,60 @@ import { useMemo } from 'react';
 import { V1VirtualMachine } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { ActionDropdownItemType } from '@kubevirt-utils/components/ActionsDropdown/constants';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { isPaused, isRunning, isStopped } from '@virtualmachines/utils';
 
-import { restartVM, startVM, stopVM, unpauseVM } from '../actions';
+import { pauseVM, restartVM, startVM, stopVM, unpauseVM } from '../actions';
+
+import { ACTIONS_ID } from './constants';
 
 type UseMultipleVirtualMachineActions = (vms: V1VirtualMachine[]) => ActionDropdownItemType[];
 
 const useMultipleVirtualMachineActions: UseMultipleVirtualMachineActions = (vms) => {
   const { t } = useKubevirtTranslation();
 
-  const actions: ActionDropdownItemType[] = useMemo(() => {
-    return [
+  return useMemo(() => {
+    const actions: ActionDropdownItemType[] = [
       {
-        cta: () => vms.forEach((vm) => startVM(vm)),
-        id: 'vm-action-start',
+        cta: () => vms.forEach(startVM),
+        id: ACTIONS_ID.START,
         label: t('Start'),
       },
       {
-        cta: () => vms.forEach((vm) => restartVM(vm)),
-        id: 'vm-action-restart',
+        cta: () => vms.forEach(restartVM),
+        id: ACTIONS_ID.RESTART,
         label: t('Restart'),
       },
       {
         cta: () => vms.forEach((vm) => stopVM(vm)),
-        id: 'vm-action-stop',
+        id: ACTIONS_ID.STOP,
         label: t('Stop'),
       },
       {
-        cta: () => vms.forEach((vm) => unpauseVM(vm)),
-        id: 'vm-action-unpause',
+        cta: () => vms.forEach(pauseVM),
+        id: ACTIONS_ID.PAUSE,
+        label: t('Pause'),
+      },
+      {
+        cta: () => vms.forEach(unpauseVM),
+        id: ACTIONS_ID.UNPAUSE,
         label: t('Unpause'),
       },
     ];
-  }, [t, vms]);
 
-  return actions;
+    if (vms.every(isStopped)) {
+      return actions.filter((action) => action.id !== ACTIONS_ID.STOP);
+    }
+
+    if (vms.every(isRunning)) {
+      return actions.filter((action) => action.id !== ACTIONS_ID.START);
+    }
+
+    if (vms.every(isPaused)) {
+      return actions.filter((action) => action.id !== ACTIONS_ID.PAUSE);
+    }
+
+    return actions;
+  }, [t, vms]);
 };
 
 export default useMultipleVirtualMachineActions;
