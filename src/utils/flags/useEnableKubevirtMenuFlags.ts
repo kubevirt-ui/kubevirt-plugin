62465cb0f3fa6408c02a16a8d7095241e5de610c
Ugo Palatucci <upalatuc@redhat.com>
2025-01-09 08:52:35 +0100

    CNV-52452: fix disappearing preference and instancetype navbaritems

--- src/utils/flags/useEnableKubevirtMenuFlags.ts
+++ src/utils/flags/useEnableKubevirtMenuFlags.ts
@@ -1,3 +1,5 @@
+import VirtualMachineClusterInstancetypeModel from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachineClusterInstancetypeModel';
+import VirtualMachineClusterPreferenceModel from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachineClusterPreferenceModel';
 import VirtualMachineInstancetypeModel from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachineInstancetypeModel';
 import VirtualMachinePreferenceModel from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachinePreferenceModel';
 import {
@@ -11,10 +13,23 @@ import { FLAG_KUBEVIRT_INSTANCETYPES, FLAG_KUBEVIRT_PREFERENCES } from './consts
 
 const useEnableKubevirtMenuFlags = (setFeatureFlag: SetFeatureFlag) => {
   const [namespace] = useActiveNamespace();
+
+  const [canShowClusterInstancetypes] = useAccessReview({
+    group: VirtualMachineClusterInstancetypeModel.apiGroup,
+    resource: VirtualMachineClusterInstancetypeModel.plural,
+    verb: 'list' as K8sVerb,
+  });
+
   const [canShowInstancetypes] = useAccessReview({
     group: VirtualMachineInstancetypeModel.apiGroup,
     namespace,
     resource: VirtualMachineInstancetypeModel.plural,
+    verb: 'list' as K8sVerb,
+  });
+  const [canShowClusterPreferences] = useAccessReview({
+    group: VirtualMachineClusterPreferenceModel.apiGroup,
+    namespace,
+    resource: VirtualMachineClusterPreferenceModel.plural,
     verb: 'list' as K8sVerb,
   });
 
@@ -25,8 +40,8 @@ const useEnableKubevirtMenuFlags = (setFeatureFlag: SetFeatureFlag) => {
     verb: 'list' as K8sVerb,
   });
 
-  setFeatureFlag(FLAG_KUBEVIRT_PREFERENCES, canShowPreferences);
-  setFeatureFlag(FLAG_KUBEVIRT_INSTANCETYPES, canShowInstancetypes);
+  setFeatureFlag(FLAG_KUBEVIRT_PREFERENCES, canShowPreferences || canShowClusterPreferences);
+  setFeatureFlag(FLAG_KUBEVIRT_INSTANCETYPES, canShowInstancetypes || canShowClusterInstancetypes);
 };
 
 export default useEnableKubevirtMenuFlags;
