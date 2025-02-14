28352e27afa0d50d9764da0254bc42ca372ec16c
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-02-14 17:07:53 +0100

    noVNC 1.6beta PoC
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsConsole.tsx
+++ src/views/virtualmachines/details/tabs/overview/components/VirtualMachinesOverviewTabDetails/components/VirtualMachinesOverviewTabDetailsConsole.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 
 import { V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import { inlined16beta } from '@kubevirt-utils/components/Consoles/components/novnc/inlined';
 import VncConsole from '@kubevirt-utils/components/Consoles/components/vnc-console/VncConsole';
+import { INSECURE, SECURE } from '@kubevirt-utils/components/Consoles/utils/constants';
+import { isConnectionEncrypted } from '@kubevirt-utils/components/Consoles/utils/utils';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { isHeadlessMode as isHeadlessModeVMI } from '@kubevirt-utils/resources/vm/utils/selectors';
 import { vmiStatuses } from '@kubevirt-utils/resources/vmi';
@@ -29,6 +32,19 @@ const VirtualMachinesOverviewTabDetailsConsole: FC<
     resource: 'virtualmachineinstances/vnc',
     verb: 'get',
   });
+
+  const toObjectUrl = (
+    inlineHtml: (args: { host: string; path: string; port: string }) => string,
+  ) => {
+    const path = `api/kubernetes/apis/subresources.kubevirt.io/v1/namespaces/${vmi?.metadata?.namespace}/virtualmachineinstances/${vmi?.metadata?.name}/vnc`;
+    const host = window.location.hostname;
+    const port = window.location.port || (isConnectionEncrypted() ? SECURE : INSECURE);
+    const objectUrl = URL.createObjectURL(
+      new Blob([inlineHtml({ host, path, port })], { type: 'text/html' }),
+    );
+    return objectUrl;
+  };
+
   return (
     <Bullseye className="console-overview">
       <div className="link">
@@ -44,6 +60,17 @@ const VirtualMachinesOverviewTabDetailsConsole: FC<
           variant={ButtonVariant.link}
         >
           {t('Open web console')}
+        </Button>
+      </div>
+      <div className="link">
+        <Button
+          icon={<ExternalLinkAltIcon className="icon" />}
+          iconPosition="end"
+          isDisabled={!isVMRunning || isHeadlessMode || !canConnectConsole}
+          onClick={() => window.open(toObjectUrl(inlined16beta))}
+          variant="link"
+        >
+          {t('Open novnc 1.6beta console')}
         </Button>
       </div>
       {isVMRunning && !isHeadlessMode && canConnectConsole ? (
