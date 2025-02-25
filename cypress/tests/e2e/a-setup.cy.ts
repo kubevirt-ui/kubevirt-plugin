752ebd8f4e0e864f94dba14b7c4929f530840863
Leon Kladnitsky <lkladnit@redhat.com>
2025-02-25 13:14:26 +0200

    SSH key/secret related tests added. Holding cluster test added for possibility of debugging

--- cypress/tests/e2e/a-setup.cy.ts
+++ cypress/tests/e2e/a-setup.cy.ts
@@ -1,6 +1,9 @@
 import secretFixture from '../../fixtures/secret';
-import { CLEANUP_SCRIPT, MINUTE, TEST_NS } from '../../utils/const/index';
+import { CLEANUP_SCRIPT, MINUTE, TEST_NS, TEST_SECRET_NAME } from '../../utils/const/index';
+import { authSSHKey } from '../../utils/const/string';
 import { brandImage } from '../../views/selector';
+import { manageKeysText, useExisting } from '../../views/selector-catalog';
+import { tab } from '../../views/tab';
 
 const WELCOME_OFF_CMD = `oc patch configmap -n kubevirt-hyperconverged kubevirt-user-settings --type=merge --patch '{"data": {"kube-admin": "{\\"quickStart\\":{\\"dontShowWelcomeModal\\":true}}"}}'`;
 const TREEVIEW_ON_CMD = `oc patch configmap -n kubevirt-hyperconverged kubevirt-ui-features --type=merge --patch '{"data": {"treeView": "true", "treeViewFolders": "true"}}'`;
@@ -40,5 +43,25 @@ describe('Prepare the cluster for test', () => {
 
     // needed because of https://issues.redhat.com/browse/CNV-51570
     cy.switchProject(TEST_NS);
+  });
+
+  it('configure public ssh key', () => {
+    cy.visitOverview();
+    tab.navigateToSettings();
+    cy.contains('button[role="tab"]', 'User').click();
+    cy.contains(manageKeysText).click();
+    cy.contains(authSSHKey, { timeout: 20000 }).should('be.visible');
+    cy.wait(2000);
+    // configure new public ssh key
+    cy.get('.settings-tab__content').then(() => {
+      cy.contains('Select project').click();
+      cy.byLegacyTestID(TEST_NS).click({ force: true });
+      cy.get('button.project-ssh-row__secret-name').click();
+      cy.get(useExisting).click();
+      cy.contains('Select secret').click();
+      cy.byButtonText(TEST_SECRET_NAME).click();
+      cy.clickSaveBtn();
+    });
+    cy.contains('button.project-ssh-row__secret-name', TEST_SECRET_NAME).should('exist');
   });
 });
--- cypress/tests/e2e/b-overview.cy.ts
+++ cypress/tests/e2e/b-overview.cy.ts
@@ -1,4 +1,11 @@
+import { TEST_NS } from '../../utils/const/index';
+import { authSSHKey } from '../../utils/const/string';
+import { tabModal } from '../../views/actions';
+import { addNew, manageKeysText, secretName, uploadSecret } from '../../views/selector-catalog';
+import { userButtonTxt } from '../../views/selector-instance';
 import { tab } from '../../views/tab';
+
+const testSecret = 'test-secret';
 
 describe('Test Virtualization Overview', () => {
   before(() => {
@@ -23,5 +30,31 @@ describe('Test Virtualization Overview', () => {
   it('settings tab is loaded', () => {
     tab.navigateToSettings();
     cy.contains('General settings').should('be.visible');
+  });
+
+  it('test saving SSH key', () => {
+    cy.byButtonText(userButtonTxt).click();
+    cy.byButtonText(manageKeysText).click();
+    cy.byButtonText(manageKeysText)
+      .parent()
+      .within(() => {
+        cy.contains(authSSHKey, { timeout: 20000 }).should('be.visible');
+      });
+    // configure public ssh key when it's not configured
+    cy.get('.settings-tab__content').then(($body) => {
+      if ($body.text().includes('Select project')) {
+        cy.contains('Select project').click();
+        cy.byLegacyTestID(TEST_NS).click({ force: true });
+      }
+    });
+    cy.get('button.project-ssh-row__secret-name').click();
+    cy.get(tabModal).within(() => {
+      cy.get(addNew).click();
+      cy.dropFile('./fixtures/rsa.pub', 'rsa.pub', uploadSecret);
+      cy.wait(1000);
+      cy.get(secretName).type(testSecret);
+      cy.clickSaveBtn();
+    });
+    cy.byButtonText(testSecret).should('exist');
   });
 });
