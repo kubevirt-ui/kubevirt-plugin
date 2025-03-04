2305feccaee39da263058a5713ca04d78a0988b4
Guohua Ouyang <gouyang@redhat.com>
2025-03-04 17:38:03 +0800

    CNV-57355: add test for cluster general settings

--- cypress/tests/e2e/a-setup.cy.ts
+++ cypress/tests/e2e/a-setup.cy.ts
@@ -42,17 +42,20 @@ describe('Prepare the cluster for test', () => {
     cy.contains(manageKeysText).click();
     cy.contains(authSSHKey, { timeout: 20000 }).should('be.visible');
     cy.wait(2000);
-    // configure new public ssh key
-    cy.get('.settings-tab__content').then(() => {
-      cy.contains('Select project').click();
-      cy.byLegacyTestID(TEST_NS).click({ force: true });
-      cy.get('button.project-ssh-row__secret-name').click();
-      cy.get(useExisting).click();
-      cy.contains('Select secret').click();
-      cy.byButtonText(TEST_SECRET_NAME).click();
-      cy.clickSaveBtn();
+    cy.get('.settings-tab__content').then(($body) => {
+      if ($body.text().includes('Select project')) {
+        cy.contains('Select project').click();
+        cy.byLegacyTestID(TEST_NS).click({ force: true });
+      }
+      if ($body.text().includes('Not configured')) {
+        cy.get('button.project-ssh-row__secret-name').click();
+        cy.get(useExisting).click();
+        cy.contains('Select secret').click();
+        cy.byButtonText(TEST_SECRET_NAME).click();
+        cy.clickSaveBtn();
+      }
+      cy.contains('button.project-ssh-row__secret-name', TEST_SECRET_NAME).should('exist');
     });
-    cy.contains('button.project-ssh-row__secret-name', TEST_SECRET_NAME).should('exist');
   });
 
   it('create example VM', () => {
