6e5ebe42f8f71ac03d73882a9cc88901d1ec7f10
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-03-17 13:37:16 +0100

    Choose volume/access mode based on recommendations from StorageProfile
    Based on the feedback in CNV-55151 and in CNV-55147 change the current
    behavior in the following way:
    1. instead of "Apply optimized StorageProfile settings" section
       expose all the options send via storage profile API. The first
       (the most recommended according to storage profile docs) should be
       the default but the user should be allowed to change and even
       override the recommendation.
    2. drop the section based on static data
    3. the list of available modes visible will be enhanced with:
       a) label indicating the recommendation status( 3 levels):
          most recommended, recommended or not recommended
       b) tooltip providing more information - including recommendation rank
    4. the volume mode change triggers changes in the access mode
       recommendations - due to this fact the section was moved
       to the left side.
    
    Reference-Url: https://github.com/kubevirt/containerized-data-importer/blob/acbd44447305f9b92e742d0f3c3b4a1681edb2f8/doc/storageprofile.md?plain=1#L46
    Reference-Url: https://issues.redhat.com//browse/CNV-55151
    Reference-Url: https://issues.redhat.com/browse/CNV-55147
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/utils/components/DiskModal/components/StorageProfileSettings/ApplyStorageProfileSettings.tsx
+++ src/utils/components/DiskModal/components/StorageProfileSettings/ApplyStorageProfileSettings.tsx
@@ -1,19 +1,13 @@
 import React, { FC } from 'react';
 import { useFormContext } from 'react-hook-form';
 
-import { V1beta1StorageSpecVolumeModeEnum } from '@kubevirt-ui/kubevirt-api/kubevirt';
-import ApplyStorageProfileSettingsCheckbox from '@kubevirt-utils/components/ApplyStorageProfileSettingsCheckbox/ApplyStorageProfileSettingsCheckbox';
+import ErrorAlert from '@kubevirt-utils/components/ErrorAlert/ErrorAlert';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import useStorageProfileClaimPropertySets from '@kubevirt-utils/hooks/useStorageProfileClaimPropertySets';
-import { isEmpty } from '@kubevirt-utils/utils/utils';
-import { Flex, FlexItem } from '@patternfly/react-core';
+import { Flex, FlexItem, Skeleton } from '@patternfly/react-core';
 
 import { V1DiskFormState } from '../../utils/types';
-import {
-  ACCESS_MODE_FIELD,
-  DATAVOLUME_TEMPLATE_STORAGE,
-  STORAGE_PROFILE_SETTINGS_APPLIED_FIELD,
-  VOLUME_MODE_FIELD,
-} from '../utils/constants';
+import { DATAVOLUME_TEMPLATE_STORAGE } from '../utils/constants';
 
 import AccessMode from './AccessMode';
 import VolumeMode from './VolumeMode';
@@ -21,59 +15,34 @@ import VolumeMode from './VolumeMode';
 import './ApplyStorageProfileSettings.scss';
 
 const ApplyStorageProfileSettings: FC = () => {
-  const { setValue, watch } = useFormContext<V1DiskFormState>();
+  const { t } = useKubevirtTranslation();
+  const { watch } = useFormContext<V1DiskFormState>();
 
-  const [storageProfileSettingsApplied, storage] = watch([
-    STORAGE_PROFILE_SETTINGS_APPLIED_FIELD,
-    DATAVOLUME_TEMPLATE_STORAGE,
-  ]);
+  const [storage] = watch([DATAVOLUME_TEMPLATE_STORAGE]);
+  const { storageClassName = '' } = storage ?? {};
 
-  const { accessModes, storageClassName, volumeMode } = storage;
+  const { claimPropertySets, error, loaded } = useStorageProfileClaimPropertySets(storageClassName);
 
-  const { claimPropertySets, loaded: storageProfileLoaded } =
-    useStorageProfileClaimPropertySets(storageClassName);
+  if (!loaded) {
+    return <Skeleton screenreaderText={t('Loading StorageProfile')} />;
+  }
 
-  const handleApplyOptimizedSettingsChange = (checked: boolean) => {
-    setValue(STORAGE_PROFILE_SETTINGS_APPLIED_FIELD, checked);
-
-    if (checked) {
-      setValue(ACCESS_MODE_FIELD, null);
-      setValue(VOLUME_MODE_FIELD, null);
-      return;
-    }
-
-    const propertySet = claimPropertySets?.[0];
-
-    if (isEmpty(accessModes) && !isEmpty(propertySet?.accessModes[0])) {
-      setValue(ACCESS_MODE_FIELD, [propertySet?.accessModes?.[0]]);
-    }
-    if (isEmpty(volumeMode) && !isEmpty(propertySet?.volumeMode)) {
-      setValue(VOLUME_MODE_FIELD, propertySet?.volumeMode as V1beta1StorageSpecVolumeModeEnum);
-    }
-  };
+  if (loaded && error) {
+    return <ErrorAlert error={error} />;
+  }
 
   return (
-    <div>
-      <ApplyStorageProfileSettingsCheckbox
-        claimPropertySets={claimPropertySets}
-        disabled={!storageProfileLoaded || !claimPropertySets || isEmpty(claimPropertySets)}
-        handleChange={handleApplyOptimizedSettingsChange}
-        isChecked={storageProfileSettingsApplied}
-      />
-      {!storageProfileSettingsApplied && (
-        <Flex
-          className="ApplyStorageProfileSettings--volume-access-section"
-          spaceItems={{ default: 'spaceItems3xl' }}
-        >
-          <FlexItem>
-            <AccessMode />
-          </FlexItem>
-          <FlexItem>
-            <VolumeMode />
-          </FlexItem>
-        </Flex>
-      )}
-    </div>
+    <Flex
+      className="ApplyStorageProfileSettings--volume-access-section"
+      spaceItems={{ default: 'spaceItems3xl' }}
+    >
+      <FlexItem>
+        <VolumeMode claimPropertySets={claimPropertySets ?? []} />
+      </FlexItem>
+      <FlexItem>
+        <AccessMode claimPropertySets={claimPropertySets ?? []} />
+      </FlexItem>
+    </Flex>
   );
 };
 
