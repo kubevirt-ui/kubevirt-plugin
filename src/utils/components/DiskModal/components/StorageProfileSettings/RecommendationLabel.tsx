f8a4ad7520d39ccdce71bb96a234b57d4e5528f8
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-03-17 13:37:16 +0100

    Choose volume/access mode based on recommendations from StorageProfile
    Based on the feedback in CNV-55151 and in CNV-55147 change the current
    behavior in the following way:
    1. instead of "Apply optimized StorageProfile settings" section
       expose all the options send via storage profile API. The first
       (the most recommended according to storage profile docs) should be
       the default but the user should be allowed to change and even
       override the recommendation.
    2. drop the section based on static data
    3. the list of available modes visible will be enhanced with:
       a) label indicating the recommendation status( 3 levels):
          most recommended, recommended or not recommended
       b) tooltip providing more information - including recommendation rank
    4. the volume mode change triggers changes in the access mode
       recommendations - due to this fact the section was moved
       to the left side.
    
    Reference-Url: https://github.com/kubevirt/containerized-data-importer/blob/acbd44447305f9b92e742d0f3c3b4a1681edb2f8/doc/storageprofile.md?plain=1#L46
    Reference-Url: https://issues.redhat.com//browse/CNV-55151
    Reference-Url: https://issues.redhat.com/browse/CNV-55147
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

---
+++ src/utils/components/DiskModal/components/StorageProfileSettings/RecommendationLabel.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import { Label, Tooltip } from '@patternfly/react-core';
+
+import { fromPriorityToLabels } from './utils';
+
+type RecommendationLabelProps = {
+  priority: number;
+  recommendationCount: number;
+};
+
+const RecommendationLabel: FC<RecommendationLabelProps> = ({ priority, recommendationCount }) => {
+  const { t } = useKubevirtTranslation();
+  const { className, label, tooltip } = fromPriorityToLabels(priority, recommendationCount, t);
+
+  return (
+    <Tooltip content={tooltip}>
+      <Label className={className} isCompact variant="outline">
+        {label}
+      </Label>
+    </Tooltip>
+  );
+};
+
+export default RecommendationLabel;
