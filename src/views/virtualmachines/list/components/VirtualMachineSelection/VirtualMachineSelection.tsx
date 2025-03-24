3122219ab948cf1956e1e25ddce387bbd0a0fe82
adamviktora <viktora.adam@gmail.com>
2025-03-24 14:23:02 +0100

    CNV-57968: fix selected VMs state after deletion from CLI

--- src/views/virtualmachines/list/components/VirtualMachineSelection/VirtualMachineSelection.tsx
+++ src/views/virtualmachines/list/components/VirtualMachineSelection/VirtualMachineSelection.tsx
@@ -11,8 +11,9 @@ import {
   MenuToggle,
   MenuToggleElement,
 } from '@patternfly/react-core';
+import useExistingSelectedVMs from '@virtualmachines/list/hooks/useExistingSelectedVMs';
 
-import { deselectAll, selectAll, selectedVMs } from '../../selectedVMs';
+import { deselectAll, selectAll } from '../../selectedVMs';
 
 import './virtual-machine-selection.scss';
 
@@ -25,6 +26,7 @@ type VirtualMachineSelectionProps = {
 const VirtualMachineSelection: FC<VirtualMachineSelectionProps> = ({ loaded, pagination, vms }) => {
   const { t } = useKubevirtTranslation();
   const [isOpen, setIsOpen] = useState(false);
+  const existingSelectedVMs = useExistingSelectedVMs(vms);
 
   if (!loaded) return null;
 
@@ -42,8 +44,8 @@ const VirtualMachineSelection: FC<VirtualMachineSelectionProps> = ({ loaded, pag
 
   const numCurrentPageVMs = currentPageVMs.length;
 
-  const partiallyChecked = !isEmpty(selectedVMs.value) && selectedVMs.value.length !== vms.length;
-  const isChecked = selectedVMs.value.length === vms.length;
+  const isChecked = existingSelectedVMs.length === vms.length;
+  const partiallyChecked = !isChecked && !isEmpty(existingSelectedVMs);
 
   return (
     <Dropdown
@@ -56,8 +58,8 @@ const VirtualMachineSelection: FC<VirtualMachineSelectionProps> = ({ loaded, pag
         >
           <Checkbox
             label={
-              !isEmpty(selectedVMs.value)
-                ? t('{{length}} selected', { length: selectedVMs.value.length })
+              !isEmpty(existingSelectedVMs)
+                ? t('{{length}} selected', { length: existingSelectedVMs.length })
                 : null
             }
             onChange={(event, checked) => {
