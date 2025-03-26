59c40a666e9c4991f28618e2ba6e3cc92e9552ad
Phillip Rhodes <pcbailey@users.noreply.github.com>
2025-03-26 16:22:58 -0400

    Add topology.getDataModel and topology.isResourceDepicted

--- src/views/topology/utils/constants.ts
+++ src/views/topology/utils/constants.ts
@@ -1,1 +1,17 @@
 export const TYPE_VIRTUAL_MACHINE = 'virtual-machine';
+
+export const WORKLOAD_TYPES = [
+  'deployments',
+  'deploymentConfigs',
+  'daemonSets',
+  'statefulSets',
+  'jobs',
+  'cronJobs',
+  'pods',
+];
+
+export const DEFAULT_NODE_PAD = 20;
+
+export const NODE_WIDTH = 104;
+export const NODE_HEIGHT = 104;
+export const NODE_PADDING = [0, DEFAULT_NODE_PAD];
