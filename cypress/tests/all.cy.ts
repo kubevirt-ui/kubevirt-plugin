1e9c1a1bd021c27e7e9d8774a1954f2d9738751f
Leon Kladnitsky <lkladnit@redhat.com>
2025-03-31 17:46:58 +0300

    Templates filtering tests

--- cypress/tests/all.cy.ts
+++ cypress/tests/all.cy.ts
@@ -4,3 +4,4 @@ import './e2e/check-tab-yaml.cy.ts';
 import './e2e/catalog-filter.cy.ts';
 import './e2e/create-vm.cy.ts';
 import './e2e/d-networking.cy.ts';
+import './e2e/template-list.cy.ts';
---
+++ cypress/tests/e2e/template-list.cy.ts
@@ -0,0 +1,77 @@
+import { ALL_PROJ_NS } from '../../utils/const/index';
+import { TEMPLATE } from '../../utils/const/template';
+import { catalogNav } from '../../views/selector';
+import * as tView from '../../views/selector-template';
+
+describe('Test template list page', () => {
+  before(() => {
+    cy.visit('');
+    cy.switchToVirt();
+  });
+
+  beforeEach(() => {
+    cy.clickVirtLink(catalogNav);
+    cy.visitTemplates();
+  });
+
+  it('filter template by name', () => {
+    cy.switchProject(ALL_PROJ_NS);
+    cy.get(tView.itemFilter).type(TEMPLATE.CENTOSSTREAM9.metadataName);
+    cy.byLegacyTestID(TEMPLATE.CENTOSSTREAM9.metadataName).should('exist');
+    cy.byLegacyTestID(TEMPLATE.CENTOSSTREAM10.metadataName).should('not.exist');
+  });
+
+  it('filter default templates', () => {
+    cy.get(tView.dropdownFilter).click();
+    cy.get('[data-test-row-filter="is-default"]')
+      .find('input[type="checkbox"]')
+      .check({ force: true });
+    cy.byLegacyTestID(TEMPLATE.RHEL9.metadataName).should('exist');
+    cy.byLegacyTestID(TEMPLATE.FEDORA.metadataName).should('exist');
+    cy.byLegacyTestID(TEMPLATE.WIN10.metadataName).should('exist');
+  });
+
+  it('filter templates by OS', () => {
+    cy.get(tView.dropdownFilter).click();
+    cy.get('[data-test-row-filter="windows"]')
+      .find('input[type="checkbox"]')
+      .check({ force: true });
+    cy.byLegacyTestID(TEMPLATE.RHEL9.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.RHEL8.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.RHEL7.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.WIN2K22.metadataName).should('exist');
+  });
+
+  it('filter templates by Provider', () => {
+    cy.get(tView.dropdownFilter).click();
+    cy.get('[data-test-row-filter="Other"]').find('input[type="checkbox"]').check({ force: true });
+    cy.byLegacyTestID(TEMPLATE.RHEL9.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.RHEL8.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.RHEL7.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.WIN10.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.WIN2K19.metadataName).should('not.exist');
+  });
+
+  it('template without auto-bootsource should have no available label', () => {
+    cy.get(tView.itemFilter).type('windows');
+    cy.contains('Source available').should('not.exist');
+  });
+
+  it('filter templates by boot source', () => {
+    cy.get(tView.dropdownFilter).click();
+    cy.get('[data-test-row-filter="hide-deprecated-templates"]')
+      .find('input[type="checkbox"]')
+      .check({ force: true });
+    cy.get('[data-test-row-filter="is-default"]')
+      .find('input[type="checkbox"]')
+      .check({ force: true });
+    cy.get('[data-test-row-filter="available"]')
+      .find('input[type="checkbox"]')
+      .check({ force: true });
+    cy.get(tView.dropdownFilter).click();
+    cy.byLegacyTestID(TEMPLATE.CENTOSSTREAM9.metadataName).should('exist');
+    cy.byLegacyTestID(TEMPLATE.FEDORA.metadataName).should('exist');
+    cy.byLegacyTestID(TEMPLATE.RHEL7.metadataName).should('not.exist');
+    cy.byLegacyTestID(TEMPLATE.WIN10.metadataName).should('not.exist');
+  });
+});
