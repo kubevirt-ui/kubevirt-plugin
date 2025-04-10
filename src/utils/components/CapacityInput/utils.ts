2311fd05828f65610a8148a34dd455720cc78429
adamviktora <viktora.adam@gmail.com>
2025-04-10 12:40:17 +0200

    CNV-59559: fix disk modal CapacityInput

--- src/utils/components/CapacityInput/utils.ts
+++ src/utils/components/CapacityInput/utils.ts
@@ -5,3 +5,13 @@ export enum CAPACITY_UNITS {
 }
 
 export const removeByteSuffix = (quantity: string): string => quantity?.replace(/[Bb]/, '');
+
+export const getValueFromSize = (size: string) => {
+  const [sizeValue = 0] = size?.replace(/,/g, '').match(/[0-9]+/g) || [];
+  return Number(sizeValue);
+};
+
+export const getUnitFromSize = (size: string) => {
+  const [unitValue = ''] = size?.match(/[a-zA-Z]+/g) || [];
+  return (!unitValue?.endsWith('B') ? `${unitValue}B` : unitValue) as CAPACITY_UNITS;
+};
