dcb04159310a8440c6e73469f26a756e820eede8
adamviktora <viktora.adam@gmail.com>
2025-04-10 12:40:17 +0200

    CNV-59559: fix disk modal CapacityInput

--- src/utils/components/DiskModal/components/DiskSizeInput/ExpandPVC.tsx
+++ src/utils/components/DiskModal/components/DiskSizeInput/ExpandPVC.tsx
@@ -3,11 +3,17 @@ import { useFormContext } from 'react-hook-form';
 
 import { IoK8sApiCoreV1PersistentVolumeClaim } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import CapacityInput from '@kubevirt-utils/components/CapacityInput/CapacityInput';
+import {
+  getUnitFromSize,
+  getValueFromSize,
+  removeByteSuffix,
+} from '@kubevirt-utils/components/CapacityInput/utils';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { convertToBaseValue, humanizeBinaryBytes } from '@kubevirt-utils/utils/humanize.js';
 
 import { V1DiskFormState } from '../../utils/types';
 import { EXPAND_PVC_SIZE } from '../utils/constants';
+
+import usePVCDefaultSize from './usePVCDefaultSize';
 
 type ExpandPVCProps = { pvc: IoK8sApiCoreV1PersistentVolumeClaim };
 
@@ -19,23 +25,39 @@ const ExpandPVC: FC<ExpandPVCProps> = ({ pvc }) => {
   const expandPVCSize = diskState.expandPVCSize;
 
   const pvcSize = pvc?.spec?.resources?.requests?.storage;
+  const { minSizes, selectedUnit, setSelectedUnit } = usePVCDefaultSize(pvcSize);
 
   const onQuantityChange = (quantity: string) => {
-    const newQuantityValue = convertToBaseValue(quantity)?.toString();
+    const newUnit = getUnitFromSize(quantity);
 
-    setValue(EXPAND_PVC_SIZE, newQuantityValue > pvcSize ? quantity : null);
+    if (newUnit !== selectedUnit) {
+      setSelectedUnit(newUnit);
+
+      const newSize = minSizes[newUnit];
+      if (newSize.value > getValueFromSize(quantity)) {
+        const newPvcSize = `${Math.ceil(newSize.value)}${removeByteSuffix(newSize.unit)}`;
+
+        setValue(EXPAND_PVC_SIZE, newPvcSize);
+        return;
+      }
+    }
+
+    setValue(EXPAND_PVC_SIZE, quantity);
   };
 
   if (!pvcSize) return null;
 
-  const size = expandPVCSize || humanizeBinaryBytes(convertToBaseValue(pvcSize)).string;
+  const minSize = minSizes[selectedUnit];
+  const size = expandPVCSize || minSize.string;
 
-  const isMinusDisabled = pvcSize === convertToBaseValue(expandPVCSize) || !expandPVCSize;
+  const isMinusDisabled =
+    Math.ceil(minSize.value) >= getValueFromSize(expandPVCSize) || !expandPVCSize;
 
   return (
     <CapacityInput
       isMinusDisabled={isMinusDisabled}
       label={t('PersistentVolumeClaim size')}
+      minValue={minSize.value}
       onChange={onQuantityChange}
       size={size}
     />
