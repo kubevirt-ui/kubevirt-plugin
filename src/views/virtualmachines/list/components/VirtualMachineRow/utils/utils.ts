647e225e6ebbc8793b7e37df66b9c4e4007df658
Phillip Rhodes <pcbailey@users.noreply.github.com>
2025-05-06 09:42:07 -0400

    Show LiveMigratable=True label in VMs list

---
+++ src/views/virtualmachines/list/components/VirtualMachineRow/utils/utils.ts
@@ -0,0 +1,14 @@
+import { V1VirtualMachineCondition } from '@kubevirt-ui/kubevirt-api/kubevirt';
+
+const isLiveMigratableCondition = (condition: V1VirtualMachineCondition) =>
+  condition?.type === 'LiveMigratable' && condition?.status === 'True';
+
+const isNotLiveMigratableCondition = (condition: V1VirtualMachineCondition) =>
+  condition?.type === 'LiveMigratable' && condition?.status === 'False';
+
+export const filterConditions = (conditions: V1VirtualMachineCondition[]) =>
+  conditions?.filter(
+    (condition) =>
+      isLiveMigratableCondition(condition) ||
+      (condition?.reason && !isNotLiveMigratableCondition(condition)),
+  );
