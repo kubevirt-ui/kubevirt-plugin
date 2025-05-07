288b343b7f167b4858af192b6ffb75d5a61d17aa
Phillip Rhodes <pcbailey@users.noreply.github.com>
2025-05-06 20:59:51 -0400

    Display disk size from DataVolume over PVC for VM disks list

--- src/utils/resources/bootableresources/selectors.ts
+++ src/utils/resources/bootableresources/selectors.ts
@@ -1,7 +1,9 @@
 import {
   V1beta1DataSource,
+  V1beta1DataVolume,
   V1beta1DataVolumeSourcePVC,
 } from '@kubevirt-ui/kubevirt-api/containerized-data-importer/models';
+import { IoK8sApiCoreV1PersistentVolumeClaim } from '@kubevirt-ui/kubevirt-api/kubernetes';
 import { VolumeSnapshotKind } from '@kubevirt-utils/components/SelectSnapshot/types';
 
 export const getDataSourcePVCSource = (dataSource: V1beta1DataSource): V1beta1DataVolumeSourcePVC =>
@@ -12,3 +14,18 @@ export const getVolumeSnapshotSize = (volumeSnapshot: VolumeSnapshotKind) =>
 
 export const getVolumeSnapshotStorageClass = (volumeSnapshot: VolumeSnapshotKind) =>
   volumeSnapshot?.spec?.volumeSnapshotClassName;
+
+export const getPVCSize = (pvc: IoK8sApiCoreV1PersistentVolumeClaim) =>
+  pvc?.spec?.resources?.requests?.storage;
+
+export const getPVCStorageClassName = (pvc: IoK8sApiCoreV1PersistentVolumeClaim) =>
+  pvc?.spec?.storageClassName;
+
+export const getDataVolumeSize = (dataVolume: V1beta1DataVolume) =>
+  dataVolume?.spec?.storage?.resources?.requests?.storage;
+
+export const getDataVolumeStorageClassName = (dataVolume: V1beta1DataVolume) =>
+  dataVolume?.spec?.storage?.storageClassName;
+
+export const getPhase = (volume: IoK8sApiCoreV1PersistentVolumeClaim | V1beta1DataVolume) =>
+  volume?.status?.phase;
