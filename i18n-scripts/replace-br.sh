bdf86ccd296fec586b6ee214daf3b34eca55c96e
Ugo Palatucci <upalatuc@redhat.com>
2025-05-14 15:07:23 +0200

    Add translations

--- i18n-scripts/replace-br.sh
+++ i18n-scripts/replace-br.sh
@@ -1,1 +1,8 @@
-sed -i -r "s|<br />|<br/>|g" ./locales/en/plugin__kubevirt-plugin.json
+#!/bin/bash
+
+sed -i -r "s|<br />|<br/>|g" ./locales/en/plugin__kubevirt-plugin.json \
+./locales/es/plugin__kubevirt-plugin.json \
+./locales/fr/plugin__kubevirt-plugin.json \
+./locales/ja/plugin__kubevirt-plugin.json \
+./locales/ko/plugin__kubevirt-plugin.json \
+./locales/zh/plugin__kubevirt-plugin.json
