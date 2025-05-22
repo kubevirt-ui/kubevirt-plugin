88d3ab7b8f6eb5e20e7089df33089466e18d4a63
Scott J Dickerson <sdickers@redhat.com>
2025-05-22 17:29:57 -0400

    ci: e2e test changes so they can run against a crc cluster
      - On crc, the console login auto populates "developer". Clear
        the field's contents before typing in the login name.
    
      - On the virt perspective switch, just scroll the window to the
        document top instead of relying on an element to scroll into view.
    
      - git ignore the kubectl download that the `deploy-kubevirt-gating.sh`
        script downloads.
    
    Signed-off-by: Scott J Dickerson <sdickers@redhat.com>

--- cypress/tests/upstream/a-setup.cy.ts
+++ cypress/tests/upstream/a-setup.cy.ts
@@ -1,6 +1,6 @@
 import { TEST_NS, TEST_SECRET_NAME } from '../../utils/const/index';
 import { authSSHKey, YAML } from '../../utils/const/string';
-import { itemCreateBtn, mastheadLogo, saveBtn } from '../../views/selector';
+import { itemCreateBtn, saveBtn } from '../../views/selector';
 import { manageKeysText, useExisting } from '../../views/selector-catalog';
 import { tab } from '../../views/tab';
 
@@ -13,7 +13,7 @@ describe('Prepare the cluster for test', () => {
   });
 
   it('switch to Virtualization perspective and default project', () => {
-    cy.get(mastheadLogo).scrollIntoView();
+    cy.scrollTo('top');
     cy.switchToVirt();
     cy.switchProject(TEST_NS);
   });
