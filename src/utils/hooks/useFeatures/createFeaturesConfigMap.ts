6818234984d95fa0538ee94a1f4261416571a9b0
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-07-02 23:13:08 +0200

    Fix error handling in re-creating features config map
    1. handle rejected update promise - the catch statement was not
       part of async flow
    2. prevent subsequent creation attempts if the first atempt failed
    3. prevent creation attempts if the first attempt is in progress
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

---
+++ src/utils/hooks/useFeatures/createFeaturesConfigMap.ts
@@ -0,0 +1,43 @@
+import { ConfigMapModel, RoleBindingModel, RoleModel } from '@kubevirt-ui/kubevirt-api/console';
+import {
+  IoK8sApiCoreV1ConfigMap,
+  IoK8sApiRbacV1Role,
+  IoK8sApiRbacV1RoleBinding,
+} from '@kubevirt-ui/kubevirt-api/kubernetes';
+import { k8sCreate, k8sPatch } from '@openshift-console/dynamic-plugin-sdk';
+
+import { featuresConfigMapInitialState, featuresRole, featuresRoleBinding } from './constants';
+
+export const createFeaturesConfigMap = async () => {
+  await k8sCreate<IoK8sApiCoreV1ConfigMap>({
+    data: featuresConfigMapInitialState,
+    model: ConfigMapModel,
+  });
+
+  await k8sCreate<IoK8sApiRbacV1Role>({
+    data: featuresRole,
+    model: RoleModel,
+  });
+
+  await k8sCreate<IoK8sApiRbacV1RoleBinding>({
+    data: featuresRoleBinding,
+    model: RoleBindingModel,
+  });
+};
+
+export const applyMissingFeatures = async (
+  featureName: string,
+  featureConfigMap: IoK8sApiCoreV1ConfigMap,
+) => {
+  await k8sPatch({
+    data: [
+      {
+        op: 'replace',
+        path: `/data/${featureName}`,
+        value: featuresConfigMapInitialState.data[featureName],
+      },
+    ],
+    model: ConfigMapModel,
+    resource: featureConfigMap,
+  });
+};
--- src/utils/hooks/useFeatures/useFeaturesConfigMap.ts
+++ src/utils/hooks/useFeatures/useFeaturesConfigMap.ts
@@ -11,20 +11,22 @@ import { useIsAdmin } from '../useIsAdmin';
 
 import { FEATURES_CONFIG_MAP_NAME } from './constants';
 
-type UseFeaturesConfigMap = () => {
+type UseFeaturesConfigMap = (enableQuery?: boolean) => {
   featuresConfigMapData: WatchK8sResult<IoK8sApiCoreV1ConfigMap>;
   isAdmin: boolean;
 };
 
-const useFeaturesConfigMap: UseFeaturesConfigMap = () => {
+const useFeaturesConfigMap: UseFeaturesConfigMap = (enableQuery: boolean = true) => {
   const isAdmin = useIsAdmin();
 
-  const featuresConfigMapData = useK8sWatchResource<IoK8sApiCoreV1ConfigMap>({
-    groupVersionKind: getGroupVersionKindForModel(ConfigMapModel),
-    isList: false,
-    name: FEATURES_CONFIG_MAP_NAME,
-    namespace: DEFAULT_OPERATOR_NAMESPACE,
-  });
+  const featuresConfigMapData = useK8sWatchResource<IoK8sApiCoreV1ConfigMap>(
+    enableQuery && {
+      groupVersionKind: getGroupVersionKindForModel(ConfigMapModel),
+      isList: false,
+      name: FEATURES_CONFIG_MAP_NAME,
+      namespace: DEFAULT_OPERATOR_NAMESPACE,
+    },
+  );
   return { featuresConfigMapData: [...featuresConfigMapData], isAdmin };
 };
 
