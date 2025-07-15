4ea5fc24ff2412d6cdb241cf9069b6ead19cb6ce
batyana <bnahmias@redhat.com>
2025-07-15 15:28:37 +0300

    CNV-62352: fixing the help text to reflect RHEL9+ supporting dynamic ssh +translations
    Signed-off-by: batyana <bnahmias@redhat.com>

--- src/utils/components/DynamicSSHKeyInjection/DynamicSSHKeyInjectionHelpTextIcon.tsx
+++ src/utils/components/DynamicSSHKeyInjection/DynamicSSHKeyInjectionHelpTextIcon.tsx
@@ -17,10 +17,10 @@ const DynamicSSHKeyInjectionHelpTextIcon: FC<DynamicSSHKeyInjectionHelpTextIconP
       bodyContent={
         isDisabled
           ? t(
-              'Before you boot the VirtualMachine (VM) for the first time, add a public SSH key. Only RHEL 9 supports dynamic key injection, and in RHEL 9 you must enable dynamic SSH when you create the VM.',
+              'Before you boot the VirtualMachine (VM) for the first time, add a public SSH key. Only RHEL 9+ versions support dynamic key injection, and you must enable dynamic SSH when you create the VM.',
             )
           : t(
-              'Requires RHEL 9. If enabled, SSH keys can be changed while the VirtualMachine is running. Otherwise, the VirtualMachine inherits the key injection setting of its image.',
+              'Requires RHEL versions 9+. If enabled, SSH keys can be changed while the VirtualMachine is running. Otherwise, the VirtualMachine inherits the key injection setting of its image.',
             )
       }
       className="dynamic-ssh-key-help-text-icon"
