632c4bace401a311d5c88b0b33864434725c0222
Avi Avissar <aavissar@aavissar-thinkpadp16vgen1.raanaii.csb>
2025-08-07 14:15:57 +0300

    CNV-65987: Missing i18n key at Virtualization console
    Signed-off-by: Avi Avissar <aavissar@aavissar-thinkpadp16vgen1.raanaii.csb>

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -2,7 +2,7 @@
 // labels declared in console extensions are added to the message catalog.
 
 // TODO: We should make the custom i18next JSON lexer available to plugins so
-// that they do not need this file.
+// that they dont need this file.
 
 // t('plugin__kubevirt-plugin~Cluster')
 // t('plugin__kubevirt-plugin~Virtualization')
@@ -42,3 +42,4 @@
 // t('plugin__kubevirt-plugin~Create a Virtual Machine from a template')
 // t('plugin__kubevirt-plugin~Hardware Devices')
 // t('plugin__kubevirt-plugin~Storage MigrationPlans')
+// t('plugin__kubevirt-plugin~Cross cluster migration')
