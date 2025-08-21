8c947936816c23c24999851896ed76bbd84a19aa
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-08-21 13:13:20 +0200

    Separate detected pending changes from conditions set on the VM
    Before this PR the UI first detected the changes (i.e. added NIC) and
    then tried to guess what action is needed to make the change persistent.
    Possible choices were: manual restart or manual live migration.
    
    With CNV-58395 the RestartRequired condition is set for all changes
    that are classified as not hot-pluggable. In 4.20 also the LiveUpdate
    vmRolloutStrategy is the default which in theory removes the need of
    manual live migration. However after this improvements there are still
    some open points:
    1. RestartRequired or MigrationRequired conditions are not linked with
       a particular change and are added  with a delay.
    2. Detection logic in the UI might be outdated or incorrect.
    3. The cluster may use vmRolloutStrategy Stage
    4. automatic migration might by stuck for a longer time
    
    In this PR the above points are addressed:
    1. RestartRequired condition is always shown (regardless of the detected
       changes)
    2. detected pending changes in networking area are always shown but
       without any suggested action. It's purely information that VM and
       VMI have different spec.
    3. if cluster is using LiveUpdate strategy the information about pending
       changes will automatically disappear after the changes are applied.
    4. if the cluster is using Stage strategy or if the automatic migration
       is blocked then the user will need to handle the changes manually
    
    Fixed scenarios:
    1. changing NetworkAttachementDefinition - CNV-58657
    2. changing network model i.e. virtio -> e1000e
    3. all hot-plug changes (adding bridge/SR-IOV, removing bridge)
    
    Reference-Url: https://issues.redhat.com/browse/CNV-58657
    Reference-Url: https://issues.redhat.com/browse/CNV-58395
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/views/virtualmachines/details/VirtualMachinePendingChangesAlert/VirtualMachinePendingChangesAlert.tsx
+++ src/views/virtualmachines/details/VirtualMachinePendingChangesAlert/VirtualMachinePendingChangesAlert.tsx
@@ -3,15 +3,15 @@ import React, { FC } from 'react';
 import { V1VirtualMachine, V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { usePendingChanges } from '@kubevirt-utils/components/PendingChanges/hooks/usePendingChanges';
 import { PendingChangesAlert } from '@kubevirt-utils/components/PendingChanges/PendingChangesAlert/PendingChangesAlert';
+import { getPendingChangesByTab } from '@kubevirt-utils/components/PendingChanges/utils/helpers';
+import { VirtualMachineDetailsTab } from '@kubevirt-utils/constants/tabs-constants';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { getStatusConditionByType } from '@kubevirt-utils/resources/vm';
 import { VirtualMachineStatusConditionTypes } from '@kubevirt-utils/resources/vm/utils/constants';
-import { isEmpty } from '@kubevirt-utils/utils/utils';
-import { Stack, StackItem } from '@patternfly/react-core';
-import LiveMigratePendingChanges from '@virtualmachines/details/VirtualMachinePendingChangesAlert/LiveMigratePendingChanges';
-import RestartPendingChanges from '@virtualmachines/details/VirtualMachinePendingChangesAlert/RestartPendingChanges';
+import PendingChanges from '@virtualmachines/details/VirtualMachinePendingChangesAlert/PendingChanges';
 import { isRunning } from '@virtualmachines/utils';
 
-import { splitPendingChanges } from '../utils/utils';
+import RestartPendingChanges from './RestartPendingChanges';
 
 type VirtualMachinePendingChangesAlertProps = {
   instanceTypeExpandedSpec: V1VirtualMachine;
@@ -24,36 +24,38 @@ const VirtualMachinePendingChangesAlert: FC<VirtualMachinePendingChangesAlertPro
   vmi,
 }) => {
   const pendingChanges = usePendingChanges(vm, vmi);
+  const { t } = useKubevirtTranslation();
 
-  const hasPendingChanges = pendingChanges?.some((change) => change?.hasPendingChange);
+  const pendingChangesNICsTab = getPendingChangesByTab(
+    pendingChanges ?? [],
+    VirtualMachineDetailsTab.Network,
+  );
 
-  if (!vmi || !isRunning(vm) || !hasPendingChanges) {
-    return null;
-  }
-
-  const { liveMigrationChanges, restartChanges } = splitPendingChanges(pendingChanges);
   const restartRequiredCondition = getStatusConditionByType(
     vm,
     VirtualMachineStatusConditionTypes.RestartRequired,
   );
 
+  const isRestartRequired = restartRequiredCondition?.status === 'True';
+
+  if (!vmi || !isRunning(vm) || (!isRestartRequired && !pendingChangesNICsTab.length)) {
+    return null;
+  }
   return (
-    <PendingChangesAlert isExpandable isWarning>
-      <Stack hasGutter>
-        {!isEmpty(liveMigrationChanges) && (
-          <StackItem>
-            <LiveMigratePendingChanges pendingChanges={liveMigrationChanges} />
-          </StackItem>
-        )}
-        {!isEmpty(restartChanges) && (
-          <StackItem>
-            <RestartPendingChanges
-              pendingChanges={restartChanges}
-              restartRequiredCondition={restartRequiredCondition}
-            />
-          </StackItem>
-        )}
-      </Stack>
+    <PendingChangesAlert
+      isExpandable
+      isWarning
+      title={isRestartRequired ? t('Restart required') : undefined}
+    >
+      {isRestartRequired && (
+        <RestartPendingChanges
+          pendingChanges={pendingChanges}
+          restartRequiredCondition={restartRequiredCondition}
+        />
+      )}
+      {!isRestartRequired && pendingChangesNICsTab.length > 0 && (
+        <PendingChanges pendingChanges={pendingChangesNICsTab} />
+      )}
     </PendingChangesAlert>
   );
 };
