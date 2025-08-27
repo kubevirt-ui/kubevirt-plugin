a74f2106e6d1fb804a5bd79269f03af9aedb8500
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-08-27 19:56:30 +0200

    Validate NAD name according to rules for DNS Label
    Follow the definition of a label in DNS (RFC 1123)
    
    Reference-Url: https://github.com/kubernetes/kubernetes/blob/8d7d7a5e0d4d7e75f5a860574346944b8cc0fc43/staging/src/k8s.io/apimachinery/pkg/util/validation/validation.go#L107-L124
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- src/utils/components/NetworkInterfaceModal/components/NetworkInterfaceNetworkSelect/utils.tsx
+++ src/utils/components/NetworkInterfaceModal/components/NetworkInterfaceNetworkSelect/utils.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TFunction } from 'react-i18next';
 
 import { interfaceTypesProxy } from '@kubevirt-utils/resources/vm/utils/network/constants';
+import { parseDNS1123Label } from '@kubevirt-utils/utils/validation';
 import { HelperText, HelperTextItem, Label, SelectOptionProps } from '@patternfly/react-core';
 import { InfoIcon } from '@patternfly/react-icons';
 
@@ -26,6 +27,23 @@ export const getCreateNetworkOption = (input: string, t: TFunction): SelectOptio
           <HelperTextItem icon={<InfoIcon />} variant="indeterminate">
             {t('Type namespace/network-name to use a different network')}
           </HelperTextItem>
+        </HelperText>
+      ),
+      isDisabled: true,
+    };
+  }
+
+  const { getErrorMsg } =
+    // both namespace and NAD name are DNS labels
+    input
+      .split('/')
+      .map((part) => parseDNS1123Label(part))
+      .find(({ isValid }) => !isValid) ?? {};
+  if (getErrorMsg) {
+    return {
+      children: (
+        <HelperText>
+          <HelperTextItem variant="error">{getErrorMsg(t)}</HelperTextItem>
         </HelperText>
       ),
       isDisabled: true,
