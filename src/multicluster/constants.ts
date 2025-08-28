6a2c2ab0fee0ce3bfe4024521b898da7212a0590
Ugo Palatucci <upalatuc@redhat.com>
2025-08-28 15:37:34 +0200

    Fix web console standalone url

--- src/multicluster/constants.ts
+++ src/multicluster/constants.ts
@@ -2,6 +2,8 @@ import { K8sModel } from '@openshift-console/dynamic-plugin-sdk';
 
 export const BASE_K8S_API_PATH = '/api/kubernetes';
 
+export const KUBEVIRT_VM_PATH = 'kubevirt.io~v1~VirtualMachine';
+
 export const FLAG_KUBEVIRT_DYNAMIC_ACM = 'KUBEVIRT_DYNAMIC_ACM';
 export const FEATURE_KUBEVIRT_ACM_TREEVIEW = 'kubevirtACMTreeview';
 
--- src/multicluster/urls.ts
+++ src/multicluster/urls.ts
@@ -4,22 +4,24 @@ import { ResourceRouteHandler } from '@stolostron/multicluster-sdk';
 
 import { VirtualMachineModel } from '../views/dashboard-extensions/utils';
 
+import { KUBEVIRT_VM_PATH } from './constants';
+
 export const isACMPath = (pathname: string): boolean =>
   pathname.startsWith('/k8s/cluster') || pathname.startsWith('/k8s/all-clusters');
 
 export const getACMVMURL = (cluster: string, namespace: string, name: string): string =>
-  `/k8s/cluster/${cluster}/ns/${namespace}/kubevirt.io~v1~VirtualMachine/${name}`;
+  `/k8s/cluster/${cluster}/ns/${namespace}/${KUBEVIRT_VM_PATH}/${name}`;
 
 export const getACMVMListURL = (cluster?: string): string =>
   cluster
-    ? `/k8s/cluster/${cluster}/all-namespaces/kubevirt.io~v1~VirtualMachine`
-    : `/k8s/all-clusters/all-namespaces/kubevirt.io~v1~VirtualMachine`;
+    ? `/k8s/cluster/${cluster}/all-namespaces/${KUBEVIRT_VM_PATH}`
+    : `/k8s/all-clusters/all-namespaces/${KUBEVIRT_VM_PATH}`;
 
 export const getACMVMSearchURL = (): string =>
-  `/k8s/all-clusters/all-namespaces/kubevirt.io~v1~VirtualMachine/search`;
+  `/k8s/all-clusters/all-namespaces/${KUBEVIRT_VM_PATH}/search`;
 
 export const getACMVMListNamespacesURL = (cluster: string, namespace: string): string =>
-  `/k8s/cluster/${cluster}/ns/${namespace}/kubevirt.io~v1~VirtualMachine`;
+  `/k8s/cluster/${cluster}/ns/${namespace}/${KUBEVIRT_VM_PATH}`;
 
 export const getCatalogURL = (cluster: string, namespace?: string): string => {
   const namespacePath = namespace ? `ns/${namespace}` : ALL_NAMESPACES;
@@ -27,6 +29,16 @@ export const getCatalogURL = (cluster: string, namespace?: string): string => {
   return cluster
     ? `/k8s/cluster/${cluster}/${namespacePath}/catalog`
     : `/k8s/${namespacePath}/catalog`;
+};
+
+export const getConsoleStandaloneURL = (
+  namespace: string,
+  name: string,
+  cluster?: string,
+): string => {
+  const commonPath = `/ns/${namespace}/${KUBEVIRT_VM_PATH}/${name}/console/standalone`;
+  const clusterPath = cluster ? `/cluster/${cluster}` : '';
+  return `/k8s${clusterPath}${commonPath}`;
 };
 
 export const getVMURL = (cluster: string, namespace: string, name: string): string =>
