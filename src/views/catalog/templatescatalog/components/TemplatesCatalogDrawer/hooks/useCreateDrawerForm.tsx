5c307e35de37b8a132635abf84cb055a9386fd20
Aviv Turgeman <aturgema@redhat.com>
2025-09-16 10:42:46 +0300

    Update src/views/catalog/templatescatalog/components/TemplatesCatalogDrawer/hooks/useCreateDrawerForm.tsx
    Co-authored-by: Ugo Palatucci <upalatuc@redhat.com>

--- src/views/catalog/templatescatalog/components/TemplatesCatalogDrawer/hooks/useCreateDrawerForm.tsx
+++ src/views/catalog/templatescatalog/components/TemplatesCatalogDrawer/hooks/useCreateDrawerForm.tsx
@@ -127,7 +127,7 @@ const useCreateDrawerForm = (
 
   const [hyperConverge] = useHyperConvergeConfiguration();
   const enableMultiArchBootImageImport =
-    hyperConverge.spec.featureGates?.enableMultiArchBootImageImport;
+    hyperConverge?.spec?.featureGates?.enableMultiArchBootImageImport;
 
   const storageClassRequiredMissing = storageClassRequired && isEmpty(storageClassName);
 
