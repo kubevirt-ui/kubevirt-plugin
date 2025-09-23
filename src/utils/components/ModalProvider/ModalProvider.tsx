c855c03cfd592919a4400faca9853fc4838a1c97
Gal Kremer <gkremer@redhat.com>
2025-09-23 18:28:29 -0400

    CNV-69719: Enhance ModalProvider to include router context parameters - Refactored ModalProvider to support namespace and cluster parameters from router context. - Introduced useModalWithRouterContext hook for improved modal management. - Updated DiskRowActions to utilize the new hook. - Optimized VirtualMachineNavigator with useCallback for filter change handling.

--- src/utils/components/ModalProvider/ModalProvider.tsx
+++ src/utils/components/ModalProvider/ModalProvider.tsx
@@ -1,13 +1,29 @@
-import * as React from 'react';
+import React, {
+  ComponentType,
+  createContext,
+  createElement,
+  FC,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from 'react';
 
+import useNamespaceParam from '@kubevirt-utils/hooks/useNamespaceParam';
+import useClusterParam from '@multicluster/hooks/useClusterParam';
+
+/* eslint-disable react/no-unused-prop-types */
 export type ModalComponentProps = {
   appendTo: () => HTMLElement;
   isOpen: boolean;
   onClose: () => void;
 };
-export type ModalComponent = React.ComponentType<ModalComponentProps>;
+
+export type ModalComponent = ComponentType<ModalComponentProps>;
 
 export type ModalContextType = {
+  /** cluster parameter from router context */
+  cluster?: string;
   /** receives a modal component as an argument and injects it to the dom, the component callback will receive the following parameters,
    * isOpen: open state of the modal
    * onClose: callback to close the modal
@@ -25,25 +41,75 @@ export type ModalContextType = {
   isOpen?: boolean;
   /** the modal component to render */
   modal?: ModalComponent;
+  /** namespace parameter from router context */
+  namespace?: string;
   /** callback to close the modal */
   onClose?: () => void;
 };
 
-export const ModalContext = React.createContext<ModalContextType>({});
+export const ModalContext = createContext<ModalContextType>({});
 /**
  * A hook that returns a global modal context. This context is used to inject a modal component to the dom.
+ * Automatically captures router context when available and injects it into modals.
  * @example
- * const { createModal } = useModal();
+ * const { createModal, namespace, cluster } = useModal();
  *
  * createModal(({ isOpen, onClose, appendTo }) => (
  *  <ExampleModal isOpen={isOpen} onClose={onClose} appendTo={appendTo} />
  * ))
  */
-export const useModal = () => React.useContext(ModalContext);
+
+export const useModalWithRouterContext = () => {
+  const context = useContext(ModalContext);
+  const namespace = useNamespaceParam();
+  const cluster = useClusterParam();
+
+  const createModal = useCallback(
+    (newModal: ModalComponent) => {
+      const ModalWithContext = (props: ModalComponentProps) => {
+        const contextValue = useMemo(
+          () => ({ ...context, cluster, namespace }),
+          [context, namespace, cluster],
+        );
+        return (
+          <ModalContext.Provider value={contextValue}>
+            {createElement(newModal, props)}
+          </ModalContext.Provider>
+        );
+      };
+
+      context.createModal?.(ModalWithContext);
+    },
+    [context, namespace, cluster],
+  );
+
+  return {
+    ...context,
+    cluster,
+    createModal,
+    namespace,
+  };
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+
+  const createModal = useCallback(
+    (newModal: ModalComponent) => {
+      context.createModal?.(newModal);
+    },
+    [context],
+  );
+
+  return {
+    ...context,
+    createModal,
+  };
+};
 
 export const useModalValue = (): ModalContextType => {
-  const [modal, setModal] = React.useState<ModalComponent>();
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [modal, setModal] = useState<ModalComponent>();
+  const [isOpen, setIsOpen] = useState(false);
 
   const createModal = (newModal: ModalComponent) => {
     setIsOpen(true);
@@ -58,7 +124,7 @@ export const useModalValue = (): ModalContextType => {
   return { createModal, isOpen, modal, onClose };
 };
 
-export const ModalProvider: React.FC<{ value: ModalContextType }> = ({ children, value = {} }) => {
+export const ModalProvider: FC<{ value: ModalContextType }> = ({ children, value = {} }) => {
   const { isOpen, modal: Modal, onClose } = value;
 
   return (
--- src/views/virtualmachines/details/tabs/configuration/storage/components/tables/disk/DiskRowActions.tsx
+++ src/views/virtualmachines/details/tabs/configuration/storage/components/tables/disk/DiskRowActions.tsx
@@ -8,7 +8,7 @@ import {
 } from '@kubevirt-ui/kubevirt-api/kubevirt';
 import DiskModal from '@kubevirt-utils/components/DiskModal/DiskModal';
 import { produceVMDisks } from '@kubevirt-utils/components/DiskModal/utils/helpers';
-import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
+import { useModalWithRouterContext } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
 import KebabToggle from '@kubevirt-utils/components/toggles/KebabToggle';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { getName } from '@kubevirt-utils/resources/shared';
@@ -51,7 +51,7 @@ const DiskRowActions: FC<DiskRowActionsProps> = ({
   vmi,
 }) => {
   const { t } = useKubevirtTranslation();
-  const { createModal } = useModal();
+  const { createModal } = useModalWithRouterContext();
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
