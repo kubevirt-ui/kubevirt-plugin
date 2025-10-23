57de6de01a32f6ebfe78e87ca4fea79dd6704dc7
aviavissar <aviavissar@gmail.com>
2025-10-24 02:41:43 +0300

    CNV-70777 : Bad formatting on passt binding dropdown
    Signed-off-by: aviavissar <aviavissar@gmail.com>

--- src/utils/components/NetworkInterfaceModal/NetworkInterfaceModal.tsx
+++ src/utils/components/NetworkInterfaceModal/NetworkInterfaceModal.tsx
@@ -25,7 +25,7 @@ import NameFormField from './components/NameFormField';
 import NetworkInterfaceMACAddressInput from './components/NetworkInterfaceMacAddressInput';
 import NetworkInterfaceModelSelect from './components/NetworkInterfaceModelSelect';
 import NetworkInterfaceNetworkSelect from './components/NetworkInterfaceNetworkSelect/NetworkInterfaceNetworkSelect';
-import NetworkInterfacePasst from './components/NetworkInterfacePasst';
+import NetworkInterfacePasst from './components/NetworkInterfacePasstSelect/NetworkInterfacePasst';
 import { interfaceModelType } from './utils/constants';
 import { getNetworkName } from './utils/helpers';
 
---
+++ src/utils/components/NetworkInterfaceModal/components/NetworkInterfacePasstSelect/NetworkInterfacePasst.tsx
@@ -0,0 +1,86 @@
+import React, { FC, useCallback, useState } from 'react';
+
+import NetworkInterfacePasstHelperPopover from '@kubevirt-utils/components/NetworkInterfaceModal/components/NetworkInterfacePasstSelect/NetworkInterfacePasstHelperPopover';
+import SelectToggle from '@kubevirt-utils/components/toggles/SelectToggle';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import useNamespaceUDN from '@kubevirt-utils/resources/udn/hooks/useNamespaceUDN';
+import { interfaceTypesProxy } from '@kubevirt-utils/resources/vm/utils/network/constants';
+import usePasstFeatureFlag from '@overview/SettingsTab/PreviewFeaturesTab/hooks/usePasstFeatureFlag';
+import { FormGroup, Select, SelectOption, Split } from '@patternfly/react-core';
+
+import { getPASSTSelectableOptions } from '../../utils/helpers';
+
+type NetworkInterfacePasstProps = {
+  interfaceType: string;
+  namespace: string;
+  setInterfaceType: (newValue: string) => void;
+};
+
+const NetworkInterfacePasst: FC<NetworkInterfacePasstProps> = ({
+  interfaceType,
+  namespace,
+  setInterfaceType,
+}) => {
+  const { t } = useKubevirtTranslation();
+  const [isOpen, setIsOpen] = useState(false);
+  const options = getPASSTSelectableOptions(t);
+  const passtFeatureFlag = usePasstFeatureFlag();
+  const [isNamespaceManagedByUDN] = useNamespaceUDN(namespace);
+
+  const selectedType = interfaceType || interfaceTypesProxy.l2bridge;
+
+  const selectedOption = options.find((option) => option.id === selectedType);
+
+  const onToggle = useCallback(() => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  }, []);
+
+  const onSelect = useCallback(
+    (_, value: string) => {
+      setInterfaceType(value);
+      onToggle();
+    },
+    [onToggle, setInterfaceType],
+  );
+
+  if (!isNamespaceManagedByUDN) return null;
+
+  return (
+    <Split hasGutter>
+      <FormGroup
+        labelHelp={
+          passtFeatureFlag.featureEnabled && (
+            <NetworkInterfacePasstHelperPopover namespace={namespace} />
+          )
+        }
+        className="form-group-margin"
+        fieldId="passt-checkbox"
+        label={t('Binding')}
+      >
+        <div data-test-id="network-attachment-definition-select">
+          <Select
+            toggle={SelectToggle({
+              'data-test-id': 'source-type-select',
+              isExpanded: isOpen,
+              isFullWidth: true,
+              onClick: onToggle,
+              selected: selectedOption?.title,
+            })}
+            isOpen={isOpen}
+            onOpenChange={setIsOpen}
+            onSelect={onSelect}
+            selected={selectedType}
+          >
+            {options.map((option) => (
+              <SelectOption description={option.description} key={option.id} value={option.id}>
+                {option.title}
+              </SelectOption>
+            ))}
+          </Select>
+        </div>
+      </FormGroup>
+    </Split>
+  );
+};
+
+export default NetworkInterfacePasst;
