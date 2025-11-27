acee71ea9a299691bc71cee986a0409a71c9b194
Ugo Palatucci <upalatuc@redhat.com>
2025-11-27 11:09:41 +0100

    CNV-69204: add template details url extension

--- src/views/templates/details/TemplateNavPage.tsx
+++ src/views/templates/details/TemplateNavPage.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useParams } from 'react-router-dom-v5-compat';
 
 import {
   modelToGroupVersionKind,
@@ -7,26 +8,30 @@ import {
 } from '@kubevirt-ui/kubevirt-api/console';
 import Loading from '@kubevirt-utils/components/Loading/Loading';
 import { SidebarEditorProvider } from '@kubevirt-utils/components/SidebarEditor/SidebarEditorContext';
-import { HorizontalNav, useK8sWatchResource } from '@openshift-console/dynamic-plugin-sdk';
+import useKubevirtWatchResource from '@kubevirt-utils/hooks/useKubevirtWatchResource/useKubevirtWatchResource';
+import useNamespaceParam from '@kubevirt-utils/hooks/useNamespaceParam';
+import useClusterParam from '@multicluster/hooks/useClusterParam';
+import { HorizontalNav } from '@openshift-console/dynamic-plugin-sdk';
 import { Bullseye } from '@patternfly/react-core';
 
 import useEditTemplateAccessReview from './hooks/useIsTemplateEditable';
 import { useVirtualMachineTabs } from './hooks/useTemplateTabs';
 import TemplatePageTitle from './TemplatePageTitle';
 
-type TemplateNavPageProps = {
-  name: string;
-  namespace: string;
-};
+const TemplateNavPage: FC = () => {
+  const { name } = useParams<{ name: string }>();
+  const cluster = useClusterParam();
+  const namespace = useNamespaceParam();
 
-const TemplateNavPage: FC<TemplateNavPageProps> = ({ name, namespace }) => {
-  const [template, loaded] = useK8sWatchResource<V1Template>({
+  const [template, loaded] = useKubevirtWatchResource<V1Template>({
+    cluster,
     groupVersionKind: modelToGroupVersionKind(TemplateModel),
     isList: false,
     name,
     namespace,
     namespaced: true,
   });
+
   const pages = useVirtualMachineTabs();
   const { isTemplateEditable } = useEditTemplateAccessReview(template);
 
