51e8bd3a28272eefee0480cef8c89845cd36f756
Vojtech Szocs <vszocs@redhat.com>
2025-12-04 15:36:16 +0000

    Bump Console plugin SDK deps and fix useGetModifyApplicationAction usage

--- src/views/topology/hooks/useModifyApplicationActionProvider.ts
+++ src/views/topology/hooks/useModifyApplicationActionProvider.ts
@@ -2,25 +2,29 @@ import { useMemo } from 'react';
 
 import VirtualMachineModel from '@kubevirt-ui/kubevirt-api/console/models/VirtualMachineModel';
 import { Action, K8sResourceCommon } from '@openshift-console/dynamic-plugin-sdk';
-import { getModifyApplicationAction } from '@openshift-console/dynamic-plugin-sdk-internal';
+import { useGetModifyApplicationAction } from '@openshift-console/dynamic-plugin-sdk-internal';
 import { GraphElement } from '@patternfly/react-topology';
 import { isVMType } from '@topology/utils/utils';
 
 type UseModifyApplicationActionProvider = (element: GraphElement) => [Action[], boolean, undefined];
 
 const useModifyApplicationActionProvider: UseModifyApplicationActionProvider = (element) => {
-  const actions = useMemo(() => {
-    if (isVMType(element.getType())) return undefined;
+  const isVMElement = useMemo(() => isVMType(element.getType()), [element]);
 
-    const resource = element?.getData()?.resources?.obj as K8sResourceCommon;
-    return [getModifyApplicationAction(VirtualMachineModel, resource, 'vm-action-start')];
-  }, [element]);
+  const resource = useMemo<K8sResourceCommon>(
+    () => (isVMElement ? undefined : element.getData()?.resources?.obj),
+    [element, isVMElement],
+  );
 
-  return useMemo(() => {
-    if (!actions) return [[], true, undefined];
+  const startAction = useGetModifyApplicationAction(
+    VirtualMachineModel,
+    resource,
+    'vm-action-start',
+  );
 
-    return [actions, true, undefined];
-  }, [actions]);
+  const actions = useMemo(() => (isVMElement ? [] : [startAction]), [isVMElement, startAction]);
+
+  return useMemo(() => [actions, true, undefined], [actions]);
 };
 
 export default useModifyApplicationActionProvider;
