53b8a7f0267b5b9d18bcde1ae2b988de2361968c
adamviktora <viktora.adam@gmail.com>
2025-12-04 13:09:44 +0100

    CNV-71586: advanced search IP address field validation

--- src/views/search/components/AdvancedSearchModal/formFields/IPField.tsx
+++ src/views/search/components/AdvancedSearchModal/formFields/IPField.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
 
+import FormGroupHelperText from '@kubevirt-utils/components/FormGroupHelperText/FormGroupHelperText';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { FormGroup, TextInput } from '@patternfly/react-core';
+import { FormGroup, TextInput, ValidatedOptions } from '@patternfly/react-core';
+import { isValidIPSubstring } from '@search/utils/validation';
 import { VirtualMachineRowFilterType } from '@virtualmachines/utils';
 
 import { useAdvancedSearchField } from '../store/useAdvancedSearchStore';
@@ -10,14 +12,23 @@ const IPField: FC = () => {
   const { t } = useKubevirtTranslation();
   const { setValue, value } = useAdvancedSearchField(VirtualMachineRowFilterType.IP);
 
+  const isValid = isValidIPSubstring(value);
+  const validated = isValid ? ValidatedOptions.default : ValidatedOptions.warning;
+
   return (
     <FormGroup label={t('IP address')}>
       <TextInput
         data-test="adv-search-vm-ip"
         onChange={(_, newValue) => setValue(newValue)}
         type="text"
+        validated={validated}
         value={value}
       />
+      {!isValid && (
+        <FormGroupHelperText validated={validated}>
+          {t('Invalid IP address. No VirtualMachines will be found.')}
+        </FormGroupHelperText>
+      )}
     </FormGroup>
   );
 };
---
+++ src/views/search/utils/validation.ts
@@ -0,0 +1,35 @@
+const isValidIPv4Substring = (ip: string) => {
+  // Invalid characters || consecutive dots
+  if (!/^[0-9.]*$/.test(ip) || /\.\./.test(ip)) {
+    return false;
+  }
+
+  const octets = ip.split('.');
+  if (octets.length > 4) {
+    return false;
+  }
+
+  return octets
+    .filter((o) => o !== '')
+    .every((o) => {
+      const num = parseInt(o, 10);
+      return !isNaN(num) && num >= 0 && num <= 255;
+    });
+};
+
+const isValidIPv6Substring = (ip: string) => {
+  // Invalid characters || more than 2 consecutive colons || more than one ::
+  if (!/^[0-9a-fA-F:]*$/.test(ip) || /:::/.test(ip) || (ip.match(/::/g) ?? []).length > 1) {
+    return false;
+  }
+
+  const groups = ip.split(':');
+  if (groups.length > 8) {
+    return false;
+  }
+
+  return groups.every((group) => group.length <= 4);
+};
+
+export const isValidIPSubstring = (ip: string) =>
+  !ip || isValidIPv4Substring(ip) || isValidIPv6Substring(ip);
