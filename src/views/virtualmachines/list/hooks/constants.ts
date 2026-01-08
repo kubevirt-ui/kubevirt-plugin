7671e6285f18b893a225c79920060feb49e40755
Ugo Palatucci <upalatuc@redhat.com>
2026-01-08 11:01:58 +0100

    backport cpu and network metrics fix

--- src/views/virtualmachines/list/hooks/constants.ts
+++ src/views/virtualmachines/list/hooks/constants.ts
@@ -3,25 +3,24 @@ import { isEmpty } from '@kubevirt-utils/utils/utils';
 export const TEXT_FILTER_NAME_ID = 'name';
 export const TEXT_FILTER_LABELS_ID = 'labels';
 
+export const DEFAULT_DURATION = '15m';
+
 export const VMListQueries = {
-  CPU_REQUESTED: 'CPU_REQUESTED',
   CPU_USAGE: 'CPU_USAGE',
   MEMORY_USAGE: 'MEMORY_USAGE',
   NETWORK_TOTAL_USAGE: 'NETWORK_TOTAL_USAGE',
 };
 
-export const getVMListQueries = (namespace: string, cluster?: string, allClusters = false) => {
+export const getVMListQueries = (namespace: string, cluster?: string) => {
   const namespaceFilter = isEmpty(namespace) ? '' : `namespace='${namespace}'`;
 
   const clusterFilter = cluster ? `cluster='${cluster}'` : '';
 
   const filters = [namespaceFilter, clusterFilter].filter((filter) => filter.length > 0).join(',');
 
-  const duration = cluster || allClusters ? '15m' : '30s';
   return {
-    [VMListQueries.CPU_REQUESTED]: `kube_pod_resource_request{resource='cpu',${filters}}`,
-    [VMListQueries.CPU_USAGE]: `rate(kubevirt_vmi_cpu_usage_seconds_total{${filters}}[${duration}])`,
-    [VMListQueries.MEMORY_USAGE]: `kubevirt_vmi_memory_used_bytes{${filters}}`,
-    [VMListQueries.NETWORK_TOTAL_USAGE]: `rate(kubevirt_vmi_network_transmit_bytes_total{${filters}}[${duration}]) + rate(kubevirt_vmi_network_receive_bytes_total{${filters}}[${duration}])`,
+    [VMListQueries.CPU_USAGE]: `sum(rate(kubevirt_vmi_cpu_usage_seconds_total{${filters}}[${DEFAULT_DURATION}])) BY (name, namespace, cluster)`,
+    [VMListQueries.MEMORY_USAGE]: `sum(kubevirt_vmi_memory_used_bytes{${filters}}) BY (name, namespace, cluster)`,
+    [VMListQueries.NETWORK_TOTAL_USAGE]: `sum(rate(kubevirt_vmi_network_transmit_bytes_total{${filters}}[${DEFAULT_DURATION}]) + rate(kubevirt_vmi_network_receive_bytes_total{${filters}}[${DEFAULT_DURATION}])) BY (name, namespace, cluster)`,
   };
 };
--- src/views/virtualmachines/list/utils/totalsQueries.ts
+++ src/views/virtualmachines/list/utils/totalsQueries.ts
@@ -8,6 +8,8 @@ export const VMTotalsQueries = {
   TOTAL_STORAGE_USAGE: 'TOTAL_STORAGE_USAGE',
 };
 
+const DEFAULT_DURATION = '15m';
+
 const getSumBy = (isAllNamespaces: boolean, cluster?: string, allClusters = false) => {
   if (allClusters || isAllNamespaces) return 'sum';
 
@@ -25,11 +27,10 @@ export const getVMTotalsQueries = (namespace: string, cluster?: string, allClust
     ? `{${clusterFilter}}`
     : `{namespace='${namespace}',${clusterFilter}}`;
 
-  const duration = cluster || allClusters ? '15m' : '30s';
   const sumBy = getSumBy(isAllNamespaces, cluster, allClusters);
 
   return {
-    [VMTotalsQueries.TOTAL_CPU_USAGE]: `${sumBy}(rate(kubevirt_vmi_cpu_usage_seconds_total${filterByNamespace}[${duration}]))`,
+    [VMTotalsQueries.TOTAL_CPU_USAGE]: `${sumBy}(rate(kubevirt_vmi_cpu_usage_seconds_total${filterByNamespace}[${DEFAULT_DURATION}]))`,
     [VMTotalsQueries.TOTAL_MEMORY_USAGE]: `${sumBy}(kubevirt_vmi_memory_used_bytes${filterByNamespace})`,
     [VMTotalsQueries.TOTAL_STORAGE_CAPACITY]: `${sumBy}(max(kubevirt_vmi_filesystem_capacity_bytes${filterByNamespace}) by (namespace, name, disk_name, cluster))`,
     [VMTotalsQueries.TOTAL_STORAGE_USAGE]: `${sumBy}(max(kubevirt_vmi_filesystem_used_bytes${filterByNamespace}) by (namespace, name, disk_name, cluster))`,
