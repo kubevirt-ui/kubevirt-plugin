18540b16b2b6d6141335d43165ed906ad358ef29
Ugo Palatucci <upalatuc@redhat.com>
2026-01-19 15:42:11 +0100

    CNV-72165: fix cluster-wide fetch in acm with RBAC

--- src/views/virtualmachines/search/hooks/useAccessibleResources.ts
+++ src/views/virtualmachines/search/hooks/useAccessibleResources.ts
@@ -1,11 +1,14 @@
 import { useMemo } from 'react';
 
 import { useIsAdmin } from '@kubevirt-utils/hooks/useIsAdmin';
+import { KubevirtDataPodFilters } from '@kubevirt-utils/hooks/useKubevirtDataPod/useKubevirtDataPodFilters';
 import useKubevirtWatchResource from '@kubevirt-utils/hooks/useKubevirtWatchResource/useKubevirtWatchResource';
 import useProjects from '@kubevirt-utils/hooks/useProjects';
 import { isEmpty } from '@kubevirt-utils/utils/utils';
+import useClusterParam from '@multicluster/hooks/useClusterParam';
 import useIsACMPage from '@multicluster/useIsACMPage';
 import { K8sGroupVersionKind, useK8sWatchResources } from '@openshift-console/dynamic-plugin-sdk';
+import { useHubClusterName } from '@stolostron/multicluster-sdk';
 import { OBJECTS_FETCHING_LIMIT } from '@virtualmachines/utils/constants';
 
 type UseAccessibleResources<T> = {
@@ -16,10 +19,13 @@ type UseAccessibleResources<T> = {
 
 export const useAccessibleResources = <T>(
   groupVersionKind: K8sGroupVersionKind,
-  cluster?: string,
+  filterOptions?: KubevirtDataPodFilters,
 ): UseAccessibleResources<T> => {
   const isAdmin = useIsAdmin();
   const isACMPage = useIsACMPage();
+  const cluster = useClusterParam();
+  const [hubClusterName] = useHubClusterName();
+  const isManagedClusterFetch = isACMPage && cluster && hubClusterName !== cluster;
   const [projectNames, projectNamesLoaded, projectNamesError] = useProjects();
 
   const loadPerNamespace = !isACMPage && projectNamesLoaded && !isAdmin;
@@ -28,12 +34,14 @@ export const useAccessibleResources = <T>(
   const [allResources, allResourcesLoaded] = useKubevirtWatchResource<T[]>(
     shouldFetchClusterWide
       ? {
-          cluster,
+          cluster: !isManagedClusterFetch ? cluster : undefined,
           groupVersionKind,
           isList: true,
           limit: OBJECTS_FETCHING_LIMIT,
         }
       : null,
+    filterOptions,
+    isManagedClusterFetch ? [{ property: 'cluster', values: [cluster] }] : null,
   );
 
   const allowedResources = useK8sWatchResources<{ [key: string]: T[] }>(
