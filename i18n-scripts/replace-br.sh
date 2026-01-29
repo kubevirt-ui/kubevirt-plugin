e7ee5bd9db274c126e003bde19dd26125ec19ead
Ugo Palatucci <upalatuc@redhat.com>
2026-01-29 14:55:53 +0100

    chore(i18n): update translations
    Adding latest translations from Memsource project https://cloud.memsource.com/web/project2/show/Tez16V00nteF98APRSLmbc

--- i18n-scripts/replace-br.sh
+++ i18n-scripts/replace-br.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i -r "s|<br />|<br/>|g" ./locales/en/plugin__kubevirt-plugin.json \
+sed -i -r "s|<br/>|<br />|g" ./locales/en/plugin__kubevirt-plugin.json \
 ./locales/es/plugin__kubevirt-plugin.json \
 ./locales/fr/plugin__kubevirt-plugin.json \
 ./locales/ja/plugin__kubevirt-plugin.json \
