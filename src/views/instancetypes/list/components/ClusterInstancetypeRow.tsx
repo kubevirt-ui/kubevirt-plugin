69980dfcc67ad6c9388b309276af91c517407e83
Ugo Palatucci <upalatuc@redhat.com>
2026-02-10 15:56:50 +0100

    CNV-69204: use new sdk version that has the new search api parameters

--- src/views/instancetypes/list/components/ClusterInstancetypeRow.tsx
+++ src/views/instancetypes/list/components/ClusterInstancetypeRow.tsx
@@ -7,7 +7,7 @@ import { VENDOR_LABEL } from '@kubevirt-utils/constants/constants';
 import { getLabel } from '@kubevirt-utils/resources/shared';
 import { getName } from '@kubevirt-utils/resources/shared';
 import { NO_DATA_DASH } from '@kubevirt-utils/resources/vm/utils/constants';
-import { readableSizeUnit } from '@kubevirt-utils/utils/units';
+import { getHumanizedSize } from '@kubevirt-utils/utils/units';
 import { getCluster } from '@multicluster/helpers/selectors';
 import { RowProps, TableData } from '@openshift-console/dynamic-plugin-sdk';
 import { FleetResourceLink } from '@stolostron/multicluster-sdk';
@@ -35,7 +35,8 @@ const ClusterInstancetypeRow: FC<RowProps<V1beta1VirtualMachineClusterInstancety
       {it?.spec?.cpu?.guest}
     </TableData>
     <TableData activeColumnIDs={activeColumnIDs} id="memory">
-      {readableSizeUnit(it?.spec?.memory?.guest)}
+      {/* issues with  multicluster sdk where the guest is a number, not a string */}
+      {getHumanizedSize(it?.spec?.memory?.guest?.toString())?.string}
     </TableData>
     <TableData activeColumnIDs={activeColumnIDs} id="vendor">
       {getLabel(it, VENDOR_LABEL, NO_DATA_DASH)}
--- src/views/templates/list/hooks/useVirtualMachineTemplatesCPUMemory.tsx
+++ src/views/templates/list/hooks/useVirtualMachineTemplatesCPUMemory.tsx
@@ -1,12 +1,12 @@
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { V1Template } from '@kubevirt-utils/models';
 import { getTemplateVirtualMachineObject, vCPUCount } from '@kubevirt-utils/resources/template';
-import { getMemoryCPU } from '@kubevirt-utils/resources/vm';
+import { getMemoryCPU, NO_DATA_DASH } from '@kubevirt-utils/resources/vm';
 import { readableSizeUnit } from '@kubevirt-utils/utils/units';
 
 export const useVirtualMachineTemplatesCPUMemory = (template: V1Template): string => {
   const { t } = useKubevirtTranslation();
   const { cpu, memory } = getMemoryCPU(getTemplateVirtualMachineObject(template));
 
-  return `${vCPUCount(cpu)} CPU | ${readableSizeUnit(memory)} ${t('Memory')}`;
+  return `${vCPUCount(cpu)} CPU | ${readableSizeUnit(memory) || NO_DATA_DASH} ${t('Memory')}`;
 };
