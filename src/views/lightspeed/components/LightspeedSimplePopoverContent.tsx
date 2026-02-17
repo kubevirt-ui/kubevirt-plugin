91b4f4a1231f416006d8e05fbaabb469563a4b3e
Phillip Bailey <phbailey@redhat.com>
2026-02-17 07:51:42 -0500

    Clean up lightspeed utilities

--- src/views/lightspeed/components/LightspeedSimplePopoverContent.tsx
+++ src/views/lightspeed/components/LightspeedSimplePopoverContent.tsx
@@ -1,7 +1,12 @@
 import React, { FC, ReactNode } from 'react';
 
+import { FLAG_LIGHTSPEED_PLUGIN } from '@kubevirt-utils/flags/consts';
 import LightspeedHelpButton from '@lightspeed/components/LightspeedHelpButton/LightspeedHelpButton';
 import { OLSPromptType } from '@lightspeed/utils/prompts';
+import { useFlag } from '@openshift-console/dynamic-plugin-sdk';
+import { Stack, StackItem } from '@patternfly/react-core';
+
+import './LightspeedSimplePopoverContent.scss';
 
 type LightspeedSimplePopoverContentProps = {
   content: ReactNode;
@@ -16,12 +21,22 @@ const LightspeedSimplePopoverContent: FC<LightspeedSimplePopoverContentProps> = 
   obj,
   promptType,
 }) => {
+  const hasOLSConsole = useFlag(FLAG_LIGHTSPEED_PLUGIN);
+
+  if (!hasOLSConsole) return null;
+
   return (
-    <>
-      {content}
-      <br />
-      <LightspeedHelpButton obj={obj} onClick={hide} promptType={promptType} />
-    </>
+    <Stack hasGutter>
+      <StackItem>{content}</StackItem>
+      <StackItem>
+        <LightspeedHelpButton
+          className="lightspeed-popover-content__help-button"
+          obj={obj}
+          onClick={hide}
+          promptType={promptType}
+        />
+      </StackItem>
+    </Stack>
   );
 };
 
