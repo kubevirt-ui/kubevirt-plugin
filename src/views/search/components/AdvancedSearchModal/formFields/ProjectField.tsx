13d84bbfb62c7550ce21483d0760b05492335b10
adamviktora <viktora.adam@gmail.com>
2026-03-27 16:31:00 +0100

    CNV-82384: load Advanced search modal values based on selected clusters

--- src/views/search/components/AdvancedSearchModal/formFields/ProjectField.tsx
+++ src/views/search/components/AdvancedSearchModal/formFields/ProjectField.tsx
@@ -1,18 +1,23 @@
 import React, { FC } from 'react';
 
+import { V1VirtualMachine } from '@kubevirt-ui-ext/kubevirt-api/kubevirt';
 import MultiSelectTypeahead from '@kubevirt-utils/components/MultiSelectTypeahead/MultiSelectTypeahead';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import useProjects from '@kubevirt-utils/hooks/useProjects';
 import { FormGroup } from '@patternfly/react-core';
 import { VirtualMachineRowFilterType } from '@virtualmachines/utils';
 
+import useProjectsWithVMs from '../hooks/useProjectsWithVMs';
 import { useAdvancedSearchField } from '../store/useAdvancedSearchStore';
 
-const ProjectField: FC = () => {
+type ProjectFieldProps = {
+  vms: V1VirtualMachine[];
+};
+
+const ProjectField: FC<ProjectFieldProps> = ({ vms }) => {
   const { t } = useKubevirtTranslation();
   const { setValue, value } = useAdvancedSearchField(VirtualMachineRowFilterType.Project);
 
-  const [allProjectNames] = useProjects();
+  const allProjectNames = useProjectsWithVMs(vms);
 
   return (
     <FormGroup label={t('Project')}>
