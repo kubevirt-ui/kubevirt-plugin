bc4d2da693dc6ef81b4a8aee0990217a828801cc
Gal Kremer <gkremer@redhat.com>
2026-04-30 12:09:08 -0400

    CNV-69868: Update logic inside addCDROMModal and MountCDROMModal

--- src/utils/components/DiskModal/AddCDROMModal.tsx
+++ src/utils/components/DiskModal/AddCDROMModal.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC, useEffect, useMemo } from 'react';
 import { FormProvider, useForm, useWatch } from 'react-hook-form';
 
 import { UPLOAD_FILENAME_FIELD } from '@kubevirt-utils/components/DiskModal/components/utils/constants';
@@ -8,16 +8,14 @@ import {
   UPLOAD_MODE_SELECT,
   UPLOAD_MODE_UPLOAD,
 } from '@kubevirt-utils/components/DiskModal/utils/constants';
-import InlineFilterSelect from '@kubevirt-utils/components/FilterSelect/InlineFilterSelect';
 import { PendingChangesAlert } from '@kubevirt-utils/components/PendingChanges/PendingChangesAlert/PendingChangesAlert';
 import { useCDIUpload } from '@kubevirt-utils/hooks/useCDIUpload/useCDIUpload';
-import { isUploadingDisk } from '@kubevirt-utils/hooks/useCDIUpload/utils';
 import useHyperConvergeConfiguration from '@kubevirt-utils/hooks/useHyperConvergeConfiguration';
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { getNamespace } from '@kubevirt-utils/resources/shared';
-import { isEmpty, kubevirtConsole } from '@kubevirt-utils/utils/utils';
+import { isEmpty } from '@kubevirt-utils/utils/utils';
 import { getCluster } from '@multicluster/helpers/selectors';
-import { Content, ContentVariants, Radio, Stack, StackItem } from '@patternfly/react-core';
+import { Stack, StackItem } from '@patternfly/react-core';
 import { DECLARATIVE_HOTPLUG_VOLUMES_FEATURE_GATE } from '@settings/tabs/ClusterTab/components/GeneralSettings/AdvancedCDROMFeatures/hooks/constants';
 import { useISOOptions } from '@virtualmachines/details/tabs/configuration/storage/components/modal/hooks/useISOOptions';
 import { useMountCDROMForm } from '@virtualmachines/details/tabs/configuration/storage/components/modal/hooks/useMountCDROMForm';
@@ -25,8 +23,9 @@ import { isRunning } from '@virtualmachines/utils';
 
 import TabModal from '../TabModal/TabModal';
 
+import CDROMSourceOptions from './components/CDROMSourceOptions/CDROMSourceOptions';
 import DiskNameInput from './components/DiskNameInput/DiskNameInput';
-import DiskSourceUploadPVC from './components/DiskSourceSelect/components/DiskSourceUploadPVC/DiskSourceUploadPVC';
+import { useCDROMUploadClose } from './hooks/useCDROMUploadClose';
 import { getDefaultCreateValues } from './utils/form';
 import { submitCDROM } from './utils/submit';
 import { SourceTypes, V1DiskFormState, V1SubDiskModalProps } from './utils/types';
@@ -45,7 +44,6 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
   const [hyperConvergeConfig] = useHyperConvergeConfiguration();
   const vmNamespace = getNamespace(vm);
 
-  // Check feature gate from HyperConverged CR spec.featureGates (object with boolean values)
   const isDeclarativeHotplugEnabled = Boolean(
     hyperConvergeConfig?.spec?.featureGates?.[DECLARATIVE_HOTPLUG_VOLUMES_FEATURE_GATE],
   );
@@ -54,6 +52,7 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
   const { isoOptions } = useISOOptions(vmNamespace);
 
   const {
+    handleClearUpload,
     handleEmptyDriveSelection,
     handleFileUpload,
     handleISOSelection,
@@ -79,10 +78,13 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
   } = methods;
 
   const uploadFile = useWatch({ control, name: FORM_FIELD_UPLOAD_FILE });
-
   const hasUploadFile = !isEmpty(uploadFile?.file);
   const hasFormErrors = !isEmpty(errors);
-  const isUploading = isUploadingDisk(upload?.uploadStatus);
+
+  const { handleModalClose, isUploading, markBackgroundUploadStarted } = useCDROMUploadClose(
+    upload,
+    onClose,
+  );
 
   useEffect(() => {
     if (!uploadEnabled) {
@@ -93,29 +95,10 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
   const hasValidSelection = selectedISO || (uploadEnabled && hasUploadFile) || emptyDriveSelected;
   const isFormValid = Boolean(!hasFormErrors && hasValidSelection);
 
-  const isBackgroundUploadInProgress = useRef(false);
-
-  const handleModalClose = async () => {
-    const shouldCancelUpload =
-      isUploading && !isBackgroundUploadInProgress.current && upload?.cancelUpload;
-
-    if (shouldCancelUpload) {
-      try {
-        await upload.cancelUpload();
-      } catch (error) {
-        kubevirtConsole.error(error);
-      }
-    }
-    isBackgroundUploadInProgress.current = false;
-    onClose();
-  };
-
   const handleModalSubmit = async () => {
-    // For uploads, check certificate/config before proceeding
-    // This ensures the error is shown in the modal before it closes
     if (uploadEnabled && hasUploadFile) {
       await checkUploadReady();
-      isBackgroundUploadInProgress.current = true;
+      markBackgroundUploadStarted();
     }
     return submitCDROM(getValues(), {
       isHotPluggable,
@@ -128,6 +111,28 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
       vm,
     });
   };
+
+  const handleISOSelect = (value: string): void => {
+    handleISOSelection(value);
+    setValue(UPLOAD_FILENAME_FIELD, '');
+  };
+
+  const handleClearUploadAndFilename = (): void => {
+    handleClearUpload();
+    setValue(UPLOAD_FILENAME_FIELD, '');
+  };
+
+  const emptyDriveOption = useMemo(
+    () => ({
+      description: isEmptyDriveAllowed
+        ? t('The drive will be attached without media. You can mount an ISO later.')
+        : t('Requires enabling advanced CD-ROM features.'),
+      isAllowed: isEmptyDriveAllowed,
+      isSelected: emptyDriveSelected,
+      onSelect: handleEmptyDriveSelection,
+    }),
+    [isEmptyDriveAllowed, emptyDriveSelected, handleEmptyDriveSelection, t],
+  );
 
   return (
     <FormProvider {...methods}>
@@ -153,76 +158,19 @@ const AddCDROMModal: FC<V1SubDiskModalProps> = ({
           <StackItem>
             <DiskNameInput isDisabled={isUploading} />
           </StackItem>
-          <StackItem className="pf-v6-u-mt-md">
-            <Content component={ContentVariants.p}>{t('Select mount option')}</Content>
-          </StackItem>
-          <StackItem>
-            <Stack hasGutter>
-              <StackItem>
-                <Radio
-                  id="cdrom-source-existing"
-                  isChecked={existingISOSelected}
-                  isDisabled={isUploading}
-                  label={t('Use existing ISO')}
-                  name="cdrom-source"
-                  onChange={() => handleISOSelection('')}
-                />
-                {existingISOSelected && (
-                  <div className="pf-v6-u-ml-lg pf-v6-u-mt-sm">
-                    <InlineFilterSelect
-                      setSelected={(e) => {
-                        handleISOSelection(e);
-                        setValue(UPLOAD_FILENAME_FIELD, '');
-                      }}
-                      toggleProps={{
-                        isDisabled: isUploading,
-                        isFullWidth: true,
-                      }}
-                      options={isoOptions}
-                      placeholder={t('Select ISO file')}
-                      selected={selectedISO}
-                    />
-                  </div>
-                )}
-              </StackItem>
-              <StackItem>
-                <Radio
-                  id="cdrom-source-upload"
-                  isChecked={uploadEnabled}
-                  isDisabled={isUploading}
-                  label={t('Upload new ISO')}
-                  name="cdrom-source"
-                  onChange={handleFileUpload}
-                />
-                {uploadEnabled && (
-                  <div className="pf-v6-u-ml-lg pf-v6-u-mt-sm">
-                    <DiskSourceUploadPVC
-                      acceptedFileTypes={{
-                        'application/*': ['.iso', '.img', '.qcow2', '.gz', '.xz'],
-                      }}
-                      label=""
-                      relevantUpload={isSubmitting ? undefined : upload}
-                    />
-                  </div>
-                )}
-              </StackItem>
-              <StackItem>
-                <Radio
-                  description={
-                    isEmptyDriveAllowed
-                      ? t('The drive will be attached without media. You can mount an ISO later.')
-                      : t('Requires enabling advanced CD-ROM features.')
-                  }
-                  id="cdrom-source-empty"
-                  isChecked={emptyDriveSelected}
-                  isDisabled={isUploading || !isEmptyDriveAllowed}
-                  label={t('Leave empty drive')}
-                  name="cdrom-source"
-                  onChange={handleEmptyDriveSelection}
-                />
-              </StackItem>
-            </Stack>
-          </StackItem>
+          <CDROMSourceOptions
+            emptyDriveOption={emptyDriveOption}
+            existingISOSelected={existingISOSelected}
+            isoOptions={isoOptions}
+            isUploading={isUploading}
+            onClearUpload={handleClearUploadAndFilename}
+            onFileUpload={handleFileUpload}
+            onISOSelect={handleISOSelect}
+            radioNamePrefix="cdrom-source"
+            relevantUpload={isSubmitting ? undefined : upload}
+            selectedISO={selectedISO}
+            uploadEnabled={uploadEnabled}
+          />
         </Stack>
       </TabModal>
     </FormProvider>
