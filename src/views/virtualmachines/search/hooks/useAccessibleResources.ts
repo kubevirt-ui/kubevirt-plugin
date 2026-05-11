7b32897bc43916c6a60832e34ebdceb47a65ff4f
adamviktora <viktora.adam@gmail.com>
2026-05-11 09:11:40 +0200

    CNV-86879: show full templates info in ACM VM creation wizard
    - load templates from useFleetK8sWatchResource and not from multi-search useFleetSearchPoll
    - enrich data from useFleetK8sWatchResource with kind and apiVersion

--- src/views/virtualmachines/search/hooks/useAccessibleResources.ts
+++ src/views/virtualmachines/search/hooks/useAccessibleResources.ts
@@ -29,15 +29,13 @@ type UseAccessibleResourcesArgs = {
   watchNamespaces?: string[];
 };
 
-type UseAccessibleResources = <T extends K8sResourceCommon>(
-  args: UseAccessibleResourcesArgs,
-) => {
+type UseAccessibleResourcesReturn<T> = {
   loaded: boolean;
   loadError?: any;
   resources: T[];
 };
 
-export const useAccessibleResources: UseAccessibleResources = <T>({
+export const useAccessibleResources = <T>({
   clusters,
   fieldSelector,
   filterOptions,
@@ -47,11 +45,16 @@ export const useAccessibleResources: UseAccessibleResources = <T>({
   searchQueries,
   selector,
   watchNamespaces,
-}) => {
+}: UseAccessibleResourcesArgs): UseAccessibleResourcesReturn<T> => {
   const isAdmin = useIsAdmin();
   const isACMPage = useIsACMPage();
   const cluster = useClusterParam();
   const [projectNames, projectNamesLoaded, projectNamesError] = useProjects();
+
+  const clusterToWatch = useMemo(() => {
+    if (clusters?.length === 1) return clusters[0];
+    return clusters ? undefined : cluster;
+  }, [clusters, cluster]);
 
   const namespacesToWatch = useMemo(() => {
     if (watchNamespaces) return watchNamespaces;
@@ -65,7 +68,7 @@ export const useAccessibleResources: UseAccessibleResources = <T>({
   const [allResources, allResourcesLoaded] = useKubevirtWatchResource<T[]>(
     shouldFetchClusterWide
       ? {
-          cluster: clusters ? undefined : cluster,
+          cluster: clusterToWatch,
           fieldSelector,
           groupVersionKind,
           isList: true,
