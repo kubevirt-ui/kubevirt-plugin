fd648fc8261da0b43d17bd03627510f90ce9e020
yiraeChristineKim <yikim@redhat.com>
2026-05-25 11:25:47 -0400

    feat(cclm): add app.kubernetes.io/created-by label to MTV resources
    Add 'app.kubernetes.io/created-by: cclm' label to the metadata of
    Plan, NetworkMap, and StorageMap resources created during cross-cluster
    migration, making them identifiable and filterable by their origin.
    Ref: https://redhat.atlassian.net/browse/ACM-32316
    Signed-off-by: yiraeChristineKim <yikim@redhat.com>

--- src/multicluster/components/CrossClusterMigration/constants.ts
+++ src/multicluster/components/CrossClusterMigration/constants.ts
@@ -1,3 +1,6 @@
 export const MTV_MIGRATION_NAMESPACE = 'mtv-integrations';
 
 export const POD_NETWORK_TYPE = 'pod';
+
+export const CCLM_LABEL_KEY = 'app.kubernetes.io/created-by';
+export const CCLM_LABEL_VALUE = 'cclm';
