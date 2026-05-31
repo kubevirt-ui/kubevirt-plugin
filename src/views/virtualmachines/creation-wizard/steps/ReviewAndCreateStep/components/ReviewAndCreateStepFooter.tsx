8a2c5b6f31ca298a4f16c9ff6f78ec7f6cd9f1ca
benbassatido <ibenbass@redhat.com>
2026-05-31 13:11:20 +0300

    CNV-87879: Add error alert to custom configuration on create VM wizard

--- src/views/virtualmachines/creation-wizard/steps/ReviewAndCreateStep/components/ReviewAndCreateStepFooter.tsx
+++ src/views/virtualmachines/creation-wizard/steps/ReviewAndCreateStep/components/ReviewAndCreateStepFooter.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 
-import { t } from '@kubevirt-utils/hooks/useKubevirtTranslation';
+import ErrorAlert from '@kubevirt-utils/components/ErrorAlert/ErrorAlert';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import useWizardFooterProps from '@kubevirt-utils/hooks/useWizardFooterProps';
 import {
   ActionList,
@@ -10,6 +11,7 @@ import {
   useWizardContext,
   WizardFooterWrapper,
 } from '@patternfly/react-core';
+import { Stack } from '@patternfly/react-core';
 import VMNameConfirmationNextButton from '@virtualmachines/creation-wizard/components/VMNameConfirmationNextButton';
 import useCloseWizard from '@virtualmachines/creation-wizard/hooks/useCloseWizard';
 import useCreateVM from '@virtualmachines/creation-wizard/hooks/useCreateVM';
@@ -17,10 +19,11 @@ import useVMWizardStore from '@virtualmachines/creation-wizard/state/vm-wizard-s
 import { isCloneCreationMethod } from '@virtualmachines/creation-wizard/utils/utils';
 
 const ReviewAndCreateStepFooter: FC = () => {
+  const { t } = useKubevirtTranslation();
   const { goToPrevStep } = useWizardContext();
   const { creationMethod } = useVMWizardStore();
   const isCloneMethod = isCloneCreationMethod(creationMethod);
-  const createVM = useCreateVM();
+  const { createVM, error, isSubmitting } = useCreateVM();
   const closeWizard = useCloseWizard();
   const { backButtonText, cancelButtonText } = useWizardFooterProps();
 
@@ -28,27 +31,30 @@ const ReviewAndCreateStepFooter: FC = () => {
 
   return (
     <WizardFooterWrapper>
-      <ActionList>
-        <ActionListGroup>
-          <ActionListItem>
-            <Button onClick={goToPrevStep} variant="secondary">
-              {backButtonText}
-            </Button>
-          </ActionListItem>
-          <ActionListItem>
-            <VMNameConfirmationNextButton onClick={createVM}>
-              {createButtonText}
-            </VMNameConfirmationNextButton>
-          </ActionListItem>
-        </ActionListGroup>
-        <ActionListGroup>
-          <ActionListItem>
-            <Button onClick={closeWizard} variant="link">
-              {cancelButtonText}
-            </Button>
-          </ActionListItem>
-        </ActionListGroup>
-      </ActionList>
+      <Stack hasGutter>
+        {error && <ErrorAlert error={error} />}
+        <ActionList>
+          <ActionListGroup>
+            <ActionListItem>
+              <Button onClick={goToPrevStep} variant="secondary">
+                {backButtonText}
+              </Button>
+            </ActionListItem>
+            <ActionListItem>
+              <VMNameConfirmationNextButton isSubmitting={isSubmitting} onClick={createVM}>
+                {createButtonText}
+              </VMNameConfirmationNextButton>
+            </ActionListItem>
+          </ActionListGroup>
+          <ActionListGroup>
+            <ActionListItem>
+              <Button onClick={closeWizard} variant="link">
+                {cancelButtonText}
+              </Button>
+            </ActionListItem>
+          </ActionListGroup>
+        </ActionList>
+      </Stack>
     </WizardFooterWrapper>
   );
 };
