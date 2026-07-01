ea2c5890e267059b6dea97a921875fb9a5572969
adamviktora <viktora.adam@gmail.com>
2026-07-01 12:29:28 +0200

    CNV-87886 replace useListPageFilter on Migration policies list page

--- src/views/migrationpolicies/list/hooks/useMigrationPoliciesFilters.ts
+++ src/views/migrationpolicies/list/hooks/useMigrationPoliciesFilters.ts
@@ -1,7 +1,8 @@
-import { useClusterFilter } from '@kubevirt-utils/hooks/useClusterFilter';
+import useClusterFilter from '@kubevirt-utils/hooks/useKubevirtDataViewFilters/filters/useClusterFilter';
+import { type KubevirtFilter } from '@kubevirt-utils/hooks/useKubevirtDataViewFilters/types';
 import useIsACMPage from '@multicluster/useIsACMPage';
 
-const useMigrationPoliciesFilters = () => {
+const useMigrationPoliciesFilters = (): KubevirtFilter[] => {
   const isACMPage = useIsACMPage();
 
   const clusterFilter = useClusterFilter();
