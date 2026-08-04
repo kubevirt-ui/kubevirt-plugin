7960011013a8d501f26b2863934bf60f0b88e5c5
Adam Viktora <84135613+adamviktora@users.noreply.github.com>
2026-08-04 17:07:32 +0200

    CNV-87886: replace useListPageFilter on Storage migration list page (#4518)

--- src/views/storagemigrations/list/StorageMigrationListFilters.ts
+++ src/views/storagemigrations/list/StorageMigrationListFilters.ts
@@ -1,30 +1,26 @@
 import { TFunction } from 'i18next';
 
+import { KubevirtFilter } from '@kubevirt-utils/hooks/useKubevirtDataViewFilters/types';
 import { MultiNamespaceVirtualMachineStorageMigrationPlan } from '@kubevirt-utils/resources/migrations/constants';
 import {
   getStorageMigrationStatus,
   StorageMigrationStatusFilterValue,
 } from '@kubevirt-utils/resources/migrations/storageMigrationLifecycle';
-import { RowFilter } from '@openshift-console/dynamic-plugin-sdk';
 
 export const STORAGE_MIGRATION_STATUS_FILTER_TYPE = 'storage-migration-status';
 
-export { getStorageMigrationStatus, StorageMigrationStatusFilterValue };
-
 export const getStorageMigrationStatusFilters = (
   t: TFunction,
-): RowFilter<MultiNamespaceVirtualMachineStorageMigrationPlan>[] => [
+): KubevirtFilter<MultiNamespaceVirtualMachineStorageMigrationPlan>[] => [
   {
-    filter: ({ selected }, obj) =>
-      selected?.length === 0 || selected.includes(getStorageMigrationStatus(obj)),
-    filterGroupName: t('Status'),
-    items: [
-      { id: StorageMigrationStatusFilterValue.Running, title: t('Running') },
-      { id: StorageMigrationStatusFilterValue.Pending, title: t('Pending') },
-      { id: StorageMigrationStatusFilterValue.Failed, title: t('Failed') },
-      { id: StorageMigrationStatusFilterValue.Completed, title: t('Completed') },
+    categoryLabel: t('Status'),
+    id: STORAGE_MIGRATION_STATUS_FILTER_TYPE,
+    match: (obj, selected) => selected.includes(getStorageMigrationStatus(obj)),
+    options: [
+      { label: t('Running'), value: StorageMigrationStatusFilterValue.Running },
+      { label: t('Pending'), value: StorageMigrationStatusFilterValue.Pending },
+      { label: t('Failed'), value: StorageMigrationStatusFilterValue.Failed },
+      { label: t('Completed'), value: StorageMigrationStatusFilterValue.Completed },
     ],
-    reducer: (obj) => getStorageMigrationStatus(obj),
-    type: STORAGE_MIGRATION_STATUS_FILTER_TYPE,
   },
 ];
