ece10b575ef4e91c1259f314c30da986e5169e4f
adamviktora <viktora.adam@gmail.com>
2026-09-01 14:08:03 +0200

    CNV-96253: fix switch layout in Project dropdown
    - fix switch not toggling on first click

--- src/utils/components/ClusterProjectDropdown/Dropdown/ShowSystemNamespacesSwitch.tsx
+++ src/utils/components/ClusterProjectDropdown/Dropdown/ShowSystemNamespacesSwitch.tsx
@@ -1,17 +1,15 @@
-import React, { FC, JSX } from 'react';
+import React, { type FC, type JSX } from 'react';
 
 import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
-import { Divider, Switch } from '@patternfly/react-core';
+import { MenuSearch, Switch } from '@patternfly/react-core';
 
 type ShowSystemNamespacesSwitchProps = {
-  cssPrefix: string;
   hasSystemNamespaces: boolean;
   isChecked: boolean;
   onChange: (isChecked: boolean) => void;
 };
 
 const ShowSystemNamespacesSwitch: FC<ShowSystemNamespacesSwitchProps> = ({
-  cssPrefix,
   hasSystemNamespaces,
   isChecked,
   onChange,
@@ -23,21 +21,17 @@ const ShowSystemNamespacesSwitch: FC<ShowSystemNamespacesSwitchProps> = ({
   }
 
   return (
-    <>
-      <Divider />
-      <div className={`${cssPrefix}__system-switch`}>
-        <Switch
-          onChange={(_event, checked) => {
-            _event.preventDefault();
-            _event.stopPropagation();
-            onChange(checked);
-          }}
-          id={`${cssPrefix}-show-system-switch`}
-          isChecked={isChecked}
-          label={t('Show default projects')}
-        />
-      </div>
-    </>
+    <MenuSearch>
+      <Switch
+        id="show-system-namespaces-switch"
+        isChecked={isChecked}
+        label={t('Show default projects')}
+        onChange={(_event, checked) => {
+          _event.stopPropagation();
+          onChange(checked);
+        }}
+      />
+    </MenuSearch>
   );
 };
 
