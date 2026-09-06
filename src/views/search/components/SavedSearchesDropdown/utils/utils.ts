8186c3027fe8533767f1b76650dc85e1a9db0cac
mayarub <mrubinst@redhat.com>
2026-09-06 13:18:59 +0300

    CNV-96336: Added is scrollable and divider to saved searches dropdown

---
+++ src/views/search/components/SavedSearchesDropdown/utils/utils.ts
@@ -0,0 +1,28 @@
+import { isEmpty } from '@kubevirt-utils/utils/utils';
+import { type SavedSearchEntry } from '@search/savedSearches/types';
+
+export const getSortedSavedsearches = (searches: SavedSearchEntry[], filterText: string) => {
+  const sorted = [...searches].sort((a, b) => Number(b.isFavorited) - Number(a.isFavorited));
+  if (!filterText) return sorted;
+  const lower = filterText.toLowerCase();
+  return sorted.filter(({ name }) => name.toLowerCase().includes(lower));
+};
+
+type SavedSearchesItemsToDisplay = {
+  favoriteSavedSearchesItems: SavedSearchEntry[];
+  otherSavedSearchesItems: SavedSearchEntry[];
+  shouldDisplayDivider: boolean;
+};
+
+export const getSavedSearchesItemsToDisplay = (
+  savedSearchesItems: SavedSearchEntry[],
+): SavedSearchesItemsToDisplay => {
+  const favoriteSavedSearchesItems = savedSearchesItems.filter((item) => item.isFavorited);
+  const otherSavedSearchesItems = savedSearchesItems.filter((item) => !item.isFavorited);
+
+  return {
+    favoriteSavedSearchesItems,
+    otherSavedSearchesItems,
+    shouldDisplayDivider: !isEmpty(favoriteSavedSearchesItems) && !isEmpty(otherSavedSearchesItems),
+  };
+};
