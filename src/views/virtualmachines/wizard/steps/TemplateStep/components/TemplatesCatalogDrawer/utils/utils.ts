7c306112086b12d77ef76bb659562ae72d519483
Maya Rubinstein <85433775+mayarub@users.noreply.github.com>
2026-09-08 13:36:01 +0300

    CNV-92165: Cloud user password parameter field validation (#4625)

--- src/views/virtualmachines/wizard/steps/TemplateStep/components/TemplatesCatalogDrawer/utils/utils.ts
+++ src/views/virtualmachines/wizard/steps/TemplateStep/components/TemplatesCatalogDrawer/utils/utils.ts
@@ -1,32 +1,32 @@
 /* eslint-disable */
-import { TemplateParameter } from '@kubevirt-ui-ext/kubevirt-api/console';
+import { type TFunction } from 'i18next';
+
+import { type TemplateParameter } from '@kubevirt-ui-ext/kubevirt-api/console';
 import {
-  V1beta1DataVolumeSpec,
-  V1ContainerDiskSource,
-  V1VirtualMachine,
+  type V1beta1DataVolumeSpec,
+  type V1ContainerDiskSource,
+  type V1VirtualMachine,
 } from '@kubevirt-ui-ext/kubevirt-api/kubevirt';
-import { V1beta1VirtualMachineTemplateSpecParameters } from '@kubevirt-ui-ext/kubevirt-api/virt-template';
+import { type V1beta1VirtualMachineTemplateSpecParameters } from '@kubevirt-ui-ext/kubevirt-api/virt-template';
 import {
   getParameters,
   isOpenShiftTemplate,
   isVirtualMachineTemplate,
-  Template,
+  type Template,
 } from '@kubevirt-utils/resources/template';
 import { getDisks, getVolumes } from '@kubevirt-utils/resources/vm';
+import { PARAMETER_VALUE_TYPES } from '@templates/details/tabs/parameters/constants';
+import {
+  getValueTypeFromParameter,
+  isPasswordParameter,
+} from '@templates/details/tabs/parameters/utils';
 import { NAME_INPUT_FIELD } from '@virtualmachines/wizard/steps/TemplateStep/components/TemplatesCatalog/utils/consts';
 
-export const getTemplateParametersSplit = (
-  parameters: TemplateParameter[],
-): [required: TemplateParameter[], optional: TemplateParameter[]] =>
-  (parameters ?? []).reduce(
-    (acc, currentParameter) => {
-      if (currentParameter.name === NAME_INPUT_FIELD) return acc;
-
-      acc[currentParameter.required ? 0 : 1].push(currentParameter);
-      return acc;
-    },
-    [[], []],
+export const getRequiredTemplateParameter = (parameters: TemplateParameter[]) => {
+  return (parameters ?? []).filter(
+    (parameter) => parameter.name !== NAME_INPUT_FIELD && parameter.required,
   );
+};
 
 export const getDiskSource = (
   vm: V1VirtualMachine,
@@ -83,3 +83,39 @@ export const getFirstUnfulfilledRequiredParameter = (
 
 export const allRequiredParametersAreFulfilled = (template: Template): boolean =>
   !getFirstUnfulfilledRequiredParameter(template);
+
+const MIN_CLOUD_USER_PASSWORD_LENGTH = 5;
+
+export const getPasswordParameterValueError = (t: TFunction, value: string): string | undefined => {
+  if (value?.length >= MIN_CLOUD_USER_PASSWORD_LENGTH) {
+    return;
+  }
+
+  return t('Minimum password length is {{ minLength }} characters', {
+    minLength: MIN_CLOUD_USER_PASSWORD_LENGTH,
+  });
+};
+
+export const hasPasswordParameterValueError = (t: TFunction, value: string): boolean =>
+  Boolean(getPasswordParameterValueError(t, value));
+
+export const hasAnyParameterValidationError = (
+  parameters: TemplateParameter[],
+  t: TFunction,
+): boolean =>
+  parameters.some((parameter) => hasPasswordParameterValueError(t, parameter.value ?? ''));
+
+export const hasInvalidPasswordParameter = (
+  parameters: TemplateParameter[],
+  t: TFunction,
+): boolean =>
+  parameters.some((parameter) => {
+    if (
+      getValueTypeFromParameter(parameter) !== PARAMETER_VALUE_TYPES.VALUE ||
+      !isPasswordParameter(parameter.name)
+    ) {
+      return false;
+    }
+
+    return hasPasswordParameterValueError(t, parameter.value ?? '');
+  });
