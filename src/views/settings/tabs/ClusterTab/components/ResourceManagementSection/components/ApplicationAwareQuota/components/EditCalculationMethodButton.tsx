4af83a631446834b71fb9d8a6c7ba08db94f7c03
Maya Rubinstein <85433775+mayarub@users.noreply.github.com>
2026-09-23 17:36:05 +0300

    CNV-97065: Improve Quota calculation method visual hierarchy (#4784)

--- src/views/settings/tabs/ClusterTab/components/ResourceManagementSection/components/ApplicationAwareQuota/components/EditCalculationMethodButton.tsx
+++ src/views/settings/tabs/ClusterTab/components/ResourceManagementSection/components/ApplicationAwareQuota/components/EditCalculationMethodButton.tsx
@@ -3,11 +3,15 @@ import { type FC } from 'react';
 import EditButton from '@kubevirt-utils/components/EditButton/EditButton';
 import { useModal } from '@kubevirt-utils/components/ModalProvider/ModalProvider';
 import { type HyperConverged } from '@kubevirt-utils/hooks/useHyperConvergeConfiguration';
+import { useKubevirtTranslation } from '@kubevirt-utils/hooks/useKubevirtTranslation';
 import { type CalculationMethod } from '@kubevirt-utils/resources/quotas/types';
+import { ButtonVariant, Popover, PopoverPosition } from '@patternfly/react-core';
 
 import { type CalculationMethodContentMapper } from '../types';
 
 import EditCalculationMethodModal from './EditCalculationMethodModal';
+
+import './EditCalculationMethodButton.scss';
 
 type EditCalculationMethodButtonProps = {
   calculationMethodContentMapper: CalculationMethodContentMapper;
@@ -20,9 +24,8 @@ const EditCalculationMethodButton: FC<EditCalculationMethodButtonProps> = ({
   hyperConverge,
   selectedCalculationMethod,
 }) => {
+  const { t } = useKubevirtTranslation();
   const { createModal } = useModal();
-
-  const selectedLabel = calculationMethodContentMapper[selectedCalculationMethod]?.label;
 
   const onClick = (): void => {
     createModal(({ isOpen, onClose }) => (
@@ -36,7 +39,17 @@ const EditCalculationMethodButton: FC<EditCalculationMethodButtonProps> = ({
     ));
   };
 
-  return <EditButton onClick={onClick}>{selectedLabel}</EditButton>;
+  return (
+    <Popover
+      bodyContent={t('Edit quota calculation method')}
+      className="edit-calculation-method-button__popover"
+      hasAutoWidth
+      position={PopoverPosition.right}
+      triggerAction="hover"
+    >
+      <EditButton onClick={onClick} variant={ButtonVariant.link} />
+    </Popover>
+  );
 };
 
 export default EditCalculationMethodButton;
