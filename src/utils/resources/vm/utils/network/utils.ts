733b5ae11b92cb526544816cd013347959481763
Gal Kremer <gkremer@redhat.com>
2026-09-25 21:04:08 -0400

    Manual cherry-pick 2f42f7f4c51e9c72f5b2b966db194767973b3231 to release-4.19

--- src/utils/resources/vm/utils/network/utils.ts
+++ src/utils/resources/vm/utils/network/utils.ts
@@ -1,4 +1,8 @@
-import { V1VirtualMachine, V1VirtualMachineInstance } from '@kubevirt-ui/kubevirt-api/kubevirt';
+import {
+  V1VirtualMachine,
+  V1VirtualMachineInstance,
+  V1VirtualMachineInstanceNetworkInterface,
+} from '@kubevirt-ui/kubevirt-api/kubevirt';
 import { getInterfaces, getNetworks } from '@kubevirt-utils/resources/vm';
 import {
   getVMIInterfaces,
@@ -24,6 +28,14 @@ export const sortNICs = (nics: NetworkPresentation[], direction: SortByDirection
     ),
   );
 
+const isLoopbackOrPseudoInterface = (status: V1VirtualMachineInstanceNetworkInterface): boolean => {
+  const { ipAddress = '', ipAddresses = [], name = '' } = status ?? {};
+
+  const isLoopbackName = name.toLowerCase().includes('loopback');
+  const isLoopbackIP = ipAddress === '127.0.0.1' || ipAddresses.includes('::1');
+
+  return isLoopbackName || isLoopbackIP;
+};
 export const getInterfacesAndNetworks = (vm: V1VirtualMachine, vmi: V1VirtualMachineInstance) => {
   const vmNetworks = getNetworks(vm) || [];
   const vmInterfaces = getInterfaces(vm) || [];
@@ -32,7 +44,9 @@ export const getInterfacesAndNetworks = (vm: V1VirtualMachine, vmi: V1VirtualMac
   const vmiNetworks = getVMINetworks(vmi) || [];
 
   const networks = removeDuplicatesByName([...vmNetworks, ...vmiNetworks]);
-  const interfaces = removeDuplicatesByName([...vmInterfaces, ...vmiInterfaces]);
+  const interfaces = removeDuplicatesByName([...vmInterfaces, ...vmiInterfaces]).filter(
+    (iface) => !isLoopbackOrPseudoInterface(iface),
+  );
 
   return {
     interfaces,
